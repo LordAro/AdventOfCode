9a48a0645da470b69ecc1ad9189faa083f3ce40f
Charles Pigott <charlespigott@googlemail.com>
2015-12-21 11:41:24 +0000

    Completed day19

--- src/day19.cpp
+++ src/day19.cpp
@@ -3,16 +3,52 @@
 #include <sstream>
 #include <map>
 #include <vector>
+#include <set>
+#include <cctype>
 
 using map_t = std::map<std::string, std::vector<std::string>>;
 
-std::vector<std::string> get_all_replacements(map_t conversions, std::string input)
+std::set<std::string> get_all_replacements(const map_t &conversions, const std::string &input)
 {
-	for (const auto &conv : conversions) {
-		for (const auto &res : conv.second) {
-			auto it = input.find(conv.first);
+	std::set<std::string> list;
+	for (size_t i = 0; i < input.size(); i++) {
+		if (islower(input[i])) continue;
+
+		std::string elem(1, input[i]);
+		if (i < input.size() - 1 && islower(input[i + 1])) elem += input[i + 1];
+		auto elem_convs = conversions.find(elem);
+		if (elem_convs == conversions.end()) continue;
+
+		for (const auto &conv : elem_convs->second) {
+			std::string converted = input;
+			converted.replace(i, elem.size(), conv);
+			list.insert(converted);
 		}
 	}
+	return list;
+}
+
+int fabrication_count(const map_t &conversions, const std::string &input)
+{
+	if (input == "") return 0;
+	for (const auto &conv : conversions) {
+		size_t pos;
+		size_t len = 1;
+		for (const auto &res : conv.second) {
+			pos = input.find(res);
+			if (pos != std::string::npos) {
+				len = res.size();
+				break;
+			}
+		}
+		if (pos == std::string::npos) continue;
+
+		std::string fab = input;
+		std::string repl = conv.first == "e" ? "" : conv.first;
+		fab.replace(pos, len, repl);
+		return fabrication_count(conversions, fab) + 1;
+	}
+	return 999999; // No result
 }
 
 int main(int argc, char **argv)
@@ -21,12 +57,11 @@ int main(int argc, char **argv)
 		std::cerr << "Incorrect number of arguments provided\n";
 		return 1;
 	}
-//	std::fstream input(argv[1]);
-//	if (!input) {
-//		std::cerr << "Could not open input file\n";
-//		return 1;
-//	}
-	std::istringstream input("H => HO\nH => OH\nO => HH\n\n HOH");
+	std::fstream input(argv[1]);
+	if (!input) {
+		std::cerr << "Could not open input file\n";
+		return 1;
+	}
 
 	map_t conversions;
 
@@ -39,16 +74,12 @@ int main(int argc, char **argv)
 		stream >> key >> arrow >> value;
 		conversions[key].push_back(value);
 	}
-
 	std::string medicine;
 	std::getline(input, medicine);
-	std::cout << medicine << "\n";
 
-	for (const auto &it : conversions) {
-		std::cout << it.first << " => ";
-		for (const auto &res : it.second) std::cout << res << ",";
-		std::cout << "\n";
-	}
+	auto list = get_all_replacements(conversions, medicine);
+	std::cout << "Number of possible replacements: " << list.size() << "\n";
 
+	std::cout << "Fabrication count: " << fabrication_count(conversions, medicine) << "\n";
 	return 0;
 }
