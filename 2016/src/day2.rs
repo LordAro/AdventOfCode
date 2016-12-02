21d40bc1c764a73c1ea9a34d9820a30dab5bf525
Charles Pigott <charlespigott@googlemail.com>
2016-12-02 18:28:04 +0000

    Implement d2p2

--- 2016/src/day2.rs
+++ 2016/src/day2.rs
@@ -14,6 +14,72 @@ use std::io::{BufRead, BufReader};
 //   A B C
 //     D
 
+fn up(p: i32) -> Option<i32> {
+    match p {
+        1 | 2 | 3 => Some(p),
+        4 | 5 | 6 => Some(p - 3),
+        7 | 8 | 9 => Some(p - 3),
+        _ => None,
+    }
+}
+fn down(p: i32) -> Option<i32> {
+    match p {
+        1 | 4 | 7 => Some(p + 1),
+        2 | 5 | 8 => Some(p + 1),
+        3 | 6 | 9 => Some(p),
+        _ => None,
+    }
+}
+fn left(p: i32) -> Option<i32> {
+    match p {
+        1 | 4 | 7 => Some(p),
+        2 | 5 | 8 => Some(p - 1),
+        3 | 6 | 9 => Some(p - 1),
+        _ => None,
+    }
+}
+
+fn right(p: i32) -> Option<i32> {
+    match p {
+        1 | 2 | 3 => Some(p + 3),
+        4 | 5 | 6 => Some(p + 3),
+        7 | 8 | 9 => Some(p),
+        _ => None,
+    }
+}
+
+
+fn up2(p: i32) -> Option<i32> {
+    match p {
+        1 | 2 | 4 | 5 | 9 => Some(p),
+        6 | 7 | 8 | 0xA | 0xB | 0xC => Some(p - 4),
+        3 | 0xD => Some(p - 2),
+        _ => None,
+    }
+}
+fn down2(p: i32) -> Option<i32> {
+    match p {
+        5 | 9 | 0xA | 0xC | 0xD => Some(p),
+        1 | 0xB => Some(p + 2),
+        2 | 3 | 4 | 6 | 7 | 8 => Some(p + 4),
+        _ => None,
+    }
+}
+fn left2(p: i32) -> Option<i32> {
+    match p {
+        1 | 2 | 5 | 0xA | 0xD => Some(p),
+        3 | 4 | 6 | 7 | 8 | 9 | 0xB | 0xC => Some(p - 1),
+        _ => None,
+    }
+}
+fn right2(p: i32) -> Option<i32> {
+    match p {
+        1 | 4 | 9 | 0xC | 0xD => Some(p),
+        2 | 3 | 5 | 6 | 7 | 8 | 0xA | 0xB => Some(p + 1),
+        _ => None,
+    }
+}
+
 fn main() {
     if env::args().len() != 2 {
         panic!("Incorrect number of arguments provided\n");
@@ -25,47 +91,33 @@ fn main() {
     let file_buf = BufReader::new(&f);
 
     let mut code = String::new();
+    let mut code2 = String::new();
     let mut cur_pos = 5; // Starting point
+    let mut cur_pos2 = 5;
+
     for line in file_buf.lines() {
         let l = line.unwrap();
         for c in l.chars() {
+            cur_pos2 = match c {
+                    'U' => up2(cur_pos2),
+                    'L' => left2(cur_pos2),
+                    'R'_ => right2(cur_pos2),
+                    'D' => down2(cur_pos2),
+                    _ => panic!("Unrecognised character"),
+                }
+                .unwrap();
             cur_pos = match c {
-                'U' => {
-                    match cur_pos {
-                        1 | 2 | 3 => cur_pos,
-                        4 | 5 | 6 => cur_pos - 3,
-                        7 | 8 | 9 => cur_pos - 3,
-                        _ => panic!("Weird position: {}", cur_pos),
-                    }
+                    'U' => up(cur_pos),
+                    'L' => left(cur_pos),
+                    'R'_ => right(cur_pos),
+                    'D' => down(cur_pos),
+                    _ => panic!("Unrecognised character"),
                 }
-                'L' => {
-                    match cur_pos {
-                        1 | 4 | 7 => cur_pos,
-                        2 | 5 | 8 => cur_pos - 1,
-                        3 | 6 | 9 => cur_pos - 1,
-                        _ => panic!("Weird position: {}", cur_pos),
-                    }
-                }
-                'R'_ => {
-                    match cur_pos {
-                        1 | 4 | 7 => cur_pos + 1,
-                        2 | 5 | 8 => cur_pos + 1,
-                        3 | 6 | 9 => cur_pos,
-                        _ => panic!("Weird position: {}", cur_pos),
-                    }
-                }
-                'D' => {
-                    match cur_pos {
-                        1 | 2 | 3 => cur_pos + 3,
-                        4 | 5 | 6 => cur_pos + 3,
-                        7 | 8 | 9 => cur_pos,
-                        _ => panic!("Weird position: {}", cur_pos),
-                    }
-                }
-                _ => panic!("Unrecognised character"),
-            };
+                .unwrap();
         }
         code.push_str(&cur_pos.to_string());
+        code2.push_str(&format!("{:X}", cur_pos2));
     }
     println!("Bathroom code: {}", code);
+    println!("Actual bathroom code: {}", code2);
 }
