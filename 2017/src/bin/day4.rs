f54b05d54cb0ed348ccf2feef91a68e905e49c46
Charles Pigott <charlespigott@googlemail.com>
2017-12-04 19:09:56 +0000

    into_iter was unnecessary

--- 2017/src/bin/day4.rs
+++ 2017/src/bin/day4.rs
@@ -19,13 +19,13 @@ fn main() {
     let valid1: usize = input
         .iter()
         .map(|v| v.iter().sorted())
-        .filter(|v| v.into_iter().dedup().count() == v.len())
+        .filter(|v| v.iter().dedup().count() == v.len())
         .count();
 
     let valid2: usize = input
         .iter()
-        .map(|v| v.into_iter().map(|w| w.chars().sorted()).sorted())
-        .filter(|v| v.into_iter().dedup().count() == v.len())
+        .map(|v| v.iter().map(|w| w.chars().sorted()).sorted())
+        .filter(|v| v.iter().dedup().count() == v.len())
         .count();
 
     println!("Valid passphrases: {:?}", valid1);
