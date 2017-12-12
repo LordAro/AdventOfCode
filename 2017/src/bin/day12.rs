71dec478f0f8ee731dcc7380fe106bd327d86c1c
Charles Pigott <charlespigott@googlemail.com>
2017-12-12 14:00:17 +0000

    Add day12

---
+++ 2017/src/bin/day12.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use std::env;
+use std::io::{BufReader, BufRead};
+use std::collections::{HashSet, HashMap};
+
+fn get_linked(data: &Vec<Vec<usize>>, idx: usize, linked: &mut HashSet<usize>) {
+    if !linked.insert(idx) {
+        return;
+    }
+    data[idx].iter().for_each(|i| get_linked(data, *i, linked));
+}
+
+fn main() {
+    if env::args().len() != 2 {
+        panic!("Incorrect number of arguments provided");
+    }
+    let input: Vec<_> = BufReader::new(File::open(&env::args().nth(1).unwrap()).unwrap())
+        .lines()
+        .map(|l| l.unwrap())
+        .collect();
+
+    let splitted: Vec<Vec<usize>> = input
+        .iter()
+        .map(|l| {
+            l.split(" <-> ")
+                .nth(1)
+                .unwrap()
+                .split(", ")
+                .map(|n| n.parse().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let mut groups: HashMap<_, HashSet<_>> = HashMap::new();
+    for n in 0..splitted.len() {
+        if groups
+            .iter()
+            .flat_map(|(_, v)| v.iter())
+            .find(|&&x| x == n)
+            .is_some()
+        {
+            continue;
+        }
+        groups.insert(n, HashSet::new());
+        get_linked(&splitted, n, &mut groups.get_mut(&n).unwrap());
+    }
+
+    println!("Group 0 size: {}", groups.get(&0).unwrap().len());
+    println!("Total groups: {}", groups.keys().count());
+}
