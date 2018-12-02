07845e57561f1e5c07fe095f7eba6bcf17ba8077
Charles Pigott <charlespigott@googlemail.com>
2018-12-02 22:31:24 +0000

    rbtree -> assoc arr

--- 2018/src/day1.d
+++ 2018/src/day1.d
@@ -1,5 +1,4 @@
 import std.algorithm;
-import std.container.rbtree;
 import std.file;
 import std.range;
 import std.stdio;
@@ -9,13 +8,13 @@ void main(string[] args)
 	auto lines = slurp!(int)(args[1], "%d");
 	writeln("Resulting frequency: ", sum(lines));
 
-	auto seen = redBlackTree!int();
+	bool[int] seen;
 	int cur_freq = 0;
 	foreach (freq; cycle(lines)) {
 		if (cur_freq in seen) {
 			break;
 		}
-		seen.insert(cur_freq);
+		seen[cur_freq] = true;
 		cur_freq += freq;
 	}
 	writeln("First duplicate frequency: ", cur_freq);
