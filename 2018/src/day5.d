08b617341f7114b9c243077a479d56cd40e9c66a
Charles Pigott <charlespigott@googlemail.com>
2018-12-09 01:49:51 +0000

    Mild horribleness to implement minor optimisation

--- 2018/src/day5.d
+++ 2018/src/day5.d
@@ -1,45 +1,48 @@
 import std.ascii : toUpper;
-import std.algorithm : min, count;
+import std.algorithm : min;
 import std.file : readText;
 import std.container : SList;
+import std.range : dropBackOne;
 import std.stdio : writeln;
-import std.string : strip;
 import std.typecons : tuple, Tuple;
 
-Tuple!(SList!ubyte, size_t) collapse(SList!ubyte polymer, ubyte ignore = 0xff)
+alias ByteStack = Tuple!(SList!ubyte, "s", size_t, "len");
+
+ByteStack collapse(ByteStack polymer, size_t cur_min, ubyte ignore = 0xff)
 {
-	size_t len;
-	SList!ubyte reduced;
-	foreach (c; polymer) {
-		if (!reduced.empty && reduced.front != c && toUpper(reduced.front) == toUpper(c)) {
-			reduced.removeFront();
-			len--;
-		} else if (ignore != c && toUpper(ignore) != c) {
-			reduced.insertFront(c);
-			len++;
+	size_t len = 0;
+	ByteStack reduced;
+	ubyte ignore_upper = toUpper(ignore);
+	size_t i = 0;
+	foreach (c; polymer.s) {
+		// Bail early if length has exceeded previous minimum - it can't get any smaller than this...
+		auto remaining = polymer.len - i;
+		if (reduced.len > remaining && reduced.len - remaining > cur_min) { // Remember to protect against underflows
+			return tuple!("s", "len")(polymer.s, cur_min);
 		}
+		if (!reduced.s.empty && reduced.s.front != c && toUpper(reduced.s.front) == toUpper(c)) {
+			reduced.s.removeFront();
+			reduced.len--;
+		} else if (ignore != c && ignore_upper != c) {
+			reduced.s.insertFront(c);
+			reduced.len++;
+		}
+		i++;
 	}
-	return tuple(reduced, len);
+	return reduced;
 }
 
 void main(string[] args)
 {
-	auto polymer = SList!ubyte(cast(ubyte[])readText(args[1]).strip);
+	auto input = cast(ubyte[])readText(args[1]).dropBackOne;
+	auto polymer = tuple!("s", "len")(SList!ubyte(input), input.length);
 
-	auto reduced = collapse(polymer);
+	auto reduced = collapse(polymer, input.length + 1);
 	writeln("Final length: ", reduced[1]);
 
 	size_t min_len = 50000;
 	for (byte c = 'a'; c <= 'z'; c++) {
-		min_len = min(min_len, collapse(reduced[0], c)[1]);
+		min_len = collapse(reduced, min_len, c)[1]; // always returns minimum
 	}
 	writeln("Minimal length: ", min_len);
-}
-
-unittest
-{
-	assert(collapse("aA") == tuple("", 0));
-	assert(collapse("abBA") == tuple("", 0));
-	assert(collapse("abAB") == tuple("abAB", 4));
-	assert(collapse("aabAAB") == tuple("aabAAB", 6));
 }
