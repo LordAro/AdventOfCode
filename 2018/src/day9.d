6b5464b99dfaac93a1e73ce97783d4120ab26542
Charles Pigott <charlespigott@googlemail.com>
2018-12-09 22:27:04 +0000

    Implement own doubly linked list, for x2 speed up

--- 2018/src/day9.d
+++ 2018/src/day9.d
@@ -1,19 +1,54 @@
 import std.algorithm : maxElement;
-import std.container : DList;
 import std.file : slurp;
 import std.stdio : writeln;
 
-void rotate(ref DList!int dll, int count)
-{
-	if (count > 0) {
-		for (int i = 0; i < count; i++) {
-			dll.insertBack(dll.front);
-			dll.removeFront();
-		}
-	} else {
-		for (int i = 0; i < -count; i++) {
-			dll.insertFront(dll.back);
-			dll.removeBack();
+struct Ring {
+	struct Node {
+		Node *prev;
+		Node *next;
+		int val;
+	}
+
+	private Node *current;
+
+	this(int init)
+	{
+		this.current = new Node;
+		this.current.prev = this.current;
+		this.current.next = this.current;
+		this.current.val = init;
+	}
+
+	int popVal()
+	{
+		auto n = this.current;
+		n.prev.next = n.next;
+		n.next.prev = n.prev;
+		this.current = this.current.next;
+		return n.val;
+	}
+
+	void insert(int val)
+	{
+		auto n = new Node;
+		n.val = val;
+		n.next = this.current;
+		n.prev = this.current.prev;
+		this.current.prev.next = n;
+		this.current.prev = n;
+		this.current = n;
+	}
+
+	void rotate(int count)
+	{
+		if (count > 0) {
+			for (int i = 0; i < count; i++) {
+				this.current = this.current.next;
+			}
+		} else {
+			for (int i = 0; i < -count; i++) {
+				this.current = this.current.prev;
+			}
 		}
 	}
 }
@@ -21,18 +56,15 @@ void rotate(ref DList!int dll, int count)
 ulong marble_game(int players, int last_marble)
 {
 	ulong[int] player_scores;
-	DList!int marble_circle;
-	marble_circle.insertBack(0);
+	auto marble_circle = Ring(0);
 	for (int marble = 1; marble <= last_marble; marble++) {
 		if (marble % 23 == 0) {
-			// Also remove previous increment
-			rotate(marble_circle, 7);
-			player_scores[marble % players] += marble_circle.front + marble;
-			marble_circle.removeFront();
-			rotate(marble_circle, -1);
+			marble_circle.rotate(7);
+			player_scores[marble % players] += marble_circle.popVal() + marble;
+			marble_circle.rotate(-1);
 		} else {
-			rotate(marble_circle, -1);
-			marble_circle.insertFront(marble);
+			marble_circle.rotate(-1);
+			marble_circle.insert(marble);
 		}
 	}
 	return player_scores.values.maxElement;
@@ -48,3 +80,21 @@ void main(string[] args)
 	writeln("High score for bigger game: ", marble_game(players, last_marble * 100));
 }
 
+unittest
+{
+	Ring r = Ring(0);
+	r.insert(1);
+	assert(r.current.val == 1);
+	assert(r.current.prev.val == 0);
+	assert(r.current.next.val == 0);
+}
+
+unittest
+{
+	assert(marble_game(9, 25) == 32);
+	assert(marble_game(17, 1104) == 2764);
+	assert(marble_game(10, 1618) == 8317);
+	assert(marble_game(30, 5807) == 37305);
+	assert(marble_game(21, 6111) == 54718);
+	assert(marble_game(13, 7999) == 146373);
+}
