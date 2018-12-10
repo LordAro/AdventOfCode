924a6c43a57bb61e100582b3b2c45d4b03644739
Charles Pigott <charlespigott@googlemail.com>
2018-12-10 22:18:10 +0000

    Minor generalisation & type consistency

--- 2018/src/day10.d
+++ 2018/src/day10.d
@@ -7,7 +7,7 @@ import std.math : abs;
 import std.typecons : Tuple, tuple;
 import std.string : strip;
 
-auto get_minmax(Tuple!(int, int)[] grid)
+auto get_minmax(G)(G grid)
 {
 	auto xs = grid.map!(a => a[0]);
 	auto ys = grid.map!(a => a[1]);
@@ -15,7 +15,7 @@ auto get_minmax(Tuple!(int, int)[] grid)
 	                                                  ys.minElement, ys.maxElement);
 }
 
-void print_grid(Tuple!(int, int)[] grid)
+void print_grid(G)(G grid)
 {
 	auto minmax = get_minmax(grid);
 	for (int j = minmax.min_y; j <= minmax.max_y; j++) {
@@ -30,7 +30,7 @@ void print_grid(Tuple!(int, int)[] grid)
 	}
 }
 
-ulong get_gridsize(Tuple!(int, int)[] grid)
+ulong get_gridsize(G)(G grid)
 {
 	auto minmax = get_minmax(grid);
 	return cast(ulong)abs(minmax.max_x - minmax.min_x) * cast(ulong)abs(minmax.max_y - minmax.min_y);
@@ -41,21 +41,21 @@ void main(string[] args)
 	auto input = slurp!(string, string, string, string)(args[1], "position=<%s, %s> velocity=<%s, %s>")
 		.map!(t => tuple(tuple(to!int(t[0].strip), to!int(t[1].strip)),
 		                 tuple(to!int(t[2].strip), to!int(t[3].strip))));
-	Tuple!(int, int)[] points = input.map!(t => t[0]).array;
-	Tuple!(int, int)[] velos = input.map!(t => t[1]).array;
+	auto points = input.map!(t => t[0]).array; // Both need to be arrays so it doesn't try to recompute values each time
+	auto velos = input.map!(t => t[1]).array;
 
 	auto grid_size = get_gridsize(points);
 
-	int s = 1;
+	ulong s = 1;
 	for (; s < 12000; s++) {
-		for (size_t i = 0; i < points.length; i++) {
+		for (ulong i = 0; i < points.length; i++) {
 			points[i][0] += velos[i][0];
 			points[i][1] += velos[i][1];
 		}
 		auto new_grid_size = get_gridsize(points);
 		if (new_grid_size > grid_size) {
 			// Rewind a timestep
-			for (size_t i = 0; i < points.length; i++) {
+			for (ulong i = 0; i < points.length; i++) {
 				points[i][0] -= velos[i][0];
 				points[i][1] -= velos[i][1];
 			}
