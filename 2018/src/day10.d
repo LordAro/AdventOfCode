f517c18df187fa2f0308425f9c2e61dd903c609f
Charles Pigott <charlespigott@googlemail.com>
2018-12-10 23:22:03 +0000

    Couple minor improvements with unrolling a function, and some parameter tweaking

--- 2018/src/day10.d
+++ 2018/src/day10.d
@@ -1,5 +1,5 @@
 import std.array : array;
-import std.algorithm : minElement, maxElement, map, canFind;
+import std.algorithm : minElement, maxElement, map, canFind, min, max;
 import std.conv : to;
 import std.file : slurp;
 import std.stdio : write, writeln;
@@ -8,7 +8,7 @@ import std.range : iota;
 import std.typecons : Tuple, tuple;
 import std.string : strip;
 
-auto get_minmax(G)(G grid)
+auto get_minmax(G)(in auto ref G grid)
 {
 	auto xs = grid.map!(a => a[0]);
 	auto ys = grid.map!(a => a[1]);
@@ -16,7 +16,7 @@ auto get_minmax(G)(G grid)
 	                                                  ys.minElement, ys.maxElement);
 }
 
-void print_grid(G)(G grid)
+void print_grid(G)(in auto ref G grid)
 {
 	auto minmax = get_minmax(grid);
 	for (int j = minmax.min_y; j <= minmax.max_y; j++) {
@@ -31,13 +31,31 @@ void print_grid(G)(G grid)
 	}
 }
 
-ulong get_gridsize(G)(G grid)
+ulong get_gridsize(G)(in auto ref G grid)
 {
 	auto minmax = get_minmax(grid);
 	return cast(ulong)abs(minmax.max_x - minmax.min_x) * cast(ulong)abs(minmax.max_y - minmax.min_y);
 }
 
-G get_grid(G)(G points, G velocities, int factor)
+// Without having to get the grid first
+ulong get_gridsize(G)(in auto ref G points, in auto ref G velocities, int factor)
+{
+	int min_x = int.max;
+	int max_x = int.min;
+	int min_y = int.max;
+	int max_y = int.min;
+	foreach (i; points.length.iota) {
+		auto x = points[i][0] + (factor * velocities[i][0]);
+		auto y = points[i][1] + (factor * velocities[i][1]);
+		min_x = min(min_x, x);
+		max_x = max(max_x, x);
+		min_y = min(min_y, y);
+		max_y = max(max_y, y);
+	}
+	return cast(ulong)abs(max_x - min_x) * cast(ulong)abs(max_y - min_y);
+}
+
+G get_grid(G)(in auto ref G points, in auto ref G velocities, int factor)
 {
 	return points.length.iota.map!(i => tuple(points[i][0] + (factor * velocities[i][0]), points[i][1] + (factor * velocities[i][1]))).array;
 }
@@ -47,16 +65,16 @@ void main(string[] args)
 	auto input = slurp!(string, string, string, string)(args[1], "position=<%s, %s> velocity=<%s, %s>")
 		.map!(t => tuple(tuple(to!int(t[0].strip), to!int(t[1].strip)),
 		                 tuple(to!int(t[2].strip), to!int(t[3].strip))));
-	auto points = input.map!(t => t[0]).array; // Both need to be arrays so it doesn't try to recompute values each time
-	auto velos = input.map!(t => t[1]).array;
+	immutable auto points = input.map!(t => t[0]).array; // Both need to be arrays so it doesn't try to recompute values each time
+	immutable auto velos = input.map!(t => t[1]).array;
 
 	int l = 0;
 	int u = 1;
-	while (l != u) {
+	while (true) {
 		int u2 = 1;
 		for (;; u2 *= 2) {
-			auto s1 = get_gridsize(get_grid(points, velos, l + u2));
-			auto s2 = get_gridsize(get_grid(points, velos, l + u2 + 1));
+			auto s1 = get_gridsize(points, velos, l + u2);
+			auto s2 = get_gridsize(points, velos, l + u2 + 1);
 			if (s1 < s2) break;
 		}
 		u = l + u2;
