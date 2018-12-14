4e4464c33de31b224b5f307ab019a100d5746dcb
Charles Pigott <charlespigott@googlemail.com>
2018-12-14 19:52:14 +0000

    Fix up conditions

--- 2018/src/day14.d
+++ 2018/src/day14.d
@@ -1,10 +1,9 @@
-import std.algorithm : maxElement;
 import std.file : readText;
 import std.stdio : writeln;
-import std.range;
-import std.string;
-import std.algorithm;
-import std.conv;
+import std.range : array;
+import std.string : strip;
+import std.algorithm : map;
+import std.conv : to;
 
 void main(string[] args)
 {
@@ -14,7 +13,6 @@ void main(string[] args)
 	ubyte[] input_array = input.map!(c => cast(ubyte)(c - '0')).array;
 
 	ubyte[] recipes = [3, 7];
-	recipes.reserve(input_limit + 10);
 
 	ulong elf1 = 0;
 	ulong elf2 = 1;
@@ -30,15 +28,22 @@ void main(string[] args)
 		}
 		elf1 = (elf1 + 1 + recipes[elf1]) % recipes.length;
 		elf2 = (elf2 + 1 + recipes[elf2]) % recipes.length;
-		if (recipes.length == input_limit + 10) {
-			writeln("Recipes: ", cast(string)recipes[$-10 .. $].map!(c => c + '0').array);
+
+		// Avoid possibility of off by one error by checking both conditions
+		if (!reached_limit && recipes.length == input_limit + 10) {
+			writeln("Recipes: ", cast(string)recipes[$ - 10 .. $].map!(c => c + '0').array);
+			reached_limit = true;
+		} else if (!reached_limit && recipes.length == input_limit + 10 + 1) {
+			writeln("Recipes: ", cast(string)recipes[$ - 10 - 1 .. $ - 1].map!(c => c + '0').array);
 			reached_limit = true;
 		}
+
+		// Part2
 		if (!found_subset && recipes.length > input_array.length) {
-			if (recipes[$-input_array.length - 1 .. $ - 1] == input_array) {
-				writeln("Found pattern at position ", recipes.length - input_array.length - 1);
+			if (recipes[$ - input_array.length .. $] == input_array) {
+				writeln("Found pattern at position ", recipes.length - input_array.length);
 				found_subset = true;
-			} else if (recipes[$-input_array.length - 1 .. $ - 1] == input_array) {
+			} else if (recipes[$ - input_array.length - 1 .. $ - 1] == input_array) {
 				writeln("Found pattern at position ", recipes.length - input_array.length - 1);
 				found_subset = true;
 			}
