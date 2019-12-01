9b75038ad16dcf9750e47db36580a472aadfd259
Charles Pigott <charlespigott@googlemail.com>
2019-12-01 18:55:01 +0000

    Add day 1 solution

--- 2019/src/bin/day1.rs
+++ 2019/src/bin/day1.rs
@@ -1,16 +1,37 @@
 use std::env;
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{BufRead, BufReader};
+
+/* Shiny iterator function */
+fn fuel_reqs(mass: i32) -> impl std::iter::Iterator<Item = i32> {
+    let mut m = mass;
+    std::iter::from_fn(move || {
+        let result;
+        m = m / 3 - 2;
+        if m > 0 {
+            result = Some(m);
+        } else {
+            result = None
+        }
+        result
+    })
+}
 
 fn main() -> io::Result<()> {
     if env::args().len() != 2 {
         panic!("Incorrect number of arguments provided");
     }
-    let mut input = File::open(&env::args().nth(1).unwrap()).expect("Could not open input file");
+    let modules: Vec<_> = BufReader::new(
+        File::open(&env::args().nth(1).unwrap()).expect("Could not open input file"),
+    )
+    .lines()
+    .map(|l| l.unwrap().parse().unwrap())
+    .collect();
 
-    let mut contents = String::new();
-    input.read_to_string(&mut contents)?;
-    println!("Input str: {}", contents);
+    let fuel_sum: i32 = modules.iter().map(|n| fuel_reqs(*n).next().unwrap()).sum();
+    let rec_fuel_sum: i32 = modules.iter().map(|n| fuel_reqs(*n).sum::<i32>()).sum();
+    println!("Fuel requirements: {}", fuel_sum);
+    println!("Fuel requirements (inc fuel): {}", rec_fuel_sum);
     Ok(())
 }
