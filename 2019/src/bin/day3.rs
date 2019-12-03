b013e58bfaaeed87d20c970d2cb4b223c5bdc2ab
Charles Pigott <charlespigott@googlemail.com>
2019-12-03 12:18:40 +0000

    Complete p2

--- 2019/src/bin/day3.rs
+++ 2019/src/bin/day3.rs
@@ -1,8 +1,31 @@
 use std::collections::HashSet;
 use std::env;
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 use std::io;
 use std::io::{BufRead, BufReader};
+
+#[derive(Clone, Copy)]
+struct Coord {
+    signal_delay: usize,
+    x: i32,
+    y: i32,
+}
+
+// Big hacks to make the HashSet ignore the signal delay when comparing/hashing Coords
+impl PartialEq for Coord {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+impl Eq for Coord {}
+
+impl Hash for Coord {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
 
 fn get_route<T: BufRead>(reader: &mut std::io::Lines<T>) -> Vec<(char, i32)> {
     reader
@@ -43,30 +66,52 @@ fn main() -> io::Result<()> {
     let route1 = get_route(&mut reader);
     let route2 = get_route(&mut reader);
 
-    let mut visited_coords: HashSet<(i32, i32)> = HashSet::new();
-    let mut cur_pos = (0, 0);
+    let mut visited_coords: HashSet<Coord> = HashSet::new();
+    let mut cur_pos = Coord {
+        signal_delay: 0,
+        x: 0,
+        y: 0,
+    };
     for (dir, dist) in &route1 {
         let change = get_change_dir(*dir).unwrap();
         for _ in 0..*dist {
-            cur_pos = (cur_pos.0 + change.0, cur_pos.1 + change.1);
-            visited_coords.insert(cur_pos);
+            cur_pos = Coord {
+                signal_delay: cur_pos.signal_delay + 1,
+                x: cur_pos.x + change.0,
+                y: cur_pos.y + change.1,
+            };
+            if !visited_coords.contains(&cur_pos) {
+                visited_coords.insert(cur_pos);
+            }
         }
     }
 
-    cur_pos = (0, 0);
+    cur_pos = Coord {
+        signal_delay: 0,
+        x: 0,
+        y: 0,
+    };
     let mut min_dist = i32::max_value();
+    let mut min_signal_delay = usize::max_value();
     for (dir, dist) in &route2 {
         let change = get_change_dir(*dir).unwrap();
         for _ in 0..*dist {
-            cur_pos = (cur_pos.0 + change.0, cur_pos.1 + change.1);
+            cur_pos.signal_delay += 1;
+            cur_pos.x += change.0;
+            cur_pos.y += change.1;
             if visited_coords.contains(&cur_pos) {
-                let man_dist = cur_pos.0.abs() + cur_pos.1.abs();
+                let man_dist = cur_pos.x.abs() + cur_pos.y.abs();
                 if man_dist < min_dist {
                     min_dist = man_dist;
+                }
+                let previous_signal_delay = visited_coords.get(&cur_pos).unwrap().signal_delay;
+                if previous_signal_delay + cur_pos.signal_delay < min_signal_delay {
+                    min_signal_delay = previous_signal_delay + cur_pos.signal_delay;
                 }
             }
         }
     }
     println!("Wires cross at distance: {}", min_dist);
+    println!("Minimum signal delay: {}", min_signal_delay);
     Ok(())
 }
