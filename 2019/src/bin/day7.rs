d74f9503b710e156f1ada0a7ef04e25f0aab71a2
Charles Pigott <charlespigott@googlemail.com>
2019-12-07 17:45:55 +0000

    Day7p1

---
+++ 2019/src/bin/day7.rs
@@ -0,0 +1,50 @@
+use std::cmp::max;
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+
+extern crate permutohedron;
+use permutohedron::Heap;
+
+extern crate advent_of_code;
+use advent_of_code::intcode;
+
+fn main() -> io::Result<()> {
+    let program_str = BufReader::new(
+        File::open(
+            &env::args()
+                .nth(1)
+                .expect("Incorrect number of arguments provided"),
+        )
+        .expect("Could not open input file"),
+    )
+    .lines()
+    .next()
+    .unwrap()
+    .unwrap();
+
+    let program = intcode::read_input(&program_str);
+
+    let mut phases = vec![0, 1, 2, 3, 4];
+    let phase_heap = Heap::new(&mut phases);
+    let mut max_output = isize::min_value();
+    for permutation in phase_heap {
+        let mut input_val = 0;
+        for phase in permutation {
+            let mut mach = intcode::Machine::new(&program, &[phase, input_val]);
+            let output = mach.run();
+            input_val = *output.last().unwrap();
+        }
+        max_output = max(max_output, input_val);
+    }
+    println!("Maximum output {}", max_output);
+
+    let mut phases = vec![5, 6, 7, 8, 9];
+    let phase_heap = Heap::new(&mut phases);
+    for permutation in phase_heap {
+        println!("{:?}", permutation);
+    }
+
+    Ok(())
+}
