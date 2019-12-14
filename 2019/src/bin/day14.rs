24cbf9ef23286eedbc828e14a14fcdaa6d382de2
Charles Pigott <charlespigott@googlemail.com>
2019-12-14 22:13:24 +0000

    Day14p2

--- 2019/src/bin/day14.rs
+++ 2019/src/bin/day14.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-type ReactionMap<'a> = HashMap<&'a str, (u32, Vec<(&'a str, u32)>)>;
+type ReactionMap<'a> = HashMap<&'a str, (u64, Vec<(&'a str, u64)>)>;
 
 fn parse_reactions<'a, T: AsRef<str>>(input_str: &'a [T]) -> ReactionMap {
     input_str
@@ -16,30 +16,30 @@ fn parse_reactions<'a, T: AsRef<str>>(input_str: &'a [T]) -> ReactionMap {
                 .split(", ")
                 .map(|s| {
                     let mut ins = s.split(" ");
-                    let in_n = ins.next().unwrap().parse::<u32>().unwrap();
+                    let in_n = ins.next().unwrap().parse::<u64>().unwrap();
                     let in_name = ins.next().unwrap();
                     (in_name, in_n)
                 })
                 .collect::<Vec<_>>();
 
             let mut out = in_out.next().unwrap().split(" ");
-            let output_n = out.next().unwrap().parse::<u32>().unwrap();
+            let output_n = out.next().unwrap().parse::<u64>().unwrap();
             let output_name = out.next().unwrap();
             (output_name, (output_n, inputs))
         })
         .collect()
 }
 
-fn div_ceil(a: u32, b: u32) -> u32 {
+fn div_ceil(a: u64, b: u64) -> u64 {
     (a + b - 1) / b
 }
 
 fn get_ore_count<'a, 'b>(
     reactions: &'a ReactionMap,
     target: &'a str,
-    quantity_required: u32,
-    waste: &'b mut HashMap<&'a str, u32>,
-) -> u32 {
+    quantity_required: u64,
+    waste: &'b mut HashMap<&'a str, u64>,
+) -> u64 {
     let mut quantity_required = quantity_required;
     if target == "ORE" {
         return quantity_required;
@@ -64,9 +64,28 @@ fn get_ore_count<'a, 'b>(
         .sum()
 }
 
-fn required_ore_count(reactions: &ReactionMap) -> u32 {
+fn required_ore_count(reactions: &ReactionMap) -> u64 {
     let mut waste = HashMap::new();
     get_ore_count(reactions, "FUEL", 1, &mut waste)
+}
+
+fn get_fuel_count(reactions: &ReactionMap) -> u64 {
+    let mut lower_bound = 1;
+    let mut upper_bound = 2;
+    while get_ore_count(reactions, "FUEL", upper_bound, &mut HashMap::new()) < 1_000_000_000_000 {
+        upper_bound *= 2;
+    }
+
+    while lower_bound < upper_bound {
+        let mid = (upper_bound + lower_bound) / 2;
+        let ore_count = get_ore_count(reactions, "FUEL", mid, &mut HashMap::new());
+        if ore_count < 1_000_000_000_000 {
+            lower_bound = mid + 1;
+        } else {
+            upper_bound = mid;
+        }
+    }
+    lower_bound - 1 // lower_bound is first count > 1tril
 }
 
 fn main() {
@@ -88,11 +107,57 @@ fn main() {
         "Total ore needed for 1 FUEL: {}",
         required_ore_count(&reactions)
     );
+
+    println!(
+        "Total FUEL able to be produced: {}",
+        get_fuel_count(&reactions)
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use crate::*;
+
+    const EX3_INPUT_STR: &str = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
+
+    const EX4_INPUT_STR: &str = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+
+    const EX5_INPUT_STR: &str = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
 
     #[test]
     fn ex1() {
@@ -127,17 +192,7 @@ mod tests {
 
     #[test]
     fn ex3() {
-        let input_str: Vec<_> = "157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
-            .lines()
-            .collect();
+        let input_str: Vec<_> = EX3_INPUT_STR.lines().collect();
 
         let reactions = parse_reactions(&input_str);
         assert_eq!(required_ore_count(&reactions), 13312);
@@ -145,20 +200,7 @@ mod tests {
 
     #[test]
     fn ex4() {
-        let input_str: Vec<_> = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
-17 NVRVD, 3 JNWZP => 8 VPVL
-53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
-22 VJHF, 37 MNCFX => 5 FWMGM
-139 ORE => 4 NVRVD
-144 ORE => 7 JNWZP
-5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
-5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
-145 ORE => 6 MNCFX
-1 NVRVD => 8 CXFTF
-1 VJHF, 6 MNCFX => 4 RFSQX
-176 ORE => 6 VJHF"
-            .lines()
-            .collect();
+        let input_str: Vec<_> = EX4_INPUT_STR.lines().collect();
 
         let reactions = parse_reactions(&input_str);
         assert_eq!(required_ore_count(&reactions), 180697);
@@ -166,27 +208,33 @@ mod tests {
 
     #[test]
     fn ex5() {
-        let input_str: Vec<_> = "171 ORE => 8 CNZTR
-7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
-114 ORE => 4 BHXH
-14 VRPVC => 6 BMBT
-6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-5 BMBT => 4 WPTQ
-189 ORE => 9 KTJDG
-1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
-12 VRPVC, 27 CNZTR => 2 XDBXC
-15 KTJDG, 12 BHXH => 5 XCVML
-3 BHXH, 2 VRPVC => 7 MZWV
-121 ORE => 7 VRPVC
-7 XCVML => 6 RJRHP
-5 BHXH, 4 VRPVC => 5 LTCX"
-            .lines()
-            .collect();
+        let input_str: Vec<_> = EX5_INPUT_STR.lines().collect();
 
         let reactions = parse_reactions(&input_str);
         assert_eq!(required_ore_count(&reactions), 2210736);
+    }
+
+    #[test]
+    fn p2_ex3() {
+        let input_str: Vec<_> = EX3_INPUT_STR.lines().collect();
+
+        let reactions = parse_reactions(&input_str);
+        assert_eq!(get_fuel_count(&reactions), 82892753);
+    }
+
+    #[test]
+    fn p2_ex4() {
+        let input_str: Vec<_> = EX4_INPUT_STR.lines().collect();
+
+        let reactions = parse_reactions(&input_str);
+        assert_eq!(get_fuel_count(&reactions), 5586022);
+    }
+
+    #[test]
+    fn p2_ex5() {
+        let input_str: Vec<_> = EX5_INPUT_STR.lines().collect();
+
+        let reactions = parse_reactions(&input_str);
+        assert_eq!(get_fuel_count(&reactions), 460664);
     }
 }
