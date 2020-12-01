bb399f7606f8a54248f16bb397318c59f16e8195
Charles Pigott <charlespigott@googlemail.com>
2020-12-01 11:41:23 +0000

    Reformat with 2 spaces

--- 2020/src/day1.nim
+++ 2020/src/day1.nim
@@ -1,19 +1,19 @@
 import os
-import strutils
 import sequtils
+import strutils
 
-let inputData = open(paramStr(1)).readAll().strip().splitLines().map(parseInt)
+var inputData = open(paramStr(1)).readAll().strip().splitLines().map(parseInt)
 
 var matchedI = 0
 var matchedJ = 0
 
 block outer:
-    for i in low(inputData) .. high(inputData) - 1:
-        for j in i + 1 .. high(inputData):
-            if inputData[i] + inputData[j] == 2020:
-                matchedI = i
-                matchedJ = j
-                break outer
+  for i in low(inputData) .. high(inputData) - 1:
+    for j in i + 1 .. high(inputData):
+      if inputData[i] + inputData[j] == 2020:
+        matchedI = i
+        matchedJ = j
+        break outer
 
 let pairProduct = inputData[matchedI] * inputData[matchedJ]
 echo "Matched pair: ", $(pairProduct)
@@ -21,14 +21,14 @@ echo "Matched pair: ", $(pairProduct)
 var matchedK = 0
 
 block outer:
-    for i in low(inputData) .. high(inputData) - 2:
-        for j in i + 1 .. high(inputData) - 1:
-            for k in j + 1 .. high(inputData):
-                if inputData[i] + inputData[j] + inputData[k] == 2020:
-                    matchedI = i
-                    matchedJ = j
-                    matchedK = k
-                    break outer
+  for i in low(inputData) .. high(inputData) - 2:
+    for j in i + 1 .. high(inputData) - 1:
+      for k in j + 1 .. high(inputData):
+        if inputData[i] + inputData[j] + inputData[k] == 2020:
+          matchedI = i
+          matchedJ = j
+          matchedK = k
+          break outer
 
 let tripleProduct = inputData[matchedI] * inputData[matchedJ] * inputData[matchedK]
 echo "Matched triple: ", $(tripleProduct)
