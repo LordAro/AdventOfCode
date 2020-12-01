76a9da2d651cb7162257de762233b6f2c31be2d5
Charles Pigott <charlespigott@googlemail.com>
2020-12-01 11:25:45 +0000

    Minor tidyup

--- 2020/src/day1.nim
+++ 2020/src/day1.nim
@@ -9,24 +9,26 @@ var matchedJ = 0
 
 block outer:
     for i in low(inputData) .. high(inputData) - 1:
-        for j in i + 1.. high(inputData):
+        for j in i + 1 .. high(inputData):
             if inputData[i] + inputData[j] == 2020:
                 matchedI = i
                 matchedJ = j
                 break outer
 
-echo "Matched pair: ", $(inputData[matchedI] * inputData[matchedJ])
+let pairProduct = inputData[matchedI] * inputData[matchedJ]
+echo "Matched pair: ", $(pairProduct)
 
 var matchedK = 0
 
 block outer:
     for i in low(inputData) .. high(inputData) - 2:
-        for j in i + 1.. high(inputData) - 1:
-            for k in j + 1.. high(inputData):
+        for j in i + 1 .. high(inputData) - 1:
+            for k in j + 1 .. high(inputData):
                 if inputData[i] + inputData[j] + inputData[k] == 2020:
                     matchedI = i
                     matchedJ = j
                     matchedK = k
                     break outer
 
-echo "Matched triple: ", $(inputData[matchedI] * inputData[matchedJ] * inputData[matchedK])
+let tripleProduct = inputData[matchedI] * inputData[matchedJ] * inputData[matchedK]
+echo "Matched triple: ", $(tripleProduct)
