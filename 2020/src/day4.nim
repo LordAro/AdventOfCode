059e44ad04e8ebb2484780a44f541979f3054656
Charles Pigott <charlespigott@googlemail.com>
2020-12-04 07:39:25 +0000

    Single filter is twice as fast

--- 2020/src/day4.nim
+++ 2020/src/day4.nim
@@ -20,28 +20,32 @@ echo "Number of passports with correct fields: ", $(passportsWithCorrectFields.l
 
 let validPassports = passportsWithCorrectFields.filter(proc(rec: Table[string, string]): bool =
   let byr = parseInt(rec["byr"])
-  byr >= 1920 and byr <= 2002
-).filter(proc(rec: Table[string, string]): bool =
+  let byr_valid = byr >= 1920 and byr <= 2002
+
   let iyr = parseInt(rec["iyr"])
-  iyr >= 2010 and iyr <= 2020
-).filter(proc(rec: Table[string, string]): bool =
+  let iyr_valid = iyr >= 2010 and iyr <= 2020
+
   let eyr = parseInt(rec["eyr"])
-  eyr >= 2020 and eyr <= 2030
-).filter(proc(rec: Table[string, string]): bool =
+  let eyr_valid = eyr >= 2020 and eyr <= 2030
+
+  var hgt_valid = false # mutable state :(
   try:
-    let hgt = parseInt(rec["hgt"][0 .. ^3]) # assume suffix exists and trim it off
-    result = (rec["hgt"].endsWith("cm") and hgt >= 150 and hgt <= 193) or
+    let hgt = parseInt(rec["hgt"][0 .. ^3]) # trim off suffix
+    hgt_valid = (rec["hgt"].endsWith("cm") and hgt >= 150 and hgt <= 193) or
       (rec["hgt"].endsWith("in") and hgt >= 59 and hgt <= 76)
   except ValueError:
-    result = false
-).filter(proc(rec: Table[string, string]): bool =
-  rec["hcl"][0] == '#' and rec["hcl"][1 .. ^1].filter(proc(x: char): bool =
+    hgt_valid = false
+
+  let hcl_valid = rec["hcl"][0] == '#' and rec["hcl"][1 .. ^1].filter(proc(x: char): bool =
     (x >= 'a' and x <= 'f') or (x >= '0' and x <= '9')
   ).len == 6
-).filter(proc(rec: Table[string, string]): bool =
-  any(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], proc(col: string): bool = rec["ecl"] == col)
-).filter(proc(rec: Table[string, string]): bool =
-  rec["pid"].len == 9 and rec["pid"].filter(isDigit).len == 9
+
+  let ecl_valid = any(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], proc(col: string): bool =
+    rec["ecl"] == col
+  )
+
+  let pid_valid = rec["pid"].len == 9 and rec["pid"].filter(isDigit).len == 9
+  byr_valid and iyr_valid and eyr_valid and hgt_valid and hcl_valid and ecl_valid and pid_valid
 )
 
 echo "Number of valid passports: ", $(validPassports.len)
