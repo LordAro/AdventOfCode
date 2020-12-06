23ca1a0a31026aa0559d72062a6e36cb94df3e5d
Charles Pigott <charlespigott@googlemail.com>
2020-12-06 06:57:41 +0000

    Further simplification

--- 2020/src/day6.nim
+++ 2020/src/day6.nim
@@ -3,9 +3,7 @@ import strutils
 import sequtils
 import sets
 
-let inputData = open(paramStr(1)).readAll.split("\n\n").map(proc(answerList: string): seq[HashSet[char]] =
-  answerList.strip.split.mapIt(it.toHashSet)
-)
+let inputData = open(paramStr(1)).readAll.split("\n\n").mapIt(it.strip.split.mapIt(it.toHashSet))
 
 let p1 = inputData.mapIt(it.foldl(a + b)).mapIt(it.len).foldl(a + b)
 let p2 = inputData.mapIt(it.foldl(a * b)).mapIt(it.len).foldl(a + b)
