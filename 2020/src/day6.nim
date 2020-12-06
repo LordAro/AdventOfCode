828e90fac14b6e52424c144312804475e33ece41
Charles Pigott <charlespigott@googlemail.com>
2020-12-06 07:09:37 +0000

    Simplification

--- 2020/src/day6.nim
+++ 2020/src/day6.nim
@@ -1,3 +1,4 @@
+import math
 import os
 import strutils
 import sequtils
@@ -5,8 +6,8 @@ import sets
 
 let inputData = open(paramStr(1)).readAll.split("\n\n").mapIt(it.strip.split.mapIt(it.toHashSet))
 
-let p1 = inputData.mapIt(it.foldl(a + b)).mapIt(it.len).foldl(a + b)
-let p2 = inputData.mapIt(it.foldl(a * b)).mapIt(it.len).foldl(a + b)
+let p1 = inputData.mapIt(it.foldl(a + b).len).sum
+let p2 = inputData.mapIt(it.foldl(a * b).len).sum
 
 echo "Set union: ", $(p1)
 echo "Set intersection: ", $(p2)
