109891f6ac24738c74a6ce6b06b42633c329babd
Charles Pigott <charlespigott@googlemail.com>
2020-12-06 14:47:23 +0000

    Use bitsets instead of hashsets - 3.5ms -> 1.2ms

--- 2020/src/day6.nim
+++ 2020/src/day6.nim
@@ -2,9 +2,13 @@ import math
 import os
 import strutils
 import sequtils
-import sets
 
-let inputData = open(paramStr(1)).readAll.split("\n\n").mapIt(it.strip.split.mapIt(it.toHashSet))
+proc toBitset(s: string): set[char] =
+  result = {}
+  for c in s:
+    result.incl(c)
+
+let inputData = open(paramStr(1)).readAll.split("\n\n").mapIt(it.strip.split.mapIt(it.toBitset))
 
 let p1 = inputData.mapIt(it.foldl(a + b).len).sum
 let p2 = inputData.mapIt(it.foldl(a * b).len).sum
