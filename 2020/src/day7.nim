4bd0de773c27fceeb070ac9b4413490b2c90e1ca
Charles Pigott <charlespigott@googlemail.com>
2020-12-07 17:23:12 +0000

    Hacky state machine parser

--- 2020/src/day7.nim
+++ 2020/src/day7.nim
@@ -15,21 +15,34 @@ var
   bagMap: BagMap
 
 for line in paramStr(1).lines:
-  let lineWords = line.split
-  if lineWords.len < 2:
-    continue  # trailing newline
+  let idx = line.find(" bags contain ")
+  if idx == -1:
+    continue
+  let primaryColour = line[0 .. idx - 1]
 
-  let primaryColour = lineWords[0] & lineWords[1]
+  let slicePoint = idx + len(" bags contain ")
   bagMap[primaryColour] = @[]
-  for i, w in lineWords[.. ^3]:
-    if w[0] in '0' .. '9':
-      let num = int(w[0]) - int('0')
-      let containedColour = lineWords[i + 1] & lineWords[i + 2]
+  var inBag = 0
+  var num = 0
+  var containedColour = "";
+  for w in line[slicePoint .. ^3].split:
+    if inBag == 2:
+      containedColour &= " " & w
+
       containingBags.mgetOrPut(containedColour, @[]).add(primaryColour)
       bagMap[primaryColour].add((num, containedColour))
 
+      inBag = 0
+      num = 0
+    elif inBag == 1:
+      containedColour = w
+      inBag = 2
+    elif w[0] in '0' .. '9':
+      num = int(w[0]) - int('0')
+      inBag = 1
+
 var outerBags: HashSet[BagColour]
-var bagsToCheck = @["shinygold".BagColour]
+var bagsToCheck = @["shiny gold".BagColour]
 
 while bagsToCheck.len > 0:
   let col = bagsToCheck.pop
@@ -45,4 +58,4 @@ proc GetBagCount(bagMap : BagMap, col: BagColour): int =
   let containingBags = bagMap[col]
   containingBags.mapIt(it[0] * (GetBagCount(bagMap, it[1]) + 1)).sum
 
-echo "Bag count: ", $(GetBagCount(bagMap, "shinygold".BagColour))
+echo "Bag count: ", $(GetBagCount(bagMap, "shiny gold".BagColour))
