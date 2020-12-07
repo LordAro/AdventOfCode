18ab59b33c947c9893a67a2ee04e17f2e55c3589
Charles Pigott <charlespigott@googlemail.com>
2020-12-07 11:30:42 +0000

    Day7p2

--- 2020/src/day7.nim
+++ 2020/src/day7.nim
@@ -1,28 +1,35 @@
 import os
-import nre
+import strutils
 import sequtils
 import tables
+import math
 import sets
-
-let containingBagRE = re"[0-9] (\w+ \w+) bags?"
-let primaryColourRE = re"([a-z]+ [a-z]+)"
 
 type
   BagColour = string
   Bags = Table[BagColour, seq[BagColour]]
+  BagMap = Table[BagColour, seq[(int, BagColour)]]
 
-var containingBags: Bags
+var
+  containingBags: Bags
+  bagMap: BagMap
 
 for line in paramStr(1).lines:
-  let primaryColourMatch = find(line, primaryColourRE) # first 2 words are primary colour
-  if primaryColourMatch.isSome(): # trailing newline
-    let primaryColour = primaryColourMatch.get.captures[0]
-    let colours = findAll(line, containingBagRE).mapIt(find(it, primaryColourRE).get.captures[0]) # get rid of number and 'bag'
-    for c in colours:
-      containingBags.mgetOrPut(c, @[]).add(primaryColour)
+  let lineWords = line.split
+  if lineWords.len < 2:
+    continue  # trailing newline
+
+  let primaryColour = lineWords[0] & lineWords[1]
+  bagMap[primaryColour] = @[]
+  for i, w in lineWords[.. ^3]:
+    if w[0] in '0' .. '9':
+      let num = int(w[0]) - int('0')
+      let containedColour = lineWords[i + 1] & lineWords[i + 2]
+      containingBags.mgetOrPut(containedColour, @[]).add(primaryColour)
+      bagMap[primaryColour].add((num, containedColour))
 
 var outerBags: HashSet[BagColour]
-var bagsToCheck = @["shiny gold".BagColour]
+var bagsToCheck = @["shinygold".BagColour]
 
 while bagsToCheck.len > 0:
   let col = bagsToCheck.pop
@@ -31,3 +38,11 @@ while bagsToCheck.len > 0:
     outerBags.incl(c)
 
 echo "Number of outer bags: ", $(outerBags.len)
+
+proc GetBagCount(bagMap : BagMap, col: BagColour): int =
+  if col notin bagMap:
+    return 0
+  let containingBags = bagMap[col]
+  containingBags.mapIt(it[0] * (GetBagCount(bagMap, it[1]) + 1)).sum
+
+echo "Bag count: ", $(GetBagCount(bagMap, "shinygold".BagColour))
