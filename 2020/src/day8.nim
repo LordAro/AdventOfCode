ca65b2f28184769b0bff0c592bcaed3bc08f9c60
Charles Pigott <charlespigott@googlemail.com>
2020-12-08 21:03:26 +0000

    Alternate non-bruteforce d8p2 solution

--- 2020/src/day8.nim
+++ 2020/src/day8.nim
@@ -14,17 +14,16 @@ for line in paramStr(1).lines:
   let arg = parseInt(line[4..^1])
   initialProg.add((opcode, arg))
 
-proc RunProg(prog: Program): (bool, int) =
-  var seenIns: set[uint16] # upper limit for program size
+proc RunProg(prog: Program): (bool, int, set[uint16]) =
   var pc = 0
-  result = (true, 0)
+  result = (true, 0, {})
 
   while pc < prog.len():
-    if cast[uint16](pc) in seenIns:
+    if cast[uint16](pc) in result[2]:
       result[0] = false  # have looped
       break
     let ins = prog[pc]
-    seenIns.incl(cast[uint16](pc))
+    result[2].incl(cast[uint16](pc))
     case ins[0]:
       of nop:
         discard
@@ -37,15 +36,51 @@ proc RunProg(prog: Program): (bool, int) =
 let res = RunProg(initialProg)
 echo "Accumulator value: ", res[1]
 
-var modifiedProg = initialProg
-for i, ins in initialProg:
-  if ins[0] == acc:
-    continue
+let trace = res[2]
 
-  let newOpcode = (if ins[0] == nop: jmp else: nop)
-  modifiedProg[i][0] = newOpcode
-  let res = RunProg(modifiedProg)
-  if res[0]:
-    echo "Accumulator value after properly terminating: ", res[1]
+var potential_landing_spots: set[uint16]
+
+var i = initialProg.len()
+while true:
+  potential_landing_spots.incl(cast[uint16](i))
+  dec i
+
+  if initialProg[i][0] == jmp and initialProg[i][1] < 0:
     break
-  modifiedProg[i][0] = ins[0] # put it back
+
+let start = i
+var swapIdx = 0
+
+if cast[uint16](i) in trace:
+  swapIdx = i
+else:
+  while true:
+    dec i
+    if cast[uint16](i) in potential_landing_spots:
+      continue
+    elif initialProg[i][0] == nop:
+      if cast[uint16](i) in trace and cast[uint16](i + initialProg[i][1]) in potential_landing_spots:
+        swapIdx = i
+        break
+    elif initialProg[i][0] == jmp:
+      if cast[uint16](i) notin trace and
+        cast[uint16](i) notin potential_landing_spots and
+        cast[uint16](i + initialProg[i][1]) in potential_landing_spots:
+        var j = i - 1
+        while true:
+          if initialProg[j][0] == jmp:
+            break
+          dec j
+
+        if cast[uint16](j) in trace:
+          swapIdx = j
+          break
+        else:
+          for k in j + 1..i:
+            potential_landing_spots.incl(cast[uint16](k))
+          i = start
+
+var modifiedProg = initialProg
+modifiedProg[swapIdx][0] = (if modifiedProg[swapIdx][0] == nop: jmp else: nop)
+let res2 = RunProg(modifiedProg)
+echo "Accumulator value after properly terminating: ", (res2[0], res2[1])
