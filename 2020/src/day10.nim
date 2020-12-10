c6017a3fda24ceb68d0d63792a5f7de2e8f08991
Charles Pigott <charlespigott@googlemail.com>
2020-12-10 14:21:33 +0000

    Complete d10

--- 2020/src/day10.nim
+++ 2020/src/day10.nim
@@ -2,11 +2,9 @@ import algorithm
 import os
 import strutils
 
-#var inputData = @[16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
-var inputData = @[28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
-#var inputData: seq[int] = @[]
-#for line in paramStr(1).lines:
-#  inputData.add(parseInt(line))
+var inputData: seq[int] = @[]
+for line in paramStr(1).lines:
+  inputData.add(parseInt(line))
 inputData.sort()
 
 var
@@ -22,21 +20,22 @@ for i, v in inputData:
 
 echo "Sum of jolt differences: ", jolt1Diff, " * ", jolt3Diff, " = ", jolt1Diff * jolt3Diff
 
-var memo = newSeq[int](inputData.len())
+# Uses -1 to represent the 0th element (i.e. starting point)
+var memo = newSeq[int](inputData.len() + 1)
 
 proc CountCombinations(arr: seq[int], i: int): int =
-  if memo[i] != 0:
-    return memo[i]
-  echo i, " ", arr[i]
+  if memo[i + 1] != 0:
+    return memo[i + 1]
   if i == arr.high():
     result = 1
   else:
+    let val = (if i == -1: 0 else: arr[i])
     result = CountCombinations(arr, i + 1)
-    if i < arr.len() - 2 and arr[i + 2] <= arr[i] + 3:
+    if i < arr.len() - 2 and arr[i + 2] <= val + 3:
       result += CountCombinations(arr, i + 2)
-    if i < arr.len() - 3 and arr[i + 3] <= arr[i] + 3:
+    if i < arr.len() - 3 and arr[i + 3] <= val + 3:
       result += CountCombinations(arr, i + 3)
-  memo[i] = result
+  memo[i + 1] = result
   return result
 
-echo "Total possible arrangements: ", CountCombinations(inputData, 0)
+echo "Total possible arrangements: ", CountCombinations(inputData, -1)
