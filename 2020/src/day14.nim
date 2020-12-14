308b2618dc3f2f93fbbdd9d652e87140e3d9b107
Charles Pigott <charlespigott@googlemail.com>
2020-12-14 18:53:01 +0000

    Complete day14

--- 2020/src/day14.nim
+++ 2020/src/day14.nim
@@ -3,28 +3,65 @@ import os
 import strscans
 import strutils
 import tables
+import sets
 
-var mask0: int64 = 0b1111_1111_1111_1111_1111_1111_1111_1111_1111
-var mask1: int64 = 0
-var mem: Table[int64, int64]
+# Copied straight out of narimiran/itertools
+iterator product*[T](s: openArray[T], repeat: Positive): seq[T] =
+  ## Iterator yielding Cartesian products of ``s`` with itself, ``repeat`` number of times.
+  var counters = newSeq[int](repeat)
 
-for line in [
-"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
-"mem[8] = 11",
-"mem[7] = 101",
-"mem[8] = 0",
-]:
-#for line in paramStr(1).lines:
+  block outer:
+    while true:
+      var result = newSeq[T](repeat)
+      for i, cnt in counters:
+        result[i] = s[cnt]
+      yield result
+
+      var i = repeat - 1
+      while true:
+        inc counters[i]
+        if counters[i] == s.len:
+          counters[i] = 0
+          dec i
+        else: break
+        if i < 0:
+          break outer
+
+var
+  mask_str: string
+  mask0, mask1: int64
+  mem, mem2: Table[int64, int64]
+  bitTwiddles: seq[int]
+  memAddrMask: int64
+
+#for line in [
+#"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+#"mem[8] = 11",
+#"mem[7] = 101",
+#"mem[8] = 0",
+#]:
+#for line in [
+#"mask = 000000000000000000000000000000X1001X",
+#"mem[42] = 100",
+#"mask = 00000000000000000000000000000000X0XX",
+#"mem[26] = 1",
+#]:
+for line in paramStr(1).lines:
   if line.startsWith("mask = "):
-    let mask_str = line[7 .. ^1]
+    mask0 = 0b1111_1111_1111_1111_1111_1111_1111_1111_1111
+    mask1 = 0
+    mask_str = line[7 .. ^1]
+    bitTwiddles.setLen(0)
+    memAddrMask = 0
     for i, c in mask_str:
       let b = 36 - i - 1
       case c:
         of 'X':
-          discard
+          bitTwiddles.add(b)
         of '1':
           mask0.setBit(b)
           mask1.setBit(b)
+          memAddrMask.setBit(b)
         of '0':
           mask0.clearBit(b)
           mask1.clearBit(b)
@@ -33,14 +70,20 @@ for line in [
   else:
     var
       mem_addr, mem_value: int
-      mem_value64: int64
     if scanf(line, "mem[$i] = $i", mem_addr, mem_value):
-      mem_value_64 = mem_value
-      mem[mem_addr] = mem_value64.bitor(mask1).bitand(mask0)
-#      echo mem_value.toBin(36)
-#      echo mem_value64.bitor(mask1).bitand(mask0).toBin(36)
-#      echo ""
-      echo (mem_addr, mem_value, mem_value64.bitor(mask1).bitand(mask0))
+      mem[mem_addr] = cast[int64](mem_value).bitor(mask1).bitand(mask0)
+      var memAddrMasked = cast[int64](mem_addr).bitor(memAddrMask)
+
+      # Get all combinations of bit twiddling
+      for arr in product([0, 1], bit_twiddles.len):
+        var mem_addr_modif2 = memAddrMasked
+        for i, b in arr:
+          if b == 0:
+            mem_addr_modif2.clearBit(bit_twiddles[i])
+          else:
+            mem_addr_modif2.setBit(bit_twiddles[i])
+        mem2[mem_addr_modif2] = mem_value
+
     else:
       raise newException(ValueError, "Could not match mem address")
 
@@ -49,3 +92,8 @@ for val in mem.values:
   total_val += val
 
 echo "Total memory value: ", total_val
+
+total_val = 0
+for val in mem2.values:
+  total_val += val
+echo "Total memory value v2: ", total_val
