41be290c833417056505bf0432a36db6b5803512
Charles Pigott <charlespigott@googlemail.com>
2020-12-17 23:49:40 +0000

    Generics all the things

--- 2020/src/day17.nim
+++ 2020/src/day17.nim
@@ -4,52 +4,52 @@ import algorithm
 import sequtils
 
 type
-  Coord3D = tuple[x: int, y: int, z: int]
-  Coord4D = tuple[x: int, y: int, z: int, w: int]
-  Coord = Coord4D
-  CoordSeq = seq[int]
-  State3D = HashSet[Coord3D]
-  State4D = HashSet[Coord4D]
-  State = State4D
+  Coord[N: static int] = array[0 .. N - 1, int]
+  State[N: static int] = HashSet[Coord[N]]
+
+const deltas = @[-1, 0, 1]
 
 iterator getAllNeighbours(coord: Coord): Coord =
-  for x1 in coord.x - 1 .. coord.x + 1:
-    for y1 in coord.y - 1 .. coord.y + 1:
-      for z1 in coord.z - 1 .. coord.z + 1:
-        for w1 in coord.w - 1 .. coord.w + 1:
-          let n = (x1, y1, z1, w1)
-          if n == coord:
-            continue
-          yield (x1, y1, z1, w1)
-
+  for c in product(repeat(deltas, coord.len)):
+    var n = coord
+    for i, v in c:
+      n[i] += v
+    if n == coord:
+      continue
+    yield n
 
 proc countNeighbours(state: State, coord: Coord): int =
   for neighbour in getAllNeighbours(coord):
     if state.contains(neighbour):
       inc result
 
+proc runStep[N](inputState: State[N]): State[N] =
+  var
+    possibleActiveNeighbours: HashSet[Coord[N]]
+  for c in inputState:
+    for n in getAllNeighbours(c):
+      if n notin inputState:
+        possibleActiveNeighbours.incl(n)
+    if countNeighbours(inputState, c) in 2 .. 3:
+      result.incl(c)
+  for n in possibleActiveNeighbours:
+    if countNeighbours(inputState, n) == 3:
+      result.incl(n)
 
-var state: State
+var state3d: State[3]
+var state4d: State[4]
 
 var y = 0 # iterators don't have indexes? :(
 for line in paramStr(1).lines:
   for x, c in line:
     if c == '#':
-      state.incl((x, y, 0, 0))
+      state3d.incl([x, y, 0])
+      state4d.incl([x, y, 0, 0])
   inc y
 
 for cycle in 1 .. 6:
-  var newState: State
-  var possibleActiveNeighbours: HashSet[Coord]
-  for c in state:
-    for n in getAllNeighbours(c):
-      if n notin state:
-        possibleActiveNeighbours.incl(n)
-    if countNeighbours(state, c) in 2 .. 3:
-      newState.incl(c)
-  for n in possibleActiveNeighbours:
-    if countNeighbours(state, n) == 3:
-      newState.incl(n)
-  state = newState
+  state3d = runStep(state3d)
+  state4d = runStep(state4d)
 
-echo "Active cells: ", state.len
+echo "Active cells (3D): ", state3d.len
+echo "Active cells (4D): ", state4d.len
