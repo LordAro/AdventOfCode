5d6245c75ff869edda8d4d807418562f84b4f8f9
Charles Pigott <charlespigott@googlemail.com>
2020-12-23 09:34:56 +0000

    Optimisations

--- 2020/src/day22.nim
+++ 2020/src/day22.nim
@@ -1,14 +1,56 @@
 import algorithm
-import deques
+import hashes
 import math
 import os
 import sequtils
 import sets
 import strutils
 
-type
-  Hand = Deque[int]
-  HandList = seq[int]
+type Hand = seq[int]
+
+proc hash(handPair: (Hand, Hand)): Hash =
+  var h: Hash = 0
+  h = h !& hash(handPair[0])
+  h = h !& hash(handPair[1])
+  result = !$h
+
+proc playGame(deck1, deck2: Hand, shouldRecurse: static bool = false): (int, bool) =
+  var roundCache: HashSet[Hash]
+
+  let deckSize = deck1.len + deck2.len
+  var player1Deck = deck1
+  var player2Deck = deck2
+  while player1Deck.len > 0 and player2Deck.len > 0:
+    let handPairHash = hash((player1Deck, player2Deck))
+    if handPairHash in roundCache:
+      return (0, true)
+    roundCache.incl(handPairHash)
+
+    let
+      player1 = player1Deck[0]
+      player2 = player2Deck[0]
+    player1Deck.delete(0)
+    player2Deck.delete(0)
+    var player1Winner = false
+    if shouldRecurse and player1 <= player1Deck.len and player2 <= player2Deck.len:
+      let subgameP1Deck = player1Deck[0 .. player1 - 1]
+      let subgameP2Deck = player2Deck[0 .. player2 - 1]
+      let subgame = playGame(subgameP1Deck, subgameP2Deck, true)
+      player1Winner = subgame[1]
+    else:
+      player1Winner = player1 > player2
+
+    if player1Winner:
+      player1Deck.add(player1)
+      player1Deck.add(player2)
+    else:
+      player2Deck.add(player2)
+      player2Deck.add(player1)
+
+  (
+    (1 .. deckSize).toSeq.reversed.zip(player1Deck & player2Deck).mapIt(it[0] * it[1]).sum,
+    player1Deck.len > 0
+  )
 
 var
   initdeck1, initdeck2: Hand
@@ -21,53 +63,11 @@ for line in paramStr(1).lines:
   elif line == "":
     isPlayer2 = true
   elif isPlayer2:
-    initdeck2.addLast(parseInt(line))
+    initdeck2.add(parseInt(line))
     inc deckSize
   else:
-    initdeck1.addLast(parseInt(line))
+    initdeck1.add(parseInt(line))
     inc deckSize
-
-proc playGame(deck1, deck2: Hand, shouldRecurse: static bool = false): (int, bool) =
-  var roundCache: HashSet[(HandList, HandList)]
-
-  let deckSize = deck1.len + deck2.len
-  var player1Deck = deck1
-  var player2Deck = deck2
-  var i = 1
-  while player1Deck.len > 0 and player2Deck.len > 0:
-    #echo "Round: ", i, " ", player1Deck, player2Deck
-    #if shouldRecurse:
-    let handPair = (player1Deck.toSeq, player2Deck.toSeq)
-    if handPair in roundCache:
-      return (0, true)
-    roundCache.incl(handPair)
-
-    let
-      player1 = player1Deck.popFirst
-      player2 = player2Deck.popFirst
-    var player1Winner = false
-    if shouldRecurse and player1 <= player1Deck.len and player2 <= player2Deck.len:
-      let subgameP1Deck = player1Deck.toSeq[0 .. player1 - 1].toDeque
-      let subgameP2Deck = player2Deck.toSeq[0 .. player2 - 1].toDeque
-      #echo player1, " ", subgameP1Deck
-      #echo player2, " ", subgameP2Deck
-      let subgame = playGame(subgameP1Deck, subgameP2Deck, true)
-      player1Winner = subgame[1]
-    else:
-      player1Winner = player1 > player2
-
-    if player1Winner:
-      player1Deck.addLast(player1)
-      player1Deck.addLast(player2)
-    else:
-      player2Deck.addLast(player2)
-      player2Deck.addLast(player1)
-    inc i
-
-  (
-    (1 .. deckSize).toSeq.reversed.zip(player1Deck.toSeq & player2Deck.toSeq).mapIt(it[0] * it[1]).sum,
-    player1Deck.len > 0
-  )
 
 echo "Winning score: ", playGame(initdeck1, initdeck2)[0]
 echo "Winning recursive score: ", playGame(initdeck1, initdeck2, true)[0]
