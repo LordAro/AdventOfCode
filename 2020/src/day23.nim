ab9fa5ef20aecca43adc3dfa2730ba131c0fd372
Charles Pigott <charlespigott@googlemail.com>
2020-12-25 00:52:28 +0000

    Rewrite day23 to use data structure that means it'll terminate in a small amount of time

--- 2020/src/day23.nim
+++ 2020/src/day23.nim
@@ -1,64 +1,59 @@
-import lists
 import os
 import sequtils
 import strutils
-import times
+import tables
+#import nimprof
 
-#for c in [3,8,9,1,2,5,4,6,7]:
 let inputData = open(paramStr(1)).readAll.strip.mapIt(ord(it) - ord('0'))
 
-proc playCrabGame(startCups: seq[int], numCups: int, numRounds: int): DoublyLinkedRing[int] =
-  var
-    cups = initDoublyLinkedRing[int]()
+proc playCrabGame(startCups: seq[int], numCups: int, numRounds: int): Table[int, int] =
+  # Set up ring as a table
+  # Use a table so we've got O(1) searching
+  var cups: Table[int, int] # n pointing to first
+  for i in 1 .. startCups.high:
+    cups[startCups[i - 1]] = startCups[i]
+  var prev = startCups[startCups.high]
+  for i in startCups.len + 1 .. numCups:
+    cups[prev] = i
+    prev = i
+  cups[prev] = startCups[0] # create the ring
 
-  for c in startCups:
-    cups.append(c)
-  for c in startCups.len + 1 .. numCups:
-    cups.append(c)
-
-  var currentCup = cups.head
-  var pickUp: seq[DoublyLinkedNode[int]] # store the node, to save reallocations
+  var currentCup = startCups[0]
 
   for i in 1 .. numRounds:
-    if i mod 1000 == 0:
-      echo "Round ", i
-    for i in 1 .. 3:
-      pickUp.add(currentCup.next)
-      cups.remove(currentCup.next)
+    let pickUp1 = cups[currentCup]
+    let pickUp2 = cups[pickUp1]
+    let pickUp3 = cups[pickUp2]
+    let linkVal = cups[pickUp3]
+    cups[currentCup] = linkVal # link past the pickup
 
-    var destCupVal = currentCup.value - 1
-    if destCupVal == 0:
-      destCupVal = 9
-    while destCupVal in pickUp.mapIt(it.value):
-      dec destCupVal
-      if destCupVal == 0:
-        destCupVal = numCups # max val
-    let destCup = cups.find(destCupVal)
+    var destCup = currentCup - 1
+    if destCup == 0:
+      destCup = numCups
+    while destCup == pickUp1 or destCup == pickUp2 or destCup == pickUp3:
+      dec destCup
+      if destCup == 0:
+        destCup = numCups
 
-    while pickUp.len > 0:
-      let n = pickUp.pop
-      n.next = destCup.next
-      n.prev = destCup
-      destCup.next.prev = n
-      destCup.next = n
-    #let now1 = now()
-    #if i mod 1000 == 0:
-    #  echo "Find time: ", now() - now1
+    # Re-insert the pickup into the ring
+    let oldLinkVal = cups[destCup]
+    cups[destCup] = pickUp1
+    cups[pickUp3] = oldLinkVal
 
-    currentCup = currentCup.next
+    currentCup = cups[currentCup]
 
   return cups
 
 let p1Ring = playCrabGame(inputData, inputData.len, 100)
 
 var r: string
-var val = p1Ring.find(1).next
-while val.value != 1:
-  r &= chr(ord('0') + val.value)
-  val = val.next
+var val = p1Ring[1]
+while val != 1:
+  r &= chr(ord('0') + val)
+  val = p1Ring[val]
 
 echo "Cup labels after 100 rounds with 9 cups: ", r
 
 let p2Ring = playCrabGame(inputData, 1_000_000, 10_000_000)
-var oneVal = p2Ring.find(1)
-echo oneVal.next.value, oneVal.next.next.value
+var oneVal = p2Ring[1]
+echo "Cup product after lots of rounds with many cups: ", oneVal * p2Ring[oneVal]
