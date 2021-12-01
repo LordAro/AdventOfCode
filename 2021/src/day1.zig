ede4cd050cdfd088a50e6c4d1f6da4a04caac2dd
Charles Pigott <charlespigott@googlemail.com>
2021-12-01 22:44:05 +0000

    Rework to remove need for window

--- 2021/src/day1.zig
+++ 2021/src/day1.zig
@@ -4,6 +4,7 @@ pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const alloc = &arena.allocator;
+    const stdout = std.io.getStdOut().writer();
 
     var args_iter = std.process.args();
     _ = args_iter.skip(); // program name
@@ -15,20 +16,23 @@ pub fn main() anyerror!void {
     };
     defer input.close();
 
-    var p1_count: u32 = 0;
-    var p2_count: u32 = 0;
-    var cur_window = [_]u32{ 0, 0, 0 };
+    var lines = std.ArrayList(u32).init(alloc);
+    defer lines.deinit();
 
     var buf: [16]u8 = undefined;
     while (try input.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        const next = try std.fmt.parseInt(u32, line, 0);
-
-        p1_count += @boolToInt(next > cur_window[0] and cur_window[0] != 0); // Previous number
-
-        std.mem.rotate(u32, cur_window[0..], 1); // Extremely basic windowing system
-        p2_count += @boolToInt(next > cur_window[0] and cur_window[0] != 0); // Only need to check the differing value, not the actual sum
-        cur_window[0] = next;
+        try lines.append(try std.fmt.parseInt(u32, line, 10));
     }
-    std.log.info("Number of increases: {}", .{p1_count});
-    std.log.info("Number of 3-sum increases: {}", .{p2_count});
+
+    var p1_count: u32 = 0;
+    var p2_count: u32 = 0;
+    var i: usize = 0;
+    const len = lines.items.len;
+    while (i < len) : (i += 1) {
+        p1_count += @boolToInt(i < len - 1 and lines.items[i + 1] > lines.items[i]);
+        p2_count += @boolToInt(i < len - 3 and lines.items[i + 3] > lines.items[i]);
+    }
+
+    try stdout.print("Number of increases: {}\n", .{p1_count});
+    try stdout.print("Number of 3-sum increases: {}\n", .{p2_count});
 }
