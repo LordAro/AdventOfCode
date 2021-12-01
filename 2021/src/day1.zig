76a5982609eef8181c17267b691c42719849f5d7
Charles Pigott <charlespigott@googlemail.com>
2021-12-01 06:42:54 +0000

    Day 1

--- 2021/src/day1.zig
+++ 2021/src/day1.zig
@@ -12,7 +12,28 @@ pub fn main() anyerror!void {
         std.log.err("Could not open {s} due to: {s}", .{ input_file, err });
         return;
     };
-    const contents = file.reader().readAllAlloc(alloc, 10_000_000);
+    defer file.close();
+    var count: u32 = 0;
+    var previous: u32 = std.math.maxInt(u32);
 
-    std.log.info("File contents: {s}", .{contents});
+    var sum_count: u32 = 0;
+    var cur_window = [_]u32{ 0, 0, 0 };
+    var prev_sum: u32 = std.math.maxInt(u32);
+
+    var buf: [16]u8 = undefined;
+    while (try file.reader().readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const next = try std.fmt.parseInt(u32, line, 0);
+        if (next > previous) count += 1;
+
+        std.mem.rotate(u32, cur_window[0..], 1); // Extremely basic windowing system
+        cur_window[0] = next;
+        if (cur_window[0] != 0 and cur_window[1] != 0 and cur_window[2] != 0) {
+            const new_sum = cur_window[0] + cur_window[1] + cur_window[2];
+            if (new_sum > prev_sum) sum_count += 1;
+            prev_sum = new_sum;
+        }
+        previous = next;
+    }
+    std.log.info("Number of increases: {}", .{count});
+    std.log.info("Number of 3-sum increases: {}", .{sum_count});
 }
