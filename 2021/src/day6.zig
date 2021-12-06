b44eedc7c1e6aa7e61d44f73f8dda4e8e804a567
Charles Pigott <charlespigott@googlemail.com>
2021-12-06 09:33:39 +0000

    Day6p2

--- 2021/src/day6.zig
+++ 2021/src/day6.zig
@@ -16,29 +16,41 @@ pub fn main() anyerror!void {
     };
     defer input.close();
 
-    var lanterns = std.ArrayList(u8).init(alloc);
-    defer lanterns.deinit();
+    const lanternSpawnRate = 7;
+    const lanternInitialSpawn = 9;
+    const dayLimit = 256;
 
+    var spawnDays: [dayLimit]u64 = [_]u64{0} ** dayLimit;
+
+    var initial_input_count: u32 = 0;
     var buf: [4]u8 = undefined;
     while (try input.reader().readUntilDelimiterOrEof(&buf, ',')) |chr| {
-        try lanterns.append(try std.fmt.charToDigit(chr[0], 10));
+        initial_input_count += 1;
+        // Use chr[0] to make sure a trailing \n does not cause errors (and charToDigit instead of parseInt is better anyway)
+        const n = try std.fmt.charToDigit(chr[0], 10);
+
+        var i: usize = 0;
+        while (n + i * lanternSpawnRate < dayLimit) : (i += 1) {
+            spawnDays[n + i * lanternSpawnRate] += 1;
+        }
     }
 
     var day: u32 = 1;
-    while (day <= 80) : (day += 1) {
-        var newCount: u32 = 0;
-        for (lanterns.items) |*l| {
-            if (l.* == 0) {
-                newCount += 1;
-                l.* = 6;
-            } else {
-                l.* -= 1;
-            }
-        }
-        while (newCount > 0) : (newCount -= 1) {
-            try lanterns.append(8);
+    while (day < dayLimit) : (day += 1) {
+        var i: usize = 0;
+        while (day + lanternInitialSpawn + i * lanternSpawnRate < dayLimit) : (i += 1) {
+            spawnDays[day + lanternInitialSpawn + i * lanternSpawnRate] += spawnDays[day];
         }
     }
 
-    try stdout.print("Lanterns after 80 days: {}\n", .{lanterns.items.len});
+    var sum80: u64 = initial_input_count;
+    for (spawnDays[0..80]) |n| {
+        sum80 += n;
+    }
+    var sum256: u64 = initial_input_count;
+    for (spawnDays[0..256]) |n| {
+        sum256 += n;
+    }
+    try stdout.print("Number of lanternfish after 80 days: {}\n", .{sum80});
+    try stdout.print("After 256 days: {}\n", .{sum256});
 }
