b617e8114eca71626cbd1da0fde900fa1ff1ab24
Charles Pigott <charlespigott@googlemail.com>
2022-01-01 14:03:38 +0000

    2016 day19

---
+++ 2016/src/bin/day19.rs
@@ -0,0 +1,54 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn josephus2(n: usize) -> usize {
+    if n == 1 {
+        return 1;
+    } else if n % 2 == 0 {
+        return 2 * josephus2(n / 2) - 1;
+    } else {
+        return 2 * josephus2(n / 2) + 1; // f(2j+1), but we're using integer division
+    }
+}
+
+fn josephus_opposite(n: usize) -> usize {
+    let mut pow3 = 1;
+    // get biggest 3**m less than n
+    while pow3 * 3 < n {
+        pow3 *= 3;
+    }
+
+    if n == pow3 * 3 {
+        return pow3 * 3;
+    } else if n <= pow3 * 2 {
+        return n - pow3;
+    } else {
+        // 2(n - pow3) - pow3
+        return 2 * n - 3 * pow3;
+    }
+}
+
+fn main() {
+    if env::args().len() != 2 {
+        panic!("Incorrect number of arguments provided\n");
+    }
+
+    let num_elves = BufReader::new(File::open(&env::args().nth(1).unwrap()).unwrap())
+        .lines()
+        .nth(0)
+        .unwrap()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+
+    println!(
+        "Elf with all the presents (stealing next): {}",
+        josephus2(num_elves),
+    );
+
+    println!(
+        "Elf with all the presents (stealing opposite): {}",
+        josephus_opposite(num_elves)
+    );
+}
