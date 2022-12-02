ad0ee6ac4a6611dd2e9f083aac78e943b47fee24
Charles Pigott <charlespigott@googlemail.com>
2022-12-02 07:59:58 +0000

    d2: Convert to numbers earlier

--- 2022/src/day2.cpp
+++ 2022/src/day2.cpp
@@ -1,40 +1,33 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <vector>
 
 bool is_win(char opponent, char self)
 {
-	return (opponent == 'A' && self == 'Y') || (opponent == 'B' && self == 'Z') || (opponent == 'C' && self == 'X');
+	return (opponent == 0 && self == 1) || (opponent == 1 && self == 2) || (opponent == 2 && self == 0);
 }
 
 bool is_draw(char opponent, char self)
 {
-	return (opponent == 'A' && self == 'X') || (opponent == 'B' && self == 'Y') || (opponent == 'C' && self == 'Z');
+	return opponent == self;
 }
 
 char needed_move(char opponent, char outcome)
 {
 	switch (opponent) {
-		case 'A':
+		case 0:
 			switch (outcome) {
-				case 'X': return 'Z';
-				case 'Y': return 'X';
-				case 'Z': return 'Y';
+				case 0: return 2;
+				case 1: return 0;
+				case 2: return 1;
 			}
 			__builtin_unreachable();
-		case 'B':
+		case 1:
+			return outcome;
+		case 2:
 			switch (outcome) {
-				case 'X': return 'X';
-				case 'Y': return 'Y';
-				case 'Z': return 'Z';
-			}
-			__builtin_unreachable();
-		case 'C':
-			switch (outcome) {
-				case 'X': return 'Y';
-				case 'Y': return 'Z';
-				case 'Z': return 'X';
+				case 0: return 1;
+				case 1: return 2;
+				case 2: return 0;
 			}
 			__builtin_unreachable();
 	}
@@ -61,20 +54,23 @@ int main(int argc, char **argv)
 	int p1_score = 0;
 	int p2_score = 0;
 
-	char opponent_move;
-	char my_move;
-	while (input >> opponent_move, input >> my_move) { // mm, commas
-		p1_score += (my_move - 'X' + 1);
+	char opponent_move_ch;
+	char my_move_ch;
+	while (input >> opponent_move_ch >> my_move_ch) {
+		int opponent_move = opponent_move_ch - 'A';
+		int my_move = my_move_ch - 'X';
+
+		p1_score += my_move + 1;
 		if (is_win(opponent_move, my_move)) {
 			p1_score += 6;
 		} else if (is_draw(opponent_move, my_move)) {
 			p1_score += 3;
 		}
 
-		const char needed_outcome = my_move;
-		char my_move2 = needed_move(opponent_move, needed_outcome);
-		p2_score += (my_move2 - 'X' + 1);
-		p2_score += (needed_outcome - 'X') * 3;
+		const int needed_outcome = my_move;
+		int my_move2 = needed_move(opponent_move, needed_outcome);
+		p2_score += my_move2 + 1;
+		p2_score += needed_outcome * 3;
 	}
 	std::cout << "Total score (p1 definition): " << p1_score << '\n';
 	std::cout << "Total score (p2 definition): " << p2_score << '\n';
