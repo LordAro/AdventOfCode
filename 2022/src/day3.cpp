6a2512b23c0f8afb9f6d801f0a7944bb9d2539f8
Charles Pigott <charlespigott@googlemail.com>
2022-12-03 09:51:10 +0000

    d3: Optimise by using bitsets
    Solution inspired by https://github.com/dholmes215/adventofcode-cpp/blob/main/src/2022/day03.cpp

--- 2022/src/day3.cpp
+++ 2022/src/day3.cpp
@@ -1,14 +1,31 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <string_view>
 
-int priority(char c)
+#include <strings.h>
+
+uint64_t priority(char c)
 {
-	if (std::isupper(c)) {
+	if (c >= 'A' && c <= 'Z') {
 		return c - 'A' + 1 + 26;
 	}
 	return c - 'a' + 1;
+}
+
+uint64_t priority_mask(std::string_view items)
+{
+	uint64_t out = 0;
+	for (char c : items) {
+		out |= 1ULL << priority(c);
+	}
+	return out;
+}
+
+int priority_from_mask(uint64_t mask)
+{
+	//std::bit_width
+	return ffsll(static_cast<int64_t>(mask)) - 1;
 }
 
 int main(int argc, char **argv)
@@ -32,34 +49,19 @@ int main(int argc, char **argv)
 
 	int priority_sum = 0;
 	for (const auto &line : bags) {
-		size_t half = line.length() / 2;
+		std::string_view line_view = line;
+		size_t half = line_view.length() / 2;
 
-		std::string compartment1(line.begin(), line.begin() + half);
-		std::string compartment2(line.begin() + half, line.end());
-		std::sort(compartment1.begin(), compartment1.end());
-		std::sort(compartment2.begin(), compartment2.end());
+		const auto compartment1 = line_view.substr(0, half);
+		const auto compartment2 = line_view.substr(half);
 
-		std::string intersection;
-		std::set_intersection(compartment1.begin(), compartment1.end(), compartment2.begin(), compartment2.end(), std::back_inserter(intersection));
-		priority_sum += priority(intersection[0]); // only 1!
+		priority_sum += priority_from_mask(priority_mask(compartment1) & priority_mask(compartment2));
 	}
 	std::cout << "Duplicate item priority sum: " << priority_sum << '\n';
 
 	int badge_priority_sum = 0;
 	for (size_t i = 0; i < bags.size(); i += 3) {
-		std::string bag1 = bags[i];
-		std::string bag2 = bags[i + 1];
-		std::string bag3 = bags[i + 2];
-		std::sort(bag1.begin(), bag1.end());
-		std::sort(bag2.begin(), bag2.end());
-		std::sort(bag3.begin(), bag3.end());
-
-		std::string bag12;
-		std::set_intersection(bag1.begin(), bag1.end(), bag2.begin(), bag2.end(), std::back_inserter(bag12));
-
-		std::string intersection;
-		std::set_intersection(bag12.begin(), bag12.end(), bag3.begin(), bag3.end(), std::back_inserter(intersection));
-		badge_priority_sum += priority(intersection[0]);
+		badge_priority_sum += priority_from_mask(priority_mask(bags[i]) & priority_mask(bags[i + 1]) & priority_mask(bags[i + 2]));
 	}
 	std::cout << "Badge item priority sum: " << badge_priority_sum << '\n';
 }
