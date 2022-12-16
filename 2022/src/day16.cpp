7a0bb7b24c4bd22f3e1468cb2ac08b97fbf75aa9
Charles Pigott <charlespigott@googlemail.com>
2022-12-16 18:22:54 +0000

    d16: Use integers instead of passing strings around everywhere

--- 2022/src/day16.cpp
+++ 2022/src/day16.cpp
@@ -7,10 +7,40 @@
 #include <map>
 #include <vector>
 
+// To save passing strings around everywhere.
+class ValveName {
+	int i;
+public:
+	ValveName() = default;
+	ValveName(const std::string_view s) : i((s[0] << 8) + s[1]) {}
+
+	bool operator==(const ValveName &other) const
+	{
+		return this->i == other.i;
+	}
+
+	bool operator!=(const ValveName &other) const
+	{
+		return this->i != other.i;
+	}
+
+	bool operator<(const ValveName &other) const
+	{
+		return this->i < other.i;
+	}
+
+	std::string str() const
+	{
+		char c1 = i >> 8;
+		char c2 = i & 0xf;
+		return std::string{c1, c2};
+	}
+};
+
 struct Valve {
-	std::string name;
+	ValveName name;
 	int flow_rate;
-	std::vector<std::string> links;
+	std::vector<ValveName> links;
 
 	int total_flow_at_time(int t) const
 	{
@@ -21,23 +51,23 @@ struct Valve {
 // debugging
 std::ostream &operator<<(std::ostream &os, const Valve &v)
 {
-	os << v.name << " (" << v.flow_rate << ") - ";
-	for (const auto &l : v.links) os << l << ", ";
+	os << v.name.str() << " (" << v.flow_rate << ") - ";
+	for (const auto &l : v.links) os << l.str() << ", ";
 	return os;
 }
 
-std::vector<std::string> split_by(const std::string &s, std::string_view delim)
+std::vector<ValveName> split_by(const std::string &s, std::string_view delim)
 {
-	std::vector<std::string> res;
+	std::vector<ValveName> res;
 
 	size_t last = 0;
 	size_t next = 0;
 	while ((next = s.find(delim, last)) != std::string::npos) {
-		res.push_back(s.substr(last, next - last));
+		res.emplace_back(s.substr(last, next - last));
 		last = next + delim.size();
 	}
 	{
-		res.push_back(s.substr(last));
+		res.emplace_back(s.substr(last));
 	}
 	return res;
 }
@@ -45,8 +75,8 @@ std::vector<std::string> split_by(const std::string &s, std::string_view delim)
 std::istream &operator>>(std::istream &is, Valve &valve)
 {
 	std::string s;
-	is >> s; // Valve
-	is >> valve.name;
+	is >> s >> s; // Valve <name>
+	valve.name = ValveName(s);
 	is >> s >> s; // has flow
 	is.ignore(6); // ' rate='
 	is >> valve.flow_rate;
@@ -59,10 +89,10 @@ std::istream &operator>>(std::istream &is, Valve &valve)
 	return is;
 }
 
-int get_path_cost(const std::map<std::string, Valve> &valves, const std::string &source, const std::string &dest)
+int get_path_cost(const std::map<ValveName, Valve> &valves, const ValveName &source, const ValveName &dest)
 {
-	std::set<std::string> visited;
-	std::deque<std::pair<std::string, int>> to_visit;
+	std::set<ValveName> visited;
+	std::deque<std::pair<ValveName, int>> to_visit;
 	to_visit.push_back({source, 0});
 
 	while (!to_visit.empty()) {
@@ -83,13 +113,13 @@ int get_path_cost(const std::map<std::string, Valve> &valves, const std::string 
 // build up list of (non-zero) valves
 // sort by
 
-int dfs(const std::map<std::string, Valve> &valves, const std::map<std::pair<std::string, std::string>, int> &route_costs, const std::vector<std::pair<std::string, int>> &route)
+int dfs(const std::map<ValveName, Valve> &valves, const std::map<std::pair<ValveName, ValveName>, int> &route_costs, const std::vector<std::pair<ValveName, int>> &route)
 {
 	const auto [cur_valve, cur_time] = route.back();
 	int max_route_cost = 0;
 	for (const auto &[n, valve] : valves) {
 		if (valve.flow_rate == 0) continue;
-		if (std::find_if(route.begin(), route.end(), [v_name=valve.name](const std::pair<std::string, int> &p) { return p.first == v_name; }) != route.end()) continue;
+		if (std::find_if(route.begin(), route.end(), [v_name=valve.name](const std::pair<ValveName, int> &p) { return p.first == v_name; }) != route.end()) continue;
 		int remaining_time = 30 - cur_time;
 		int route_cost = route_costs.at({cur_valve, valve.name});
 		if (route_cost > remaining_time) continue;
@@ -139,21 +169,21 @@ int main(int argc, char **argv)
 	std::stringstream ex_input(example_input);
 
 
-	std::map<std::string, Valve> valves;
+	std::map<ValveName, Valve> valves;
 	for (Valve v; input >> v; ) {
 		valves[v.name] = v;
 	}
 
-	std::map<std::pair<std::string, std::string>, int> route_costs;
+	std::map<std::pair<ValveName, ValveName>, int> route_costs;
 	for (const auto &source : valves) {
-		if (source.second.flow_rate == 0 && source.first != "AA") continue;
+		if (source.second.flow_rate == 0 && source.first != ValveName("AA")) continue;
 		for (const auto &dest : valves) {
 			if (source.first == dest.first || dest.second.flow_rate == 0) continue;
 			route_costs[{source.first, dest.first}] = get_path_cost(valves, source.first, dest.first);
 		}
 	}
 
-	std::vector<std::pair<std::string, int>> route;
-	route.push_back({"AA", 0});
+	std::vector<std::pair<ValveName, int>> route;
+	route.push_back({ValveName("AA"), 0});
 	std::cout << "Maximum flow rate: " << dfs(valves, route_costs, route) << '\n';
 }
