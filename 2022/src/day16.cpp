cdf0544348b3064a972f845816262b7321478610
Charles Pigott <charlespigott@googlemail.com>
2022-12-18 11:28:58 +0000

    Day 16
    Takes 5 minutes. Nice.

--- 2022/src/day16.cpp
+++ 2022/src/day16.cpp
@@ -42,9 +42,14 @@ struct Valve {
 	int flow_rate;
 	std::vector<ValveName> links;
 
-	int total_flow_at_time(int t) const
+	int total_flow_at_time(int max, int t) const
 	{
-		return this->flow_rate * (30 - t);
+		return this->flow_rate * (max - t);
+	}
+
+	bool operator==(const Valve &other) const
+	{
+		return this->name == other.name;
 	}
 };
 
@@ -109,35 +114,129 @@ int get_path_cost(const std::map<ValveName, Valve> &valves, const ValveName &sou
 	// all nodes are reachable
 	__builtin_unreachable();
 }
-// build map of cost from a valve to any (nonzero) valve
-// build up list of (non-zero) valves
-// sort by
 
 int dfs(const std::map<ValveName, Valve> &valves, const std::map<std::pair<ValveName, ValveName>, int> &route_costs, const std::vector<std::pair<ValveName, int>> &route)
 {
+	const int TOTAL_TIME = 30;
 	const auto [cur_valve, cur_time] = route.back();
 	int max_route_cost = 0;
 	for (const auto &[n, valve] : valves) {
 		if (valve.flow_rate == 0) continue;
 		if (std::find_if(route.begin(), route.end(), [v_name=valve.name](const std::pair<ValveName, int> &p) { return p.first == v_name; }) != route.end()) continue;
-		int remaining_time = 30 - cur_time;
+		int remaining_time = TOTAL_TIME - cur_time;
 		int route_cost = route_costs.at({cur_valve, valve.name});
 		if (route_cost > remaining_time) continue;
 
 		auto new_route = route;
-		new_route.push_back({valve.name, cur_time + route_cost + 1}); // costs 1 min to open valve
+		new_route.emplace_back(valve.name, cur_time + route_cost + 1); // costs 1 min to open valve
 		max_route_cost = std::max(max_route_cost, dfs(valves, route_costs, new_route));
 	}
 
 	// no more possible routes
 	if (max_route_cost == 0) {
 		int flow_total = 0;
-//		std::cout << "Route:\n";
 		for (const auto &[v, n] : route) {
-//			std::cout << v << " at " << n << " = " << valves.at(v).total_flow_at_time(n) << '\n';
-			flow_total += valves.at(v).total_flow_at_time(n);
+			flow_total += valves.at(v).total_flow_at_time(TOTAL_TIME, n);
 		}
 		return flow_total;
+	}
+	return max_route_cost;
+}
+
+std::vector<Valve> get_ordered_possible_moves(const std::map<std::pair<ValveName, ValveName>, int> &route_costs, const int total_time, std::vector<Valve> valves, ValveName cur_pos, int cur_time)
+{
+	for (auto it = valves.begin(); it != valves.end(); ) {
+		int route_cost = route_costs.at({cur_pos, it->name});
+		if (route_cost > (total_time - cur_time)) {
+			it = valves.erase(it);
+		} else {
+			++it;
+		}
+	}
+
+	// sort valves by maximum possible flow rate
+	std::sort(valves.rbegin(), valves.rend(), [&](const Valve &a, const Valve &b){
+		int route_cost_a = route_costs.at({cur_pos, a.name});
+		int route_cost_b = route_costs.at({cur_pos, b.name});
+
+		int flow_total_a = a.total_flow_at_time(total_time, cur_time - route_cost_a - 1);
+		int flow_total_b = b.total_flow_at_time(total_time, cur_time - route_cost_b - 1);
+		return flow_total_a < flow_total_b;
+	});
+	return valves;
+}
+
+using RouteItem = std::pair<ValveName, int>; // location, time
+using Route = std::vector<RouteItem>;
+
+int dfs_with_elephant(const std::map<ValveName, Valve> &valves, const std::map<std::pair<ValveName, ValveName>, int> &route_costs, const std::pair<Route, Route> &routes)
+{
+	const int TOTAL_TIME = 26;
+
+	std::vector<Valve> unused_valves;
+	for (const auto &[_, valve] : valves) {
+		if (valve.flow_rate == 0) continue;
+		if (std::find_if(routes.first.begin(), routes.first.end(),
+				[v_name=valve.name](const std::pair<ValveName, int> &p) { return p.first == v_name; }) != routes.first.end()
+		) {
+			continue;
+		}
+		if (std::find_if(routes.second.begin(), routes.second.end(),
+				[v_name=valve.name](const std::pair<ValveName, int> &p) { return p.first == v_name; }) != routes.second.end()
+		) {
+			continue;
+		}
+		unused_valves.push_back(valve);
+	}
+
+	const auto [cur_valve, cur_time] = routes.first.back();
+	const auto [ele_valve, ele_time] = routes.second.back();
+
+	const auto possible_moves = get_ordered_possible_moves(route_costs, TOTAL_TIME, unused_valves, cur_valve, cur_time);
+	const auto possible_ele_moves = get_ordered_possible_moves(route_costs, TOTAL_TIME, unused_valves, ele_valve, ele_time);
+
+	int max_route_cost = 0;
+
+	if (possible_moves.empty() && possible_ele_moves.empty()) {
+		// no more possible routes
+		int flow_total = 0;
+		for (const auto &[v, n] : routes.first) {
+			flow_total += valves.at(v).total_flow_at_time(TOTAL_TIME, n);
+		}
+		for (const auto &[v, n] : routes.second) {
+			flow_total += valves.at(v).total_flow_at_time(TOTAL_TIME, n);
+		}
+		return flow_total;
+	} else if (possible_moves.empty() && !possible_ele_moves.empty()) {
+		for (const auto &next_valve : possible_ele_moves) {
+			auto new_routes = routes;
+			int route_cost = route_costs.at({ele_valve, next_valve.name});
+			new_routes.second.emplace_back(next_valve.name, ele_time + route_cost + 1); // costs 1 min to open valve
+			max_route_cost = std::max(max_route_cost, dfs_with_elephant(valves, route_costs, new_routes));
+		}
+	} else if (possible_ele_moves.empty() && !possible_moves.empty()) {
+		for (const auto &next_valve : possible_moves) {
+			auto new_routes = routes;
+			int route_cost = route_costs.at({cur_valve, next_valve.name});
+			new_routes.first.emplace_back(next_valve.name, cur_time + route_cost + 1); // costs 1 min to open valve
+			max_route_cost = std::max(max_route_cost, dfs_with_elephant(valves, route_costs, new_routes));
+		}
+	} else {
+		for (const auto &next_valve : possible_moves) {
+			for (const auto &next_ele_valve : possible_ele_moves) {
+				if (next_valve == next_ele_valve) {
+					continue; // TODO: something else...?
+				}
+
+				int route_cost = route_costs.at({cur_valve, next_valve.name});
+				int route_cost_ele = route_costs.at({ele_valve, next_ele_valve.name});
+
+				auto new_routes = routes;
+				new_routes.first.emplace_back(next_valve.name, cur_time + route_cost + 1); // costs 1 min to open valve
+				new_routes.second.emplace_back(next_ele_valve.name, ele_time + route_cost_ele + 1); // costs 1 min to open valve
+				max_route_cost = std::max(max_route_cost, dfs_with_elephant(valves, route_costs, new_routes));
+			}
+		}
 	}
 	return max_route_cost;
 }
@@ -184,6 +283,11 @@ int main(int argc, char **argv)
 	}
 
 	std::vector<std::pair<ValveName, int>> route;
-	route.push_back({ValveName("AA"), 0});
+	route.emplace_back(ValveName("AA"), 0);
 	std::cout << "Maximum flow rate: " << dfs(valves, route_costs, route) << '\n';
+
+	std::pair<Route, Route> route_with_ele;
+	route_with_ele.first.emplace_back(ValveName("AA"), 0);
+	route_with_ele.second.emplace_back(ValveName("AA"), 0); // duplicate is fine, as AA doesn't have a flow
+	std::cout << "Maximum flow rate with an elephant helping: " << dfs_with_elephant(valves, route_costs, route_with_ele) << '\n';
 }
