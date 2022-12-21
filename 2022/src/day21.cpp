5ad4331af4bed29c669faf9fd3c18d3c7de49917
Charles Pigott <charlespigott@googlemail.com>
2022-12-21 18:57:30 +0000

    d21: Remove most of the string parsing

--- 2022/src/day21.cpp
+++ 2022/src/day21.cpp
@@ -1,34 +1,59 @@
 #include <algorithm>
+#include <charconv>
 #include <fstream>
 #include <iostream>
 #include <map>
 #include <string_view>
+#include <variant>
+
+using MonkeyName = uint32_t;
+MonkeyName parse_name(const std::string_view n)
+{
+	return n[0] << 24 | n[1] << 16 | n[2] << 8 | n[3];
+}
+
+std::string get_name(MonkeyName monkey)
+{
+	char c1 = (monkey >> 24) & 0xff;
+	char c2 = (monkey >> 16) & 0xff;
+	char c3 = (monkey >> 8) & 0xff;
+	char c4 = monkey & 0xff;
+	return std::string() + c1 + c2 + c3 + c4;
+}
+
+using Operation = std::tuple<MonkeyName, char, MonkeyName>;
+
+struct Monkey {
+	Monkey(MonkeyName name, int64_t number) : name(name), v(number) {}
+	Monkey(MonkeyName name, Operation op) : name(name), v(op) {}
+	MonkeyName name;
+	std::variant<int64_t, Operation> v;
+};
 
 bool is_number(const std::string_view str)
 {
 	return std::all_of(str.begin(), str.end(), ::isdigit);
 }
 
-int64_t monkey_number(const std::map<std::string, std::string> &monkeys, const std::string_view monkey)
+int64_t monkey_number(const std::map<MonkeyName, Monkey> &monkeys, const MonkeyName monkey)
 {
-	const auto &value = monkeys.at(std::string(monkey)); // :(
-	if (is_number(value)) {
-		return std::stoi(value);
+	const auto &value = monkeys.at(monkey);
+	if (std::holds_alternative<int64_t>(value.v)) {
+		return std::get<int64_t>(value.v);
 	}
 
-	std::string_view sv = value;
-	char op = sv[5];
-	std::string_view monkey1 = sv.substr(0, 4);
-	std::string_view monkey2 = sv.substr(7);
+	auto [monkey1, op, monkey2] = std::get<Operation>(value.v);
+	auto monkey1_num = monkey_number(monkeys, monkey1);
+	auto monkey2_num = monkey_number(monkeys, monkey2);
 	switch (op) {
 		case '+':
-			return monkey_number(monkeys, monkey1) + monkey_number(monkeys, monkey2);
+			return monkey1_num + monkey2_num;
 		case '-':
-			return monkey_number(monkeys, monkey1) - monkey_number(monkeys, monkey2);
+			return monkey1_num - monkey2_num;
 		case '*':
-			return monkey_number(monkeys, monkey1) * monkey_number(monkeys, monkey2);
+			return monkey1_num * monkey2_num;
 		case '/':
-			return monkey_number(monkeys, monkey1) / monkey_number(monkeys, monkey2);
+			return monkey1_num / monkey2_num;
 		default:
 			__builtin_unreachable();
 	}
@@ -46,12 +71,21 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	std::map<std::string, std::string> monkeys;
+	std::map<MonkeyName, Monkey> monkeys;
 	for (std::string line; std::getline(input, line); ) {
-		std::string name = line.substr(0, 4);
-		std::string operation = line.substr(6);
-		monkeys[name] = operation;
+		std::string_view line_sv = line;
+		MonkeyName name = parse_name(line_sv.substr(0, 4));
+		std::string_view value = line.substr(6); // skips ': '
+		int64_t number;
+		const auto result = std::from_chars(value.data(), value.data() + value.size(), number);
+		if (result.ec == std::errc()) {
+			monkeys.try_emplace(name, name, number);
+		} else {
+			MonkeyName monkey1 = parse_name(value.substr(0, 4));
+			char op = value[5];
+			MonkeyName monkey2 = parse_name(value.substr(7));
+			monkeys.try_emplace(name, name, Operation{monkey1, op, monkey2});
+		}
 	}
 
-	std::cout << "Number yelled by monkey 'root': " << monkey_number(monkeys, "root") << '\n';
-}
+	std::cout << "Number yelled by monkey 'root': " << monkey_number(monkeys, parse_name("root")) << '\n';
