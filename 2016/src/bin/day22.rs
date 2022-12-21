86d5bf9fe580bdff1512922d0a5d8a61597978c0
Charles Pigott <charlespigott@googlemail.com>
2022-12-21 10:10:24 +0000

    y2016d16: Some minor work on p2

--- 2016/src/bin/day22.rs
+++ 2016/src/bin/day22.rs
@@ -3,20 +3,98 @@ extern crate regex;
 
 use itertools::iproduct;
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
+type Coord = (usize, usize);
+
+#[derive(Debug, Clone, Copy)]
 struct StorageNode {
     total: usize,
     used: usize,
+    orig_pos: Coord,
 }
 
 impl StorageNode {
     fn avail(&self) -> usize {
         self.total - self.used
     }
+}
+
+type Filesystem = HashMap<Coord, StorageNode>;
+type FSHashObj = (Coord, Coord); // current pos, orig_pos
+
+fn filesystem_hashobj(fs: &Filesystem) -> Vec<FSHashObj> {
+    fs.iter().map(|(pos, node)| (*pos, node.orig_pos)).collect()
+}
+
+fn fs_str(fs: &Filesystem) -> String {
+    fs.iter()
+        .map(|(pos, node)| format!("({:?},{:?})", pos, node.orig_pos))
+        .collect()
+}
+
+fn is_viable_copy(a: &StorageNode, b: &StorageNode) -> bool {
+    a.used > 0 && b.avail() > a.used
+}
+
+fn adj(pos: &Coord) -> [Coord; 4] {
+    [
+        // nasty, but guaranteed to not be in the resulting map
+        (pos.0, pos.1.overflowing_sub(1).0),
+        (pos.0, pos.1 + 1),
+        (pos.0.overflowing_sub(1).0, pos.1),
+        (pos.0 + 1, pos.1),
+    ]
+}
+
+fn move_data(
+    cache: &mut HashSet<Vec<FSHashObj>>,
+    fs: &Filesystem,
+    target_data_pos: Coord,
+    move_count: usize,
+) -> usize {
+    if fs.get(&(0, 0)).unwrap().orig_pos == target_data_pos {
+        return move_count;
+    }
+
+    let fshash = filesystem_hashobj(fs);
+    if cache.contains(&fshash) {
+        println!("Seen {:?} before, skipping", fs_str(fs));
+        return 1 << 32;
+    }
+    cache.insert(fshash);
+
+    let possible_moves = fs
+        .iter()
+        .flat_map(|posnode_a| {
+            adj(posnode_a.0)
+                .iter()
+                .filter(|&pos_b| fs.contains_key(pos_b))
+                .map(|&pos_b| (*posnode_a.0, pos_b))
+                .collect::<Vec<_>>()
+        })
+        .filter(|(pos_a, pos_b)| is_viable_copy(fs.get(pos_a).unwrap(), fs.get(pos_b).unwrap()))
+        .collect::<Vec<_>>();
+    //println!("{:?}", possible_moves);
+
+    possible_moves
+        .iter()
+        .map(|mv| {
+            let mut new_fs = fs.clone();
+            let n0 = *new_fs.get(&mv.0).unwrap();
+            let n1 = *new_fs.get(&mv.1).unwrap();
+            new_fs.insert(mv.0, n1);
+            new_fs.insert(mv.1, n0);
+            println!("{:?} -> {:?}", fs_str(fs), fs_str(&new_fs));
+            (new_fs, mv)
+        })
+        .map(|(new_fs, _)| move_data(cache, &new_fs, target_data_pos, move_count + 1))
+        .min()
+        .unwrap()
 }
 
 fn main() {
@@ -37,30 +115,50 @@ fn main() {
         Regex::new(r".*-x([0-9]+)-y([0-9]+)\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)T\s+([0-9]+)%")
             .unwrap();
 
-    let input = BufReader::new(File::open(&env::args().nth(1).unwrap()).unwrap())
+    let initial_filesystem = BufReader::new(File::open(&env::args().nth(1).unwrap()).unwrap())
         .lines()
         .filter_map(|li| {
             let l = li.unwrap();
             grid_re.captures(&l).map(|caps| {
+                let pos = (
+                    caps.at(1).unwrap().parse::<usize>().unwrap(),
+                    caps.at(2).unwrap().parse::<usize>().unwrap(),
+                );
                 (
-                    (
-                        caps.at(1).unwrap().parse::<usize>().unwrap(),
-                        caps.at(2).unwrap().parse::<usize>().unwrap(),
-                    ),
+                    pos,
                     StorageNode {
                         total: caps.at(3).unwrap().parse::<usize>().unwrap(),
                         used: caps.at(4).unwrap().parse::<usize>().unwrap(),
+                        orig_pos: pos,
                     },
                 )
             })
         })
-        .collect::<Vec<_>>();
+        .collect::<Filesystem>();
 
-    let valid_nodes = iproduct!(input.iter(), input.iter())
+    let valid_nodes = iproduct!(initial_filesystem.iter(), initial_filesystem.iter())
         .filter(|((pos_a, node_a), (pos_b, node_b))| {
-            pos_a != pos_b && node_a.used > 0 && node_b.avail() > node_a.used
+            pos_a != pos_b && is_viable_copy(node_a, node_b)
         })
         .count();
 
     println!("Valid nodes: {}", valid_nodes);
+
+    let target_data_position = *initial_filesystem
+        .keys()
+        .filter(|(_, y)| *y == 0)
+        .max_by_key(|(x, _)| x)
+        .unwrap();
+
+    let mut fs_state_cache = HashSet::new();
+    let minimum_number_of_moves = move_data(
+        &mut fs_state_cache,
+        &initial_filesystem,
+        target_data_position,
+        1,
+    );
+    println!(
+        "Minimum number of moves to get access to data: {}",
+        minimum_number_of_moves
+    );
 }
