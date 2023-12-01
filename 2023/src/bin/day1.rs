18add4bc4cbc87cbd6caa47df1e84b6af4adcba0
Charles Pigott <charlespigott@googlemail.com>
2023-12-01 14:04:59 +0000

    Merge solutions

--- 2023/src/bin/day1.rs
+++ 2023/src/bin/day1.rs
@@ -3,6 +3,22 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
+fn get_calibration_value(digits: &[&str], line: &str) -> u32 {
+    let digit_matches: Vec<_> = digits
+        .iter()
+        .map(|digit| line.match_indices(digit))
+        .flatten()
+        .map(|(idx, digit_str)| {
+            // array index to number
+            let integer = digits.iter().position(|&d| d == digit_str).unwrap() as u32 % 9 + 1;
+            (idx, integer)
+        })
+        .collect();
+    let first_digit = digit_matches.iter().min_by_key(|&(idx, _)| idx).unwrap().1;
+    let last_digit = digit_matches.iter().max_by_key(|&(idx, _)| idx).unwrap().1;
+    first_digit * 10 + last_digit
+}
+
 fn main() -> io::Result<()> {
     if env::args().len() != 2 {
         panic!("Incorrect number of arguments provided");
@@ -13,56 +29,21 @@ fn main() -> io::Result<()> {
             .map(|l| l.unwrap().parse().unwrap())
             .collect();
 
-    let calibration_values: Vec<_> = raw_calibration_values
+    let p1_digits: &[_] = &["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let calibration_sum: u32 = raw_calibration_values
         .iter()
-        .map(|r| {
-            r.chars()
-                .find(|c| c.is_digit(10))
-                .unwrap()
-                .to_digit(10)
-                .unwrap()
-                * 10
-                + r.chars()
-                    .rfind(|c| c.is_digit(10))
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap()
-        })
-        .collect();
-    let calibration_sum: u32 = calibration_values.iter().sum();
+        .map(|l| get_calibration_value(p1_digits, l))
+        .sum();
     println!("Calibration value sum: {}", calibration_sum);
 
-    let calibration_values_with_words: Vec<_> = raw_calibration_values
+    let p2_digits: &[_] = &[
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine",
+    ];
+    let calibration_sum_with_words: u32 = raw_calibration_values
         .iter()
-        .map(|r| {
-            let digits: &[_] = &["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-            let words: &[_] = &[
-                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-            ];
-
-            let digit_matches: Vec<_> = digits
-                .iter()
-                .chain(words.iter())
-                .map(|digit| r.match_indices(digit)).flatten()
-                .map(|(idx, digit_str)| {
-                    let integer = digits
-                        .iter()
-                        .position(|&d| d == digit_str)
-                        .or(words.iter().position(|&d| d == digit_str))
-                        .unwrap()
-                        + 1; // array index to number
-                    (idx, integer)
-                })
-                .collect();
-
-
-            let first_digit = digit_matches.iter().min_by_key(|&(idx, _)| idx).unwrap().1;
-            let last_digit = digit_matches.iter().max_by_key(|&(idx, _)| idx).unwrap().1;
-            first_digit * 10 + last_digit
-        })
-        .collect();
-
-    let calibration_sum_with_words: usize = calibration_values_with_words.iter().sum();
+        .map(|l| get_calibration_value(p2_digits, l))
+        .sum();
     println!("Calibration value sum: {}", calibration_sum_with_words);
     Ok(())
 }
