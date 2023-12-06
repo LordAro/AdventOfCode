886209f210418e88587904d3382900dc16cc66af
Charles Pigott <charlespigott@googlemail.com>
2023-12-06 11:05:35 +0000

    D6 mathematical solution

--- 2023/src/bin/day6.rs
+++ 2023/src/bin/day6.rs
@@ -3,13 +3,19 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
-fn run_race_possibilities(max_time: u64) -> Vec<u64> {
-    (0..max_time)
-        .map(|held_time| {
-            let speed = held_time;
-            (max_time - held_time) * speed
-        })
-        .collect()
+fn get_winning_race_hold_minmax(race_time: u64, distance_to_beat: u64) -> (u64, u64) {
+    // (race_time - hold_time) * hold_time > distance_to_beat
+    // =>
+    // -1 * ( hold_time * hold_time ) + race_time * hold_time - distance_to_beat > 0
+    // quadratic = (-b +/- sqrt(b*b - 4ac)) / 2a
+    let a: f64 = -1.;
+    let b: f64 = race_time as f64;
+    let c: f64 = -(distance_to_beat as f64);
+    let min = (-b + (b * b - 4. * a * c).sqrt()) / 2. * a;
+    let max = (-b - (b * b - 4. * a * c).sqrt()) / 2. * a;
+    // not entirely sure why floor & ceil results in a solution that's +1 to the actual result, but
+    // there we go
+    (min.floor() as u64, max.ceil() as u64)
 }
 
 fn main() -> io::Result<()> {
@@ -36,12 +42,10 @@ fn main() -> io::Result<()> {
         .map(|n| n.parse::<u64>().unwrap())
         .collect();
 
-    let number_of_win_combinations_product: usize = (0..times.len())
+    let number_of_win_combinations_product: u64 = (0..times.len())
         .map(|idx| {
-            run_race_possibilities(times[idx])
-                .iter()
-                .filter(|&&d| d > distances_to_beat[idx])
-                .count()
+            let minmax = get_winning_race_hold_minmax(times[idx], distances_to_beat[idx]);
+            minmax.0.abs_diff(minmax.1) - 1
         })
         .product();
 
@@ -56,11 +60,9 @@ fn main() -> io::Result<()> {
     let concat_distance = distances_to_beat
         .iter()
         .fold(0, |acc, n| acc * (10u64.pow(n.ilog10() + 1)) + n);
-    let number_of_concat_win_combinations = run_race_possibilities(concat_time)
-        .iter()
-        .filter(|&&d| d > concat_distance)
-        .count();
 
+    let concat_minmax = get_winning_race_hold_minmax(concat_time, concat_distance);
+    let number_of_concat_win_combinations = concat_minmax.0.abs_diff(concat_minmax.1) - 1;
     println!(
         "Number of win combinations (by concatenation): {}",
         number_of_concat_win_combinations
