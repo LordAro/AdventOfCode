a8e307eed230e34f7a1720e11a887089b4f07a90
Charles Pigott <charlespigott@googlemail.com>
2023-12-10 16:37:13 +0000

    D10p2

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -1,9 +1,10 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io;
 use std::iter::successors;
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 struct Coord {
     x: usize,
     y: usize,
@@ -17,49 +18,164 @@ enum Dir {
     West,
 }
 
-fn get_next_coord(map: &Vec<Vec<char>>, pos: &Coord, dir: Dir) -> Option<(Coord, Dir)> {
-    let next_coord = match dir {
-        Dir::North => Coord {
-            x: pos.x,
-            y: pos.y - 1,
-        },
-        Dir::East => Coord {
-            x: pos.x + 1,
-            y: pos.y,
-        },
-        Dir::South => Coord {
-            x: pos.x,
-            y: pos.y + 1,
-        },
-        Dir::West => Coord {
-            x: pos.x - 1,
-            y: pos.y,
-        },
-    };
+fn get_next_coord(c: &Coord, d: Dir, max_dim: &Coord) -> Option<Coord> {
+    match d {
+        Dir::North => {
+            if c.y > 0 {
+                Some(Coord { x: c.x, y: c.y - 1 })
+            } else {
+                None
+            }
+        }
+        Dir::East => {
+            if c.x < max_dim.x - 1 {
+                Some(Coord { x: c.x + 1, y: c.y })
+            } else {
+                None
+            }
+        }
+        Dir::South => {
+            if c.y < max_dim.y - 1 {
+                Some(Coord { x: c.x, y: c.y + 1 })
+            } else {
+                None
+            }
+        }
+        Dir::West => {
+            if c.x > 0 {
+                Some(Coord { x: c.x - 1, y: c.y })
+            } else {
+                None
+            }
+        }
+    }
+}
 
-    // Let underflow handle any out of bounds accesses (shouldn't ever happen)
-    if next_coord.y >= map.len() || next_coord.x >= map[0].len() {
-        return None;
+fn get_next_coord_on_loop(map: &Vec<Vec<char>>, pos: &Coord, dir: Dir) -> Option<(Coord, Dir)> {
+    let max_dim = Coord {
+        x: map[0].len(),
+        y: map.len(),
+    };
+    if let Some(next_coord) = get_next_coord(pos, dir, &max_dim) {
+        let next_dir = match (map[next_coord.y][next_coord.x], dir) {
+            // no change, if we hit 'S' we're done, don't care about the direction anymore
+            ('-', Dir::East) => Dir::East,
+            ('-', Dir::West) => Dir::West,
+            ('|', Dir::North) => Dir::North,
+            ('|', Dir::South) => Dir::South,
+            ('J', Dir::South) => Dir::West,
+            ('J', Dir::East) => Dir::North,
+            ('F', Dir::North) => Dir::East,
+            ('F', Dir::West) => Dir::South,
+            ('L', Dir::South) => Dir::East,
+            ('L', Dir::West) => Dir::North,
+            ('7', Dir::North) => Dir::West,
+            ('7', Dir::East) => Dir::South,
+            _ => return None,
+        };
+        Some((next_coord, next_dir))
+    } else {
+        None
+    }
+}
+
+fn flood_fill(
+    map: &Vec<Vec<char>>,
+    route_set: &HashSet<Coord>,
+    starting_set: HashSet<Coord>,
+) -> HashSet<Coord> {
+    let mut to_search: Vec<Coord> = starting_set.iter().copied().collect();
+    let mut searched = HashSet::new();
+
+    while let Some(next) = to_search.pop() {
+        if route_set.contains(&next) {
+            continue;
+        }
+
+        if !searched.insert(next) {
+            // already searched
+            continue;
+        }
+        if next.y > 0 {
+            to_search.push(Coord {
+                x: next.x,
+                y: next.y - 1,
+            });
+        }
+        if next.x < map[0].len() - 1 {
+            to_search.push(Coord {
+                x: next.x + 1,
+                y: next.y,
+            });
+        }
+        if next.y < map.len() - 1 {
+            to_search.push(Coord {
+                x: next.x,
+                y: next.y + 1,
+            });
+        }
+        if next.x > 0 {
+            to_search.push(Coord {
+                x: next.x - 1,
+                y: next.y,
+            });
+        }
     }
 
-    let next_dir = match (map[next_coord.y][next_coord.x], dir) {
-        // no change, if we hit 'S' we're done, don't care about the direction anymore
-        ('-', Dir::East) => Dir::East,
-        ('-', Dir::West) => Dir::West,
-        ('|', Dir::North) => Dir::North,
-        ('|', Dir::South) => Dir::South,
-        ('J', Dir::South) => Dir::West,
-        ('J', Dir::East) => Dir::North,
-        ('F', Dir::North) => Dir::East,
-        ('F', Dir::West) => Dir::South,
-        ('L', Dir::South) => Dir::East,
-        ('L', Dir::West) => Dir::North,
-        ('7', Dir::North) => Dir::West,
-        ('7', Dir::East) => Dir::South,
-        _ => return None,
+    searched
+}
+
+fn trace_route(map: &Vec<Vec<char>>, route: &[(Coord, Dir)]) -> (HashSet<Coord>, HashSet<Coord>) {
+    let route_set: HashSet<Coord> = route.iter().map(|(c, _)| c).copied().collect(); // quick lookups
+    let mut set_a: HashSet<Coord> = HashSet::new();
+    let mut set_b: HashSet<Coord> = HashSet::new();
+
+    let max_dim = Coord {
+        x: map[0].len(),
+        y: map.len(),
     };
 
-    Some((next_coord, next_dir))
+    for (c, move_dir) in route {
+        let (look_dir_a, look_dir_b) = match move_dir {
+            Dir::North => (Dir::West, Dir::East),
+            Dir::East => (Dir::North, Dir::South),
+            Dir::South => (Dir::East, Dir::West),
+            Dir::West => (Dir::South, Dir::North),
+        };
+
+        if let Some(look_coord_a) = get_next_coord(c, look_dir_a, &max_dim) {
+            if !route_set.contains(&look_coord_a) {
+                set_a.insert(look_coord_a);
+            }
+        }
+        if let Some(look_coord_b) = get_next_coord(c, look_dir_b, &max_dim) {
+            if !route_set.contains(&look_coord_b) {
+                set_b.insert(look_coord_b);
+            }
+        }
+    }
+    (set_a, set_b)
+}
+
+fn get_enclosed_pieces(map: &Vec<Vec<char>>, route: &[(Coord, Dir)]) -> HashSet<Coord> {
+    let (set_a, set_b) = trace_route(map, route);
+    assert!(set_a.intersection(&set_b).count() == 0);
+
+    let route_set: HashSet<Coord> = route.iter().map(|(c, _)| c).copied().collect(); // quick lookups
+    let filled_set_a = flood_fill(map, &route_set, set_a);
+    let filled_set_b = flood_fill(map, &route_set, set_b);
+
+    assert!(filled_set_a.intersection(&filled_set_b).count() == 0);
+
+    if filled_set_a
+        .iter()
+        .any(|c| c.x == 0 || c.y == 0 || c.x == map[0].len() - 1 || c.y == map.len() - 1)
+    {
+        // found an edge piece, must be the other
+        filled_set_b
+    } else {
+        filled_set_a
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -74,17 +190,19 @@ fn main() -> io::Result<()> {
 
     let (_, start_dir) = [Dir::North, Dir::East, Dir::South, Dir::West]
         .iter()
-        .find_map(|&d| get_next_coord(&map, &start_pos, d))
+        .find_map(|&d| get_next_coord_on_loop(&map, &start_pos, d))
         .unwrap();
 
     let route: Vec<_> = successors(Some((start_pos, start_dir)), |(pos, dir)| {
-        get_next_coord(&map, &pos, *dir)
+        get_next_coord_on_loop(&map, pos, *dir)
     })
-    .map(|(pos, _)| pos)
     .collect();
 
     let max_distance = (route.len() + 1) / 2;
     println!("Maximum distance from start: {}", max_distance);
+
+    let enclosed_tiles = get_enclosed_pieces(&map, &route);
+    println!("Number of enclosed tiles: {}", enclosed_tiles.len());
 
     Ok(())
 }
