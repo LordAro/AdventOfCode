11b1b83af2e248348090f1be0849f19bcfdf2226
Charles Pigott <charlespigott@googlemail.com>
2023-12-10 16:59:38 +0000

    Minor tidying

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -18,7 +18,7 @@ enum Dir {
     West,
 }
 
-fn get_next_coord(c: &Coord, d: Dir, max_dim: &Coord) -> Option<Coord> {
+fn get_next_coord(map: &Vec<Vec<char>>, c: &Coord, d: &Dir) -> Option<Coord> {
     match d {
         Dir::North => {
             if c.y > 0 {
@@ -28,14 +28,14 @@ fn get_next_coord(c: &Coord, d: Dir, max_dim: &Coord) -> Option<Coord> {
             }
         }
         Dir::East => {
-            if c.x < max_dim.x - 1 {
+            if c.x < map[0].len() - 1 {
                 Some(Coord { x: c.x + 1, y: c.y })
             } else {
                 None
             }
         }
         Dir::South => {
-            if c.y < max_dim.y - 1 {
+            if c.y < map.len() - 1 {
                 Some(Coord { x: c.x, y: c.y + 1 })
             } else {
                 None
@@ -51,12 +51,8 @@ fn get_next_coord(c: &Coord, d: Dir, max_dim: &Coord) -> Option<Coord> {
     }
 }
 
-fn get_next_coord_on_loop(map: &Vec<Vec<char>>, pos: &Coord, dir: Dir) -> Option<(Coord, Dir)> {
-    let max_dim = Coord {
-        x: map[0].len(),
-        y: map.len(),
-    };
-    if let Some(next_coord) = get_next_coord(pos, dir, &max_dim) {
+fn get_next_coord_on_loop(map: &Vec<Vec<char>>, pos: &Coord, dir: &Dir) -> Option<(Coord, Dir)> {
+    if let Some(next_coord) = get_next_coord(map, pos, dir) {
         let next_dir = match (map[next_coord.y][next_coord.x], dir) {
             // no change, if we hit 'S' we're done, don't care about the direction anymore
             ('-', Dir::East) => Dir::East,
@@ -79,10 +75,37 @@ fn get_next_coord_on_loop(map: &Vec<Vec<char>>, pos: &Coord, dir: Dir) -> Option
     }
 }
 
+fn trace_route(map: &Vec<Vec<char>>, route: &[(Coord, Dir)]) -> (HashSet<Coord>, HashSet<Coord>) {
+    let route_set: HashSet<_> = route.iter().map(|(c, _)| c).collect(); // quick lookups
+    let mut set_a: HashSet<Coord> = HashSet::new();
+    let mut set_b: HashSet<Coord> = HashSet::new();
+
+    for (c, move_dir) in route {
+        let (look_dir_a, look_dir_b) = match move_dir {
+            Dir::North => (Dir::West, Dir::East),
+            Dir::East => (Dir::North, Dir::South),
+            Dir::South => (Dir::East, Dir::West),
+            Dir::West => (Dir::South, Dir::North),
+        };
+
+        if let Some(look_coord_a) = get_next_coord(map, c, &look_dir_a) {
+            if !route_set.contains(&look_coord_a) {
+                set_a.insert(look_coord_a);
+            }
+        }
+        if let Some(look_coord_b) = get_next_coord(map, c, &look_dir_b) {
+            if !route_set.contains(&look_coord_b) {
+                set_b.insert(look_coord_b);
+            }
+        }
+    }
+    (set_a, set_b)
+}
+
 fn flood_fill(
     map: &Vec<Vec<char>>,
-    route_set: &HashSet<Coord>,
-    starting_set: HashSet<Coord>,
+    route_set: &HashSet<&Coord>,
+    starting_set: &HashSet<Coord>,
 ) -> HashSet<Coord> {
     let mut to_search: Vec<Coord> = starting_set.iter().copied().collect();
     let mut searched = HashSet::new();
@@ -125,53 +148,21 @@ fn flood_fill(
     searched
 }
 
-fn trace_route(map: &Vec<Vec<char>>, route: &[(Coord, Dir)]) -> (HashSet<Coord>, HashSet<Coord>) {
-    let route_set: HashSet<Coord> = route.iter().map(|(c, _)| c).copied().collect(); // quick lookups
-    let mut set_a: HashSet<Coord> = HashSet::new();
-    let mut set_b: HashSet<Coord> = HashSet::new();
-
-    let max_dim = Coord {
-        x: map[0].len(),
-        y: map.len(),
-    };
-
-    for (c, move_dir) in route {
-        let (look_dir_a, look_dir_b) = match move_dir {
-            Dir::North => (Dir::West, Dir::East),
-            Dir::East => (Dir::North, Dir::South),
-            Dir::South => (Dir::East, Dir::West),
-            Dir::West => (Dir::South, Dir::North),
-        };
-
-        if let Some(look_coord_a) = get_next_coord(c, look_dir_a, &max_dim) {
-            if !route_set.contains(&look_coord_a) {
-                set_a.insert(look_coord_a);
-            }
-        }
-        if let Some(look_coord_b) = get_next_coord(c, look_dir_b, &max_dim) {
-            if !route_set.contains(&look_coord_b) {
-                set_b.insert(look_coord_b);
-            }
-        }
-    }
-    (set_a, set_b)
-}
-
 fn get_enclosed_pieces(map: &Vec<Vec<char>>, route: &[(Coord, Dir)]) -> HashSet<Coord> {
     let (set_a, set_b) = trace_route(map, route);
-    assert!(set_a.intersection(&set_b).count() == 0);
+    debug_assert!(set_a.intersection(&set_b).count() == 0);
 
-    let route_set: HashSet<Coord> = route.iter().map(|(c, _)| c).copied().collect(); // quick lookups
-    let filled_set_a = flood_fill(map, &route_set, set_a);
-    let filled_set_b = flood_fill(map, &route_set, set_b);
+    let route_set: HashSet<_> = route.iter().map(|(c, _)| c).collect(); // quick lookups
+    let filled_set_a = flood_fill(map, &route_set, &set_a);
+    let filled_set_b = flood_fill(map, &route_set, &set_b);
 
-    assert!(filled_set_a.intersection(&filled_set_b).count() == 0);
+    debug_assert!(filled_set_a.intersection(&filled_set_b).count() == 0);
 
     if filled_set_a
         .iter()
         .any(|c| c.x == 0 || c.y == 0 || c.x == map[0].len() - 1 || c.y == map.len() - 1)
     {
-        // found an edge piece, must be the other
+        // found an edge piece, must be the other set
         filled_set_b
     } else {
         filled_set_a
@@ -188,13 +179,14 @@ fn main() -> io::Result<()> {
         .find_map(|(y, l)| l.iter().position(|&c| c == 'S').map(|x| Coord { x, y }))
         .unwrap();
 
+    // look around to find a suitable starting location
     let (_, start_dir) = [Dir::North, Dir::East, Dir::South, Dir::West]
         .iter()
-        .find_map(|&d| get_next_coord_on_loop(&map, &start_pos, d))
+        .find_map(|d| get_next_coord_on_loop(&map, &start_pos, d))
         .unwrap();
 
     let route: Vec<_> = successors(Some((start_pos, start_dir)), |(pos, dir)| {
-        get_next_coord_on_loop(&map, pos, *dir)
+        get_next_coord_on_loop(&map, pos, dir)
     })
     .collect();
 
