27cfc9b3702f2525ad6fded6c4bae72d13a41746
Charles Pigott <charlespigott@googlemail.com>
2023-12-12 16:13:01 +0000

    D12p2

--- 2023/src/bin/day12.rs
+++ 2023/src/bin/day12.rs
@@ -1,36 +1,20 @@
 use std::cmp;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
 use std::str;
 
+extern crate itertools;
+use itertools::Itertools;
+
 struct Record(String, Vec<usize>);
 
-//fn count_arrangements_slow(record: &str, expected_groups: &[usize]) -> usize {
-//    if let Some(idx) = record.rfind('?') {
-//        let mut r1: Vec<_> = record.as_bytes().iter().copied().collect();
-//        r1[idx] = b'#';
-//        let mut r2: Vec<_> = record.as_bytes().iter().copied().collect();
-//        r2[idx] = b'.';
-//        count_arrangements_slow(str::from_utf8(r1.as_slice()).unwrap(), expected_groups)
-//            + count_arrangements_slow(str::from_utf8(r2.as_slice()).unwrap(), expected_groups)
-//    } else {
-//        if record
-//            .split('.')
-//            .map(|s| s.len())
-//            .filter(|&n| n > 0)
-//            .collect::<Vec<_>>()
-//            == expected_groups
-//        {
-//            1
-//        } else {
-//            0
-//        }
-//    }
-//}
-
-fn count_arrangements(record: &str, expected_groups: &[usize]) -> usize {
-    //println!("{:?} {:?}", record, expected_groups);
+fn count_arrangements(
+    cache: &mut HashMap<(String, Vec<usize>), usize>,
+    record: &str,
+    expected_groups: &[usize],
+) -> usize {
     if expected_groups.is_empty() {
         // we managed to fit in all groups, successful arrangement
         return if record.contains('#') { 0 } else { 1 };
@@ -41,38 +25,44 @@ fn count_arrangements(record: &str, expected_groups: &[usize]) -> usize {
         return 0;
     }
 
+    let cache_key = (String::from(record), expected_groups.to_vec());
+    if cache.contains_key(&cache_key) {
+        return *cache.get(&cache_key).unwrap();
+    }
+
     // find a place for the next group
     let next_group_len = expected_groups[0];
     if let Some(next_possible_position) = record.find(['#', '?']) {
         let group_plus_remainder = &record[next_possible_position..];
         if group_plus_remainder.len() < next_group_len {
-            //println!("ret1");
             // not enough string left, unsuccessful arrangement
             return 0;
         }
 
         let possible_group = &group_plus_remainder[..next_group_len];
 
-        let contains_corruption = possible_group.starts_with('?');
+        let is_corrupted = possible_group.starts_with('?');
 
         if possible_group.contains('.') {
             // can't fit group in this position, unsuccessful arrangement
-            //println!("ret2");
-            return if contains_corruption {
-                count_arrangements(&group_plus_remainder[1..], expected_groups)
+            let ret = if is_corrupted {
+                count_arrangements(cache, &group_plus_remainder[1..], expected_groups)
             } else {
                 0
             };
+            cache.insert(cache_key, ret);
+            return ret;
         }
 
         if group_plus_remainder[next_group_len..].starts_with('#') {
             // no gap after end of group, unsuccessful arrangement
-            //println!("ret3");
-            return if contains_corruption {
-                count_arrangements(&group_plus_remainder[1..], expected_groups)
+            let ret = if is_corrupted {
+                count_arrangements(cache, &group_plus_remainder[1..], expected_groups)
             } else {
                 0
             };
+            cache.insert(cache_key, ret);
+            return ret;
         }
 
         // if there's stuff left after this group, skip a position to ensure a gap
@@ -81,15 +71,17 @@ fn count_arrangements(record: &str, expected_groups: &[usize]) -> usize {
 
         // successfully placed a group, get combinations with this group removed
         let arrangements_after_group_placement =
-            count_arrangements(after_group, &expected_groups[1..]);
+            count_arrangements(cache, after_group, &expected_groups[1..]);
 
         // if we say a group doesn't go here, try the next position with the same groups
-        arrangements_after_group_placement
-            + if contains_corruption {
-                count_arrangements(&group_plus_remainder[1..], expected_groups)
+        let ret = arrangements_after_group_placement
+            + if is_corrupted {
+                count_arrangements(cache, &group_plus_remainder[1..], expected_groups)
             } else {
                 0
-            }
+            };
+        cache.insert(cache_key, ret);
+        ret
     } else {
         // No more possible positions, unsuccessful arrangement
         0
@@ -113,15 +105,12 @@ fn main() -> io::Result<()> {
         })
         .collect();
 
-    //for Record(rec, groups) in &records {
-    //    let new = count_arrangements(rec, groups);
-    //    let old = count_arrangements_slow(rec, groups);
-    //    if old != new {
-    //        println!("{:?} {:?} -> {} / {}", rec, groups, old, new);
-    //    }
-    //}
+    let mut cache = HashMap::new();
 
-    let arrangement_sum: usize = records.iter().map(|r| count_arrangements(&r.0, &r.1)).sum();
+    let arrangement_sum: usize = records
+        .iter()
+        .map(|r| count_arrangements(&mut cache, &r.0, &r.1))
+        .sum();
 
     println!("Sum of possible arrangements: {}", arrangement_sum);
 
@@ -130,7 +119,7 @@ fn main() -> io::Result<()> {
         .iter()
         .map(|r| {
             Record(
-                r.0.repeat(5),
+                itertools::repeat_n(r.0.clone(), 5).join("?"),
                 r.1.iter().cycle().take(r.1.len() * 5).copied().collect(),
             )
         })
@@ -138,7 +127,7 @@ fn main() -> io::Result<()> {
 
     let uncoiled_arrangement_sum: usize = uncoiled_records
         .iter()
-        .map(|r| count_arrangements(&r.0, &r.1))
+        .map(|r| count_arrangements(&mut cache, &r.0, &r.1))
         .sum();
 
     println!(
@@ -164,8 +153,9 @@ mod tests {
 
     #[test]
     fn ex1() {
+        let mut cache = HashMap::new();
         for (record, expected_groups) in TEST_DATA1 {
-            assert_eq!(count_arrangements(record, expected_groups), 1);
+            assert_eq!(count_arrangements(&mut cache, record, expected_groups), 1);
         }
     }
 
@@ -181,21 +171,23 @@ mod tests {
     // manual tests based on group 2
     #[test]
     fn ex2a() {
-        assert_eq!(count_arrangements("??", &[1]), 2);
-        assert_eq!(count_arrangements("????", &[1, 1]), 3);
+        let mut cache = HashMap::new();
+        assert_eq!(count_arrangements(&mut cache, "??", &[1]), 2);
+        assert_eq!(count_arrangements(&mut cache, "????", &[1, 1]), 3);
 
-        assert_eq!(count_arrangements("??.#", &[1, 1]), 2);
-        assert_eq!(count_arrangements("??", &[1]), 2);
-        assert_eq!(count_arrangements(".??", &[1]), 2);
+        assert_eq!(count_arrangements(&mut cache, "??.#", &[1, 1]), 2);
+        assert_eq!(count_arrangements(&mut cache, "??", &[1]), 2);
+        assert_eq!(count_arrangements(&mut cache, ".??", &[1]), 2);
 
-        assert_eq!(count_arrangements("?#?", &[1]), 1);
+        assert_eq!(count_arrangements(&mut cache, "?#?", &[1]), 1);
     }
 
     #[test]
     fn ex2b() {
+        let mut cache = HashMap::new();
         for (record, expected_groups, expected_combinations) in TEST_DATA2 {
             assert_eq!(
-                count_arrangements(record, expected_groups),
+                count_arrangements(&mut cache, record, expected_groups),
                 *expected_combinations
             );
         }
@@ -203,6 +195,7 @@ mod tests {
 
     #[test]
     fn ex3() {
-        assert_eq!(count_arrangements("??#?.???##", &[1, 4]), 1);
+        let mut cache = HashMap::new();
+        assert_eq!(count_arrangements(&mut cache, "??#?.???##", &[1, 4]), 1);
     }
 }
