db36778820c26950da6d87e289cf755014b49913
Charles Pigott <charlespigott@googlemail.com>
2023-12-13 13:04:04 +0000

    D13: Remove mutable state

--- 2023/src/bin/day13.rs
+++ 2023/src/bin/day13.rs
@@ -2,48 +2,31 @@ use std::env;
 use std::fs;
 use std::io;
 
+extern crate itertools;
+use itertools::Itertools;
+
 type Grid = Vec<Vec<bool>>;
 
-fn find_mirror_vert(map: &Grid, target_difference: usize) -> Option<usize> {
-    for pivot in 1..map[0].len() {
-        let mut count_diffs = 0;
+fn find_mirror_vert<const N: usize>(map: &Grid) -> Option<usize> {
+    (1..map[0].len()).find(|&pivot| {
         let x_height = pivot.min(map[0].len() - pivot);
-        for x in 0..x_height {
-            for y in 0..map.len() {
-                if map[y][pivot + x] != map[y][pivot - (x + 1)] {
-                    count_diffs += 1;
-                }
-            }
-            if count_diffs > target_difference {
-                break;
-            }
-        }
-        if count_diffs == target_difference {
-            return Some(pivot);
-        }
-    }
-    None
+        (0..map.len())
+            .cartesian_product(0..x_height)
+            .filter(|&(y, x)| map[y][pivot + x] != map[y][pivot - (x + 1)])
+            .count()
+            == N
+    })
 }
 
-fn find_mirror_horz(map: &Grid, target_difference: usize) -> Option<usize> {
-    for pivot in 1..map.len() {
-        let mut count_diffs = 0;
+fn find_mirror_horz<const N: usize>(map: &Grid) -> Option<usize> {
+    (1..map.len()).find(|&pivot| {
         let y_height = pivot.min(map.len() - pivot);
-        for y in 0..y_height {
-            for x in 0..map[0].len() {
-                if map[pivot + y][x] != map[pivot - (y + 1)][x] {
-                    count_diffs += 1;
-                }
-            }
-            if count_diffs > target_difference {
-                break;
-            }
-        }
-        if count_diffs == target_difference {
-            return Some(pivot);
-        }
-    }
-    None
+        (0..y_height)
+            .cartesian_product(0..map[0].len())
+            .filter(|&(y, x)| map[pivot + y][x] != map[pivot - (y + 1)][x])
+            .count()
+            == N
+    })
 }
 
 fn main() -> io::Result<()> {
@@ -61,8 +44,8 @@ fn main() -> io::Result<()> {
     let sum: usize = blocks
         .iter()
         .map(|b| {
-            find_mirror_vert(b, 0)
-                .or_else(|| find_mirror_horz(b, 0).map(|n| n * 100))
+            find_mirror_vert::<0>(b)
+                .or_else(|| find_mirror_horz::<0>(b).map(|n| n * 100))
                 .unwrap()
         })
         .sum();
@@ -72,8 +55,8 @@ fn main() -> io::Result<()> {
     let smudge_sum: usize = blocks
         .iter()
         .map(|b| {
-            find_mirror_vert(b, 1)
-                .or_else(|| find_mirror_horz(b, 1).map(|n| n * 100))
+            find_mirror_vert::<1>(b)
+                .or_else(|| find_mirror_horz::<1>(b).map(|n| n * 100))
                 .unwrap()
         })
         .sum();
