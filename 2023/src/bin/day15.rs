9f3e492fd61ff9ca338ee881604da4c44b1ae6fd
Charles Pigott <charlespigott@googlemail.com>
2023-12-15 16:39:49 +0000

    Split into functions

--- 2023/src/bin/day15.rs
+++ 2023/src/bin/day15.rs
@@ -7,41 +7,37 @@ fn hash_alg(s: &str) -> usize {
         .fold(0usize, |acc, c| ((acc + c as usize) * 17) % 256)
 }
 
-fn main() -> io::Result<()> {
-    let input_str: String =
-        fs::read_to_string(env::args().nth(1).expect("Incorrect number of arguments"))?;
-
-    let hash_strs: Vec<_> = input_str.trim().split(',').collect();
-
-    let hash_sum: usize = hash_strs.iter().map(|s| hash_alg(s)).sum();
-
-    println!("Sum of string HASHes: {}", hash_sum);
-
+fn place_into_boxes<'a>(strs: &[&'a str]) -> Vec<Vec<(&'a str, usize)>> {
     let mut boxes: Vec<Vec<(&str, usize)>> = vec![vec![]; 256];
 
-    for s in hash_strs {
-        let mut it = s.split(['-', '=']);
-        let label = it.next().unwrap();
-        let label_hash = hash_alg(label);
-        let focal_length = it.next().unwrap();
-        if focal_length.is_empty() {
-            // '-'
-            // remove label from box, if exists
-            if let Some(idx) = boxes[label_hash].iter().position(|&(s, _)| s == label) {
-                boxes[label_hash].remove(idx);
+    for s in strs {
+        match s.split_once('=') {
+            None => {
+                // '-'
+                let label = &s[..s.len() - 1];
+                let label_hash = hash_alg(label);
+                // remove label from box, if exists
+                if let Some(idx) = boxes[label_hash].iter().position(|&(s, _)| s == label) {
+                    boxes[label_hash].remove(idx);
+                }
             }
-        } else {
-            // '='
-            let lens = focal_length.parse().unwrap();
-            if let Some(idx) = boxes[label_hash].iter().position(|&(s, _)| s == label) {
-                boxes[label_hash][idx].1 = lens;
-            } else {
-                boxes[label_hash].push((label, lens));
+            Some((label, focal_length)) => {
+                // '='
+                let label_hash = hash_alg(label);
+                let lens = focal_length.parse().unwrap();
+                if let Some(idx) = boxes[label_hash].iter().position(|&(s, _)| s == label) {
+                    boxes[label_hash][idx].1 = lens;
+                } else {
+                    boxes[label_hash].push((label, lens));
+                }
             }
         }
     }
+    boxes
+}
 
-    let focusing_power: usize = boxes
+fn calc_focusing_power(boxes: &[Vec<(&str, usize)>]) -> usize {
+    boxes
         .iter()
         .enumerate()
         .map(|(box_number, box_)| {
@@ -52,7 +48,22 @@ fn main() -> io::Result<()> {
                 })
                 .sum::<usize>()
         })
-        .sum();
+        .sum()
+}
+
+fn main() -> io::Result<()> {
+    let input_str: String =
+        fs::read_to_string(env::args().nth(1).expect("Incorrect number of arguments"))?;
+
+    let hash_strs: Vec<_> = input_str.trim().split(',').collect();
+
+    let hash_sum: usize = hash_strs.iter().map(|s| hash_alg(s)).sum();
+
+    println!("Sum of string HASHes: {}", hash_sum);
+
+    let boxes = place_into_boxes(&hash_strs);
+
+    let focusing_power = calc_focusing_power(&boxes);
 
     println!("Focusing power of all lenses: {}", focusing_power);
 
