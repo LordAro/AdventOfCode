53c3629863c62e8aa6ce70de57a9c80dcab6cc69
Charles Pigott <charlespigott@googlemail.com>
2023-12-27 19:35:26 +0000

    2019D19p2

--- 2019/src/bin/day19.rs
+++ 2019/src/bin/day19.rs
@@ -8,6 +8,13 @@ use itertools::Itertools;
 extern crate advent_of_code;
 use advent_of_code::intcode;
 
+fn is_point_covered(program: &[isize], x: isize, y: isize) -> bool {
+    intcode::Machine::new(program, &[x, y])
+        .run_until_output()
+        .filter(|&p| p == 1)
+        .is_some()
+}
+
 fn main() {
     let program_str = BufReader::new(
         File::open(
@@ -26,16 +33,40 @@ fn main() {
 
     let points_affected = (0..50)
         .cartesian_product(0..50)
-        .filter_map(|(x, y)| {
-            let mut mach = intcode::Machine::new(&program, &[]);
-            mach.push_input(x);
-            mach.push_input(y);
-            mach.run_until_output()
-        })
-        .filter(|&p| p == 1)
+        .filter(|&(x, y)| is_point_covered(&program, x, y))
         .count();
     println!(
         "Number of points affected by tractor beam: {}",
         points_affected
     );
+
+    // cheap and nasty exit
+    let mut prev_x = 0;
+    'outer: for y in 0.. {
+        // first position must be(?) within 2n of the first position found
+        for x in prev_x..prev_x + 200 {
+            if !is_point_covered(&program, x, y) {
+                continue;
+            }
+            prev_x = x;
+
+            if !is_point_covered(&program, x + 99, y) {
+                // can't be this row, move on to the next
+                break;
+            }
+
+            if !is_point_covered(&program, x, y + 99) {
+                // might be further along the x-axis, keep checking
+                continue;
+            }
+
+            // don't need to check the 4th corner at all
+
+            println!(
+                "Found corners of 100x100 square: {x} {y}, {}",
+                x * 10000 + y
+            );
+            break 'outer;
+        }
+    }
 }
