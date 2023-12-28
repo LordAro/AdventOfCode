e7a693a0218414a6fc6fc662eaaa3ef379da6aff
Charles Pigott <charlespigott@googlemail.com>
2023-12-28 22:47:46 +0000

    2019d20p2

--- 2019/src/bin/day20.rs
+++ 2019/src/bin/day20.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs;
+use std::io;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum State {
@@ -10,18 +10,49 @@ enum State {
     Teleport,
 }
 
-type Coord = (usize, usize);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
 type Grid = Vec<Vec<State>>;
 
 struct Maze {
     map: Grid,
     teleports: HashMap<Coord, Coord>,
+    outer_teleports: HashSet<Coord>,
     start: Coord,
     end: Coord,
 }
 
-fn parse_map(input_lines: &[Vec<u8>]) -> Maze {
+fn adjacent_coords(limit: Coord, p: Coord) -> Vec<Coord> {
+    [
+        Coord {
+            x: p.x.wrapping_sub(1),
+            y: p.y,
+        },
+        Coord {
+            x: p.x,
+            y: p.y.wrapping_sub(1),
+        },
+        Coord { x: p.x + 1, y: p.y },
+        Coord { x: p.x, y: p.y + 1 },
+    ]
+    .iter()
+    .filter(move |&x| x.x < limit.x && x.y < limit.y)
+    .copied()
+    .collect()
+}
+
+fn parse_map(input_str: &str) -> Maze {
     let mut teleport_mappings: HashMap<[u8; 2], Vec<Coord>> = HashMap::new();
+    let mut outer_teleports: HashSet<Coord> = HashSet::new();
+    let input_lines: Vec<_> = input_str.lines().map(|l| l.as_bytes()).collect();
+    let limit = Coord {
+        x: input_lines[0].len(),
+        y: input_lines.len(),
+    };
     let map = input_lines
         .iter()
         .enumerate()
@@ -34,35 +65,29 @@ fn parse_map(input_lines: &[Vec<u8>]) -> Maze {
                     b'.' => State::Clear,
                     b'A'..=b'Z' => {
                         // use the letter next to the maze to build the full name of the teleport
-                        let adjs_minus: Vec<_> = [
-                            x.checked_sub(1)
-                                .and_then(|x2| input_lines.get(y).and_then(|r| r.get(x2).copied())),
-                            y.checked_sub(1)
-                                .and_then(|y2| input_lines.get(y2).and_then(|r| r.get(x).copied())),
-                        ]
-                        .iter()
-                        .flatten()
-                        .copied()
-                        .collect();
-                        let adjs_plus: Vec<_> = [
-                            input_lines.get(y).and_then(|r| r.get(x + 1).copied()),
-                            input_lines.get(y + 1).and_then(|r| r.get(x).copied()),
-                        ]
-                        .iter()
-                        .flatten()
-                        .copied()
-                        .collect();
-                        if adjs_minus.contains(&b'.') {
-                            let adj_letter =
-                                adjs_plus.iter().find(|&b| b.is_ascii_uppercase()).unwrap();
-                            let str_vec = [*adj_letter, *c];
-                            teleport_mappings.entry(str_vec).or_default().push((x, y));
-                            State::Teleport
-                        } else if adjs_plus.contains(&b'.') {
-                            let adj_letter =
-                                adjs_minus.iter().find(|&b| b.is_ascii_uppercase()).unwrap();
-                            let str_vec = [*c, *adj_letter];
-                            teleport_mappings.entry(str_vec).or_default().push((x, y));
+                        let coord = Coord { x, y };
+                        let adj_coords = adjacent_coords(limit, coord);
+                        if adj_coords.iter().any(|c| input_lines[c.y][c.x] == b'.') {
+                            let adj_letter_coord = adj_coords
+                                .iter()
+                                .find(|c| input_lines[c.y][c.x].is_ascii_uppercase())
+                                .unwrap();
+                            let adj_letter = input_lines[adj_letter_coord.y][adj_letter_coord.x];
+
+                            let str_vec = if adj_letter_coord.x < x || adj_letter_coord.y < y {
+                                [adj_letter, *c]
+                            } else {
+                                [*c, adj_letter]
+                            };
+
+                            let is_outer = adj_letter_coord.x == 0
+                                || adj_letter_coord.x == l.len() - 1
+                                || adj_letter_coord.y == 0
+                                || adj_letter_coord.y == input_lines.len() - 1;
+                            teleport_mappings.entry(str_vec).or_default().push(coord);
+                            if is_outer {
+                                outer_teleports.insert(coord);
+                            }
                             State::Teleport
                         } else {
                             State::Wall
@@ -73,6 +98,8 @@ fn parse_map(input_lines: &[Vec<u8>]) -> Maze {
                 .collect()
         })
         .collect();
+    let aa_portal = teleport_mappings[b"AA"][0];
+    let zz_portal = teleport_mappings[b"ZZ"][0];
     let mut teleports = HashMap::new();
     for (n, coords) in teleport_mappings.iter() {
         assert!(coords.len() == 2 || n == b"AA" || n == b"ZZ");
@@ -81,51 +108,39 @@ fn parse_map(input_lines: &[Vec<u8>]) -> Maze {
             teleports.insert(coords[1], coords[0]);
         }
     }
-    let aa_portal = teleport_mappings[b"AA"][0];
-    let zz_portal = teleport_mappings[b"ZZ"][0];
     Maze {
         map,
         teleports,
+        outer_teleports,
         start: aa_portal, // note: actual start position is adjacent
         end: zz_portal,
     }
 }
 
-fn open_adjacents(maze: &Maze, pos: Coord) -> Vec<Coord> {
-    let mut ret = vec![];
-    if pos.1 > 0 && maze.map[pos.1 - 1].len() > pos.0 && maze.map[pos.1 - 1][pos.0] != State::Wall {
-        ret.push((pos.0, pos.1 - 1));
-    }
-    if pos.1 < maze.map.len() - 1
-        && maze.map[pos.1 + 1].len() > pos.0
-        && maze.map[pos.1 + 1][pos.0] != State::Wall
-    {
-        ret.push((pos.0, pos.1 + 1));
-    }
-    if pos.0 > 0 && maze.map[pos.1].len() > pos.0 - 1 && maze.map[pos.1][pos.0 - 1] != State::Wall {
-        ret.push((pos.0 - 1, pos.1));
-    }
-    if pos.0 < maze.map[pos.1].len() - 1
-        && maze.map[pos.1].len() > pos.0 + 1
-        && maze.map[pos.1][pos.0 + 1] != State::Wall
-    {
-        ret.push((pos.0 + 1, pos.1));
-    }
-    ret
+fn open_adjacents(map: &Grid, pos: Coord) -> Vec<Coord> {
+    let limit = Coord {
+        x: map[0].len(),
+        y: map.len(),
+    };
+    adjacent_coords(limit, pos)
+        .into_iter()
+        .filter(|c| map[c.y][c.x] != State::Wall)
+        .collect()
 }
 
 fn maybe_teleport(maze: &Maze, pos: Coord) -> Coord {
     // move out of teleport position - teleports only have one open adjacent position
     maze.teleports
         .get(&pos)
-        .map(|dest| open_adjacents(maze, *dest)[0])
+        .map(|&dest| open_adjacents(&maze.map, dest)[0])
         .unwrap_or(pos)
 }
 
 fn flood_fill(maze: &Maze) -> usize {
-    let mut to_search: Vec<_> = open_adjacents(maze, maze.start);
+    let mut to_search: Vec<_> = open_adjacents(&maze.map, maze.start);
     let mut searched: HashSet<Coord> = HashSet::new();
     searched.insert(maze.start);
+
     let mut iteration_count = 0;
     while !to_search.is_empty() {
         if to_search.contains(&maze.end) {
@@ -135,7 +150,7 @@ fn flood_fill(maze: &Maze) -> usize {
         let next_to_search: Vec<_> = to_search
             .iter()
             .flat_map(|&c| {
-                open_adjacents(maze, c)
+                open_adjacents(&maze.map, c)
                     .iter()
                     .map(|&c| maybe_teleport(maze, c))
                     .filter(|d| !searched.contains(d))
@@ -148,24 +163,68 @@ fn flood_fill(maze: &Maze) -> usize {
     iteration_count - 1 // first step doesn't count (should start at position adjacent)
 }
 
-fn main() {
-    let input_lines: Vec<_> = BufReader::new(
-        File::open(
-            env::args()
-                .nth(1)
-                .expect("Incorrect number of arguments provided"),
-        )
-        .expect("Could not open input file"),
-    )
-    .lines()
-    .map(|l| l.unwrap().as_bytes().to_vec())
-    .collect();
+fn flood_fill_with_depth(maze: &Maze) -> usize {
+    let mut to_search: Vec<(Coord, usize)> = open_adjacents(&maze.map, maze.start)
+        .iter()
+        .map(|c| (*c, 0))
+        .collect();
+    let mut searched: HashSet<(Coord, usize)> = HashSet::new();
+    searched.insert((maze.start, 0));
 
-    let maze = parse_map(&input_lines);
+    let mut iteration_count = 0;
+    while !to_search.is_empty() {
+        if to_search.iter().any(|&(c, n)| c == maze.end && n == 0) {
+            // first step doesn't count (should start at position adjacent)
+            return iteration_count - 1;
+        }
+        searched.extend(to_search.clone());
+        let next_to_search: Vec<_> = to_search
+            .iter()
+            .flat_map(|&(c, n)| {
+                open_adjacents(&maze.map, c)
+                    .iter()
+                    .filter_map(|&c| {
+                        if maze.teleports.contains_key(&c) {
+                            if n == 0 && maze.outer_teleports.contains(&c) {
+                                None
+                            } else {
+                                let new_n = if maze.outer_teleports.contains(&c) {
+                                    n - 1
+                                } else {
+                                    n + 1
+                                };
+                                Some((maybe_teleport(maze, c), new_n))
+                            }
+                        } else {
+                            Some((c, n))
+                        }
+                    })
+                    .filter(|coord_depth| !searched.contains(coord_depth))
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        to_search = next_to_search;
+        iteration_count += 1;
+    }
+    unreachable!("Run out of places to search!");
+}
+
+fn main() -> io::Result<()> {
+    let input_str: String =
+        fs::read_to_string(env::args().nth(1).expect("Incorrect number of arguments"))?;
+
+    let maze = parse_map(&input_str);
 
     let num_steps = flood_fill(&maze);
     println!(
         "Number of steps needed to reach the end goal: {}",
         num_steps
     );
+
+    let num_recursive_steps = flood_fill_with_depth(&maze);
+    println!(
+        "Number of steps needed to reach the end goal with recursion: {}",
+        num_recursive_steps
+    );
+    Ok(())
 }
