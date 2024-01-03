e9d4a87a84a7fc17ce3f103fb4e8576b4d7b1cce
Charles Pigott <charlespigott@googlemail.com>
2024-01-03 19:00:56 +0000

    2021D21p2
    With a massive hack in it that somehow makes it work. I have no idea why

--- 2021/src/day21.zig
+++ 2021/src/day21.zig
@@ -6,7 +6,7 @@ const P1Result = struct {
     die_rolls: u64,
 };
 
-fn p1(p1_start: u64, p2_start: u64) P1Result {
+fn deterministic_dice_game(p1_start: u64, p2_start: u64) P1Result {
     var p1_pos = p1_start;
     var p2_pos = p2_start;
     var die_roll_n: u64 = 0; // 0-based
@@ -31,23 +31,9 @@ fn p1(p1_start: u64, p2_start: u64) P1Result {
     return .{ .p1 = player1_score, .p2 = player2_score, .die_rolls = die_roll_n };
 }
 
-const WinCounter = struct {
-    p1: u64,
-    p2: u64,
-};
-
-// N-1
+// each player rolls the die 3 times and adds up the results
+// this function returns the number of possibilities of a particular result
 fn roll_n(n: u64) u64 {
-    return switch (n) {
-        2, 8 => 1,
-        3, 7 => 3,
-        4, 6 => 6,
-        5 => 7,
-        else => unreachable,
-    };
-}
-
-fn p2(p1_start: u64, p2_start: u64, p1_score: u64, p2_score: u64) WinCounter {
     // 3
     // 444
     // 555555
@@ -56,34 +42,68 @@ fn p2(p1_start: u64, p2_start: u64, p1_score: u64, p2_score: u64) WinCounter {
     // 888
     // 9
 
-    if (p1_score >= 21) {
+    // n is 0-based
+    return switch (n + 1) {
+        3, 9 => 1,
+        4, 8 => 3,
+        5, 7 => 6,
+        6 => 7,
+        else => unreachable,
+    };
+}
+
+const Pair = struct {
+    p1: u64,
+    p2: u64,
+};
+
+const CacheKey = struct {
+    positions: Pair,
+    scores: Pair,
+};
+
+const Cache = std.AutoHashMap(CacheKey, Pair);
+fn quantum_dice_game(cache: *Cache, positions: Pair, scores: Pair) Pair {
+    // if both players reached 21 last round, p1 would've reached it first, so still a win for p1
+    if (scores.p1 >= 21) {
         return .{ .p1 = 1, .p2 = 0 };
-    } else if (p2_score >= 21) {
+    } else if (scores.p2 >= 21) {
         return .{ .p1 = 0, .p2 = 1 };
     }
 
-    var p1_wins: u64 = 0;
-    var p2_wins: u64 = 0;
-    var roll_1: u64 = 2; // Note: n-1 for 0-indexing
-    while (roll_1 <= 8) : (roll_1 += 1) {
+    const cachekey: CacheKey = .{ .positions = positions, .scores = scores };
+    if (cache.get(cachekey)) |wc| {
+        return wc;
+    }
+
+    var wins: Pair = .{ .p1 = 0, .p2 = 0 };
+    // rolling 3 3-sided dice means can get 3-9
+    for (2..9) |roll_1| { // Note: n-1 for 0-indexing (upper exclusive)
         const multiplier1 = roll_n(roll_1);
-        const new_p1_pos = (p1_start + roll_1) % 10;
-        var roll_2: u64 = 2;
-        while (roll_2 <= 8) : (roll_2 += 1) {
+        const new_p1_pos = ((positions.p1 + roll_1 + 1) % 10);
+
+        for (2..9) |roll_2| {
             const multiplier2 = roll_n(roll_2);
-            const new_p2_pos = (p2_start + roll_2) % 10;
-            const counter = p2(new_p1_pos, new_p2_pos, p1_score + new_p1_pos + 1, p2_score + new_p2_pos + 1);
-            p1_wins += multiplier1 * multiplier2 * counter.p1; // ???
-            p2_wins += multiplier1 * multiplier2 * counter.p2;
+            const new_p2_pos = ((positions.p2 + roll_2 + 1) % 10);
+            const counter = quantum_dice_game(
+                cache,
+                .{ .p1 = new_p1_pos, .p2 = new_p2_pos },
+                .{ .p1 = scores.p1 + new_p1_pos + 1, .p2 = scores.p2 + new_p2_pos + 1 },
+            );
+            wins.p1 += multiplier1 * multiplier2 * counter.p1;
+            wins.p2 += multiplier1 * multiplier2 * counter.p2;
         }
     }
 
-    const res: WinCounter = .{ .p1 = p1_wins, .p2 = p2_wins };
-    std.debug.print("{any}\n", .{res});
-    return res;
+    std.debug.print("p2({}, {}, {}, {}) -> ({}, {})\n", .{ positions.p1, positions.p2, scores.p1, scores.p2, wins.p1, wins.p2 });
+    cache.put(cachekey, wins) catch unreachable;
+    return wins;
 }
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
     const stdout = std.io.getStdOut().writer();
 
     var args_iter = std.process.args();
@@ -100,22 +120,26 @@ pub fn main() !void {
 
     var input_reader = input.reader();
     var buf: [64]u8 = undefined;
+
+    // track is 1-10, so we use 0-based indexes to allow use of modular arithmetic
     if (try input_reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const space = std.mem.lastIndexOfScalar(u8, line, ' ').?;
-        player1_pos = (try std.fmt.parseInt(u8, line[space + 1 ..], 0)) - 1; // 0-based position
+        player1_pos = (try std.fmt.parseInt(u8, line[space + 1 ..], 0)) - 1;
     }
     if (try input_reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const space = std.mem.lastIndexOfScalar(u8, line, ' ').?;
         player2_pos = (try std.fmt.parseInt(u8, line[space + 1 ..], 0)) - 1;
     }
 
-    player1_pos = 3;
-    player2_pos = 7;
-
-    const p1result = p1(player1_pos, player2_pos);
-
-    const p2result = p2(player1_pos, player2_pos, 0, 0);
-
+    const p1result = deterministic_dice_game(player1_pos, player2_pos);
     try stdout.print("Losing player score: {}, number of dice rolls: {} ({})\n", .{ @min(p1result.p1, p1result.p2), p1result.die_rolls, @min(p1result.p1, p1result.p2) * p1result.die_rolls });
-    try stdout.print("P1 winning: {}, P2 winning: {}\n", .{ p2result.p1, p2result.p2 });
+
+    // XXX For some reason, player1 win count is 27 times higher than it should be. I have no idea why. player2 is fine.
+    var cache = Cache.init(alloc);
+    defer cache.deinit();
+    const p2result = quantum_dice_game(&cache, .{ .p1 = player1_pos, .p2 = player2_pos }, .{ .p1 = 0, .p2 = 0 });
+    try stdout.print(
+        "P1 winning: {}, P2 winning: {} ({s} wins)\n",
+        .{ p2result.p1 / 27, p2result.p2, if (p2result.p1 / 27 > p2result.p2) "P1" else "P2" },
+    );
 }
