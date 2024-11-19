28c3b00fd8d4cfe4a11fb531fd6fabe2c0e856d6
Charles Pigott <charlespigott@googlemail.com>
2024-11-19 19:16:52 +0000

    Q11 tidying

--- everybody-codes/2024/src/bin/quest11.rs
+++ everybody-codes/2024/src/bin/quest11.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 use std::io;
@@ -6,26 +7,29 @@ fn run_day<'a>(
     termite_map: &HashMap<&'a str, Vec<&'a str>>,
     termites: &HashMap<&'a str, usize>,
 ) -> HashMap<&'a str, usize> {
-    let mut new_numbers: HashMap<&str, usize> = termite_map.keys().map(|t| (*t, 0)).collect();
+    let mut next_gen = HashMap::with_capacity(termites.len());
     for (t, val) in termites {
-        for spawn in termite_map.get(t).unwrap() {
-            new_numbers.entry(spawn).and_modify(|cur_val| {
-                *cur_val += val;
-            });
+        for spawn in &termite_map[t] {
+            *next_gen.entry(*spawn).or_insert(0) += val;
         }
     }
-    new_numbers
+    next_gen
+}
+
+fn run_simulation(termite_map: &HashMap<&str, Vec<&str>>, num_days: usize, start: &str) -> usize {
+    let mut termites = HashMap::from([(start, 1)]);
+    for _ in 0..num_days {
+        termites = run_day(termite_map, &termites);
+    }
+    termites.values().sum()
 }
 
 fn read_termite_map(input: &str) -> HashMap<&str, Vec<&str>> {
     input
         .lines()
         .map(|l| {
-            let mut spl = l.split(':');
-            (
-                spl.next().unwrap(),
-                spl.next().unwrap().split(",").collect::<Vec<_>>(),
-            )
+            let (parent, children) = l.split_once(':').unwrap();
+            (parent, children.split(",").collect::<Vec<_>>())
         })
         .collect()
 }
@@ -36,39 +40,23 @@ fn main() -> io::Result<()> {
 
     let p1_input_str = fs::read_to_string(p1_input_filename)?;
     let p1_input_map = read_termite_map(&p1_input_str);
-    let mut termites = HashMap::from([("A", 1)]);
-    for _ in 0..4 {
-        termites = run_day(&p1_input_map, &termites);
-    }
-    println!(
-        "P1: Number of termites: {}",
-        termites.values().sum::<usize>()
-    );
+    let p1_pop_sum = run_simulation(&p1_input_map, 4, "A");
+    println!("P1: Number of termites: {p1_pop_sum}");
 
     let p2_input_str = fs::read_to_string(p2_input_filename)?;
     let p2_input_map = read_termite_map(&p2_input_str);
-    let mut termites = HashMap::from([("Z", 1)]);
-    for _ in 0..10 {
-        termites = run_day(&p2_input_map, &termites);
-    }
-    println!(
-        "P2: Number of termites: {}",
-        termites.values().sum::<usize>()
-    );
+    let p2_pop_sum = run_simulation(&p2_input_map, 10, "Z");
+    println!("P2: Number of termites: {p2_pop_sum}");
 
     let p3_input_str = fs::read_to_string(p3_input_filename)?;
     let p3_input_map = read_termite_map(&p3_input_str);
-    let mut min_pop = usize::MAX;
-    let mut max_pop = usize::MIN;
-    for k in p3_input_map.keys() {
-        let mut termites = HashMap::from([(*k, 1)]);
-        for _ in 0..20 {
-            termites = run_day(&p3_input_map, &termites);
-        }
-        let total_pop = termites.values().sum();
-        min_pop = usize::min(min_pop, total_pop);
-        max_pop = usize::max(max_pop, total_pop);
-    }
+    let (min_pop, max_pop) = p3_input_map
+        .keys()
+        .map(|k| run_simulation(&p3_input_map, 20, k))
+        .minmax()
+        .into_option()
+        .unwrap();
+
     println!(
         "P3: Termite population difference: {} ({} - {})",
         max_pop - min_pop,
@@ -92,7 +80,7 @@ mod tests {
         ]);
         let mut termites = HashMap::from([("A", 1)]);
         termites = run_day(&input_map, &termites);
-        let expected = HashMap::from([("A", 0), ("B", 1), ("C", 1)]);
+        let expected = HashMap::from([("B", 1), ("C", 1)]);
         assert_eq!(termites, expected);
     }
 }
