a9cb9cb23c3070e4e134a5cdc0b6ac30c14bb07b
Charles Pigott <charlespigott@googlemail.com>
2024-11-24 14:40:34 +0000

    Q14

---
+++ everybody-codes/2024/src/bin/quest14.rs
@@ -0,0 +1,263 @@
+use std::collections::{HashSet, VecDeque};
+use std::fs;
+use std::io;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
+struct Coord {
+    x: isize,
+    y: isize,
+    z: isize,
+}
+
+#[derive(Debug)]
+struct PathNode {
+    c: Coord,
+    path_len: usize,
+}
+
+fn get_distance_bfs(branch_grid: &HashSet<Coord>, t: Coord, l: Coord) -> usize {
+    let mut to_search = VecDeque::<PathNode>::new();
+    to_search.push_back(PathNode { c: t, path_len: 0 });
+    let mut searched = HashSet::<Coord>::new();
+    while !to_search.is_empty() {
+        let Some(PathNode {
+            c: node,
+            path_len: node_distance,
+        }) = to_search.pop_front()
+        else {
+            unreachable!()
+        };
+        if node == l {
+            return node_distance;
+        }
+
+        if searched.contains(&node) {
+            continue;
+        }
+
+        searched.insert(node);
+
+        let node_u = Coord {
+            x: node.x + 1,
+            y: node.y,
+            z: node.z,
+        };
+        let node_d = Coord {
+            x: node.x - 1,
+            y: node.y,
+            z: node.z,
+        };
+        let node_r = Coord {
+            x: node.x,
+            y: node.y + 1,
+            z: node.z,
+        };
+        let node_l = Coord {
+            x: node.x,
+            y: node.y - 1,
+            z: node.z,
+        };
+        let node_f = Coord {
+            x: node.x,
+            y: node.y,
+            z: node.z + 1,
+        };
+        let node_b = Coord {
+            x: node.x,
+            y: node.y,
+            z: node.z - 1,
+        };
+        if branch_grid.contains(&node_u) {
+            to_search.push_back(PathNode {
+                c: node_u,
+                path_len: node_distance + 1,
+            });
+        }
+        if branch_grid.contains(&node_d) {
+            to_search.push_back(PathNode {
+                c: node_d,
+                path_len: node_distance + 1,
+            });
+        }
+        if branch_grid.contains(&node_r) {
+            to_search.push_back(PathNode {
+                c: node_r,
+                path_len: node_distance + 1,
+            });
+        }
+        if branch_grid.contains(&node_l) {
+            to_search.push_back(PathNode {
+                c: node_l,
+                path_len: node_distance + 1,
+            });
+        }
+        if branch_grid.contains(&node_f) {
+            to_search.push_back(PathNode {
+                c: node_f,
+                path_len: node_distance + 1,
+            });
+        }
+        if branch_grid.contains(&node_b) {
+            to_search.push_back(PathNode {
+                c: node_b,
+                path_len: node_distance + 1,
+            });
+        }
+    }
+    unreachable!(); // all nodes are reachable eventually
+}
+
+fn parse_instructions(input: &str) -> Vec<Vec<(u8, isize)>> {
+    input
+        .lines()
+        .map(|l| {
+            l.split(",")
+                .map(|instr| (instr.as_bytes()[0], instr[1..].parse::<isize>().unwrap()))
+                .collect()
+        })
+        .collect()
+}
+
+fn grow_tree(instrs: &[(u8, isize)]) -> Vec<Coord> {
+    instrs
+        .iter()
+        .scan(Coord { x: 0, y: 0, z: 0 }, |pos, instr| {
+            let new_positions: Vec<_> = (1..=instr.1)
+                .map(|n| match instr.0 {
+                    b'U' => Coord {
+                        x: pos.x,
+                        y: pos.y + n,
+                        z: pos.z,
+                    },
+                    b'D' => Coord {
+                        x: pos.x,
+                        y: pos.y - n,
+                        z: pos.z,
+                    },
+                    b'R' => Coord {
+                        x: pos.x + n,
+                        y: pos.y,
+                        z: pos.z,
+                    },
+                    b'L' => Coord {
+                        x: pos.x - n,
+                        y: pos.y,
+                        z: pos.z,
+                    },
+                    b'F' => Coord {
+                        x: pos.x,
+                        y: pos.y,
+                        z: pos.z + n,
+                    },
+                    b'B' => Coord {
+                        x: pos.x,
+                        y: pos.y,
+                        z: pos.z - n,
+                    },
+                    _ => unreachable!(),
+                })
+                .collect();
+            *pos = *new_positions.last().unwrap();
+            Some(new_positions)
+        })
+        .flatten()
+        .collect()
+}
+
+fn get_min_sap_murkiness(branch_instrs: &[Vec<(u8, isize)>]) -> usize {
+    let all_tree_segments: Vec<_> = branch_instrs
+        .iter()
+        .map(|instrs| grow_tree(instrs))
+        .collect();
+    let unique_tree_segments: HashSet<_> = all_tree_segments.iter().flatten().cloned().collect();
+
+    let leaf_segments: Vec<_> = all_tree_segments
+        .iter()
+        .map(|branch| branch.last().unwrap())
+        .collect();
+    let trunk_segments: HashSet<_> = unique_tree_segments
+        .iter()
+        .filter(|c| c.x == 0 && c.z == 0)
+        .collect();
+
+    trunk_segments
+        .iter()
+        .map(|&t| {
+            leaf_segments
+                .iter()
+                .map(|&l| get_distance_bfs(&unique_tree_segments, *t, *l))
+                .sum()
+        })
+        .min()
+        .unwrap()
+}
+
+fn main() -> io::Result<()> {
+    let (p1_input_filename, p2_input_filename, p3_input_filename) =
+        everybody_codes::get_input_files()?;
+
+    let p1_instrs = &parse_instructions(&fs::read_to_string(p1_input_filename)?)[0];
+    let p1_tree_segments = grow_tree(p1_instrs);
+    let p1_max_height = p1_tree_segments.iter().max_by_key(|c| c.y).unwrap().y;
+
+    println!("P1: Plant final height: {p1_max_height}");
+
+    let p2_branch_instrs = &parse_instructions(&fs::read_to_string(p2_input_filename)?);
+    let p2_tree_segments: HashSet<_> = p2_branch_instrs
+        .iter()
+        .flat_map(|instrs| grow_tree(instrs))
+        .collect();
+    println!("P2: Total unique segments: {}", p2_tree_segments.len());
+
+    let p3_branch_instrs = parse_instructions(&fs::read_to_string(p3_input_filename)?);
+    let p3_min_murkiness_level: usize = get_min_sap_murkiness(&p3_branch_instrs);
+    println!("P3: Minimum sap murkiness: {p3_min_murkiness_level}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn ex1() {
+        let instrs = &parse_instructions("U5,R3,D2,L5,U4,R5,D2")[0];
+        let tree_idxs = grow_tree(&instrs);
+        let max_height = tree_idxs.iter().max_by_key(|c| c.y).unwrap().y;
+        assert_eq!(max_height, 7);
+    }
+
+    #[test]
+    fn ex2() {
+        let input = "U5,R3,D2,L5,U4,R5,D2
+U6,L1,D2,R3,U2,L1";
+        let branch_instrs = parse_instructions(input);
+        let tree_segments: HashSet<_> = branch_instrs
+            .iter()
+            .flat_map(|instrs| grow_tree(&instrs))
+            .collect();
+        assert_eq!(tree_segments.len(), 32);
+    }
+
+    #[test]
+    fn ex3() {
+        let input = "U5,R3,D2,L5,U4,R5,D2
+U6,L1,D2,R3,U2,L1";
+        let branch_instrs = parse_instructions(input);
+        let min_murkiness_level: usize = get_min_sap_murkiness(&branch_instrs);
+        assert_eq!(min_murkiness_level, 5);
+    }
+
+    #[test]
+    fn ex3b() {
+        let input = "U20,L1,B1,L2,B1,R2,L1,F1,U1
+U10,F1,B1,R1,L1,B1,L1,F1,R2,U1
+U30,L2,F1,R1,B1,R1,F2,U1,F1
+U25,R1,L2,B1,U1,R2,F1,L2
+U16,L1,B1,L1,B3,L1,B1,F1";
+        let branch_instrs = parse_instructions(input);
+        let min_murkiness_level: usize = get_min_sap_murkiness(&branch_instrs);
+        assert_eq!(min_murkiness_level, 46);
+    }
+}
