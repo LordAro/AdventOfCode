2776ae3abb7ca72c37aab5bbe5ff76f0da18a05b
Charles Pigott <charlespigott@googlemail.com>
2024-12-04 07:22:21 +0000

    2024D4 minor tidying

--- 2024/src/bin/day4.rs
+++ 2024/src/bin/day4.rs
@@ -4,45 +4,46 @@ use std::io;
 
 fn find_xmas(grid: &[Vec<u8>]) -> (usize, usize) {
     let xmas = "XMAS".as_bytes();
-    let rev_xmas: Vec<_> = xmas.iter().rev().copied().collect();
+    let samx = "SAMX".as_bytes();
 
     let mut xmas_count = 0;
     let mut x_mas_count = 0;
     for y in 0..grid.len() {
         for x in 0..grid[y].len() {
             // -
-            if x <= grid[y].len() - xmas.len() {
-                let subword_h: Vec<_> = (0..xmas.len()).map(|n| grid[y][x + n]).collect();
-                if subword_h == xmas || subword_h == rev_xmas {
+            if x <= grid[y].len() - 4 {
+                let subword_h: Vec<_> = (0..4).map(|n| grid[y][x + n]).collect();
+                if subword_h == xmas || subword_h == samx {
                     xmas_count += 1;
                 }
             }
             // |
-            if y <= grid.len() - xmas.len() {
-                let subword_v: Vec<_> = (0..xmas.len()).map(|n| grid[y + n][x]).collect();
-                if subword_v == xmas || subword_v == rev_xmas {
+            if y <= grid.len() - 4 {
+                let subword_v: Vec<_> = (0..4).map(|n| grid[y + n][x]).collect();
+                if subword_v == xmas || subword_v == samx {
                     xmas_count += 1;
                 }
             }
             // \
-            if y <= grid.len() - xmas.len() && x <= grid[y].len() - xmas.len() {
-                let subword_d1: Vec<_> = (0..xmas.len()).map(|n| grid[y + n][x + n]).collect();
-                if subword_d1 == xmas || subword_d1 == rev_xmas {
+            if y <= grid.len() - 4 && x <= grid[y].len() - 4 {
+                let subword_d1: Vec<_> = (0..4).map(|n| grid[y + n][x + n]).collect();
+                if subword_d1 == xmas || subword_d1 == samx {
                     xmas_count += 1;
                 }
             }
             // /
-            if y >= xmas.len() - 1 && x <= grid[y].len() - xmas.len() {
-                let subword_d2: Vec<_> = (0..xmas.len()).map(|n| grid[y - n][x + n]).collect();
-                if subword_d2 == xmas || subword_d2 == rev_xmas {
+            if y >= 4 - 1 && x <= grid[y].len() - 4 {
+                let subword_d2: Vec<_> = (0..4).map(|n| grid[y - n][x + n]).collect();
+                if subword_d2 == xmas || subword_d2 == samx {
                     xmas_count += 1;
                 }
             }
+            // P2
             if y >= 1 && x >= 1 && y < grid.len() - 1 && x < grid[y].len() - 1 {
                 let submas_d1: Vec<_> = (0..3).map(|n| grid[y + n - 1][x + n - 1]).collect();
                 let submas_d2: Vec<_> = (0..3).map(|n| grid[y + 1 - n][x + n - 1]).collect();
-                if (submas_d1 == xmas[1..] || submas_d1 == rev_xmas.as_slice()[0..3])
-                    && (submas_d2 == xmas[1..] || submas_d2 == rev_xmas.as_slice()[0..3])
+                if (submas_d1 == "MAS".as_bytes() || submas_d1 == "SAM".as_bytes())
+                    && (submas_d2 == "MAS".as_bytes() || submas_d2 == "SAM".as_bytes())
                 {
                     x_mas_count += 1;
                 }
@@ -61,7 +62,7 @@ fn main() -> io::Result<()> {
 
     let (xmas_count, x_mas_count) = find_xmas(&input);
     println!("P1: Number of 'XMAS' words: {xmas_count}");
-    println!("P1: Number of 'X-MAS' words: {x_mas_count}");
+    println!("P2: Number of 'X-MAS' words: {x_mas_count}");
     Ok(())
 }
 
