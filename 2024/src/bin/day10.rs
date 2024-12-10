a75b845328c9c1d03269778d9208dbc3f84e3398
Charles Pigott <charlespigott@googlemail.com>
2024-12-10 09:53:04 +0000

    2024 Day 10

---
+++ 2024/src/bin/day10.rs
@@ -0,0 +1,98 @@
+use std::env;
+use std::fs;
+use std::io;
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, Ord, PartialOrd)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
+fn get_grid(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect()
+}
+
+fn count_trails(grid: &[Vec<u32>], start: Coord) -> (usize, usize) {
+    let mut to_search = vec![start];
+    let mut trail_ends: Vec<Coord> = vec![];
+    while !to_search.is_empty() {
+        let c = to_search.remove(0); // vecdeque?
+        let cur_level = grid[c.y][c.x];
+        if cur_level == 9 {
+            trail_ends.push(c);
+            continue;
+        }
+
+        for n in [
+            c.y.checked_sub(1).map(|y| Coord { x: c.x, y }),
+            Some(c.y + 1)
+                .filter(|y| *y < grid.len())
+                .map(|y| Coord { x: c.x, y }),
+            c.x.checked_sub(1).map(|x| Coord { x, y: c.y }),
+            Some(c.x + 1)
+                .filter(|x| *x < grid[c.y].len())
+                .map(|x| Coord { x, y: c.y }),
+        ]
+        .iter()
+        .flatten()
+        .filter(|next| grid[next.y][next.x] == cur_level + 1)
+        {
+            to_search.push(*n);
+        }
+    }
+    let total_trails = trail_ends.len();
+    trail_ends.sort();
+    trail_ends.dedup();
+    let unique_trails = trail_ends.len();
+    (unique_trails, total_trails)
+}
+
+fn get_all_trails(grid: &[Vec<u32>]) -> (usize, usize) {
+    let mut unique_trail_sum = 0;
+    let mut distinct_trail_sum = 0;
+    for (y, row) in grid.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if *c == 0 {
+                let (p1, p2) = count_trails(grid, Coord { x, y });
+                unique_trail_sum += p1;
+                distinct_trail_sum += p2;
+            }
+        }
+    }
+    (unique_trail_sum, distinct_trail_sum)
+}
+
+fn main() -> io::Result<()> {
+    let input: Vec<Vec<u32>> = get_grid(&fs::read_to_string(
+        env::args().nth(1).expect("missing cli argument"),
+    )?);
+
+    let (p1, p2) = get_all_trails(&input);
+    println!("P1: Unique trail route sum: {p1}");
+    println!("P2: Distinct trail route sum: {p2}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn ex1() {
+        let input = get_grid(
+            "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732",
+        );
+
+        assert_eq!(get_all_trails(&input).0, 36);
+    }
+}
