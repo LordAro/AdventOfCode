d8c26f8f4705e4975196b872568c1a7dd49f05ee
Charles Pigott <charlespigott@googlemail.com>
2024-12-11 08:52:06 +0000

    2024D10 minor optimisations

--- 2024/src/bin/day10.rs
+++ 2024/src/bin/day10.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::env;
 use std::fs;
 use std::io;
@@ -8,20 +9,16 @@ struct Coord {
     y: usize,
 }
 
-fn get_grid(input: &str) -> Vec<Vec<u32>> {
-    input
-        .lines()
-        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
-        .collect()
+fn get_grid(input: &str) -> Vec<Vec<u8>> {
+    input.lines().map(|l| l.bytes().collect()).collect()
 }
 
-fn count_trails(grid: &[Vec<u32>], start: Coord) -> (usize, usize) {
-    let mut to_search = vec![start];
+fn count_trails(grid: &[Vec<u8>], start: Coord) -> (usize, usize) {
+    let mut to_search = VecDeque::from([start]);
     let mut trail_ends: Vec<Coord> = vec![];
-    while !to_search.is_empty() {
-        let c = to_search.remove(0); // vecdeque?
+    while let Some(c) = to_search.pop_front() {
         let cur_level = grid[c.y][c.x];
-        if cur_level == 9 {
+        if cur_level == b'9' {
             trail_ends.push(c);
             continue;
         }
@@ -40,7 +37,7 @@ fn count_trails(grid: &[Vec<u32>], start: Coord) -> (usize, usize) {
         .flatten()
         .filter(|next| grid[next.y][next.x] == cur_level + 1)
         {
-            to_search.push(*n);
+            to_search.push_back(*n);
         }
     }
     let total_trails = trail_ends.len();
@@ -50,12 +47,12 @@ fn count_trails(grid: &[Vec<u32>], start: Coord) -> (usize, usize) {
     (unique_trails, total_trails)
 }
 
-fn get_all_trails(grid: &[Vec<u32>]) -> (usize, usize) {
+fn get_all_trails(grid: &[Vec<u8>]) -> (usize, usize) {
     let mut unique_trail_sum = 0;
     let mut distinct_trail_sum = 0;
     for (y, row) in grid.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
-            if *c == 0 {
+            if *c == b'0' {
                 let (p1, p2) = count_trails(grid, Coord { x, y });
                 unique_trail_sum += p1;
                 distinct_trail_sum += p2;
@@ -66,7 +63,7 @@ fn get_all_trails(grid: &[Vec<u32>]) -> (usize, usize) {
 }
 
 fn main() -> io::Result<()> {
-    let input: Vec<Vec<u32>> = get_grid(&fs::read_to_string(
+    let input: Vec<Vec<u8>> = get_grid(&fs::read_to_string(
         env::args().nth(1).expect("missing cli argument"),
     )?);
 
