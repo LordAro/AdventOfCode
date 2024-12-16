426cdb4d76058dfeb4d2e4d3ec6165314e19afb4
Charles Pigott <charlespigott@googlemail.com>
2024-12-16 14:13:18 +0000

    2024 Day 15 part 2

--- 2024/src/bin/day15.rs
+++ 2024/src/bin/day15.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io;
@@ -8,10 +9,12 @@ struct Coord {
     y: usize,
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Cell {
     Space,
     Box,
+    LeftBox,
+    RightBox,
     Robot,
     Wall,
 }
@@ -24,9 +27,28 @@ fn parse_grid(input: &str) -> Vec<Vec<Cell>> {
                 .map(|c| match c {
                     '.' => Cell::Space,
                     'O' => Cell::Box,
+                    '[' => Cell::LeftBox,
+                    ']' => Cell::RightBox,
                     '#' => Cell::Wall,
                     '@' => Cell::Robot,
                     _ => unreachable!(),
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn expand_grid(initial_grid: &[Vec<Cell>]) -> Vec<Vec<Cell>> {
+    initial_grid
+        .iter()
+        .map(|row| {
+            row.iter()
+                .flat_map(|c| match c {
+                    Cell::Space => [Cell::Space, Cell::Space],
+                    Cell::Box => [Cell::LeftBox, Cell::RightBox],
+                    Cell::Wall => [Cell::Wall, Cell::Wall],
+                    Cell::Robot => [Cell::Robot, Cell::Space],
+                    Cell::LeftBox | Cell::RightBox => unreachable!(),
                 })
                 .collect()
         })
@@ -40,47 +62,93 @@ fn do_move(grid: &mut [Vec<Cell>], robot_pos: &mut Coord, instr: char) {
     }
     assert!(grid[robot_pos.y][robot_pos.x] == Cell::Robot);
 
-    let mut move_list = vec![];
-    let mut coord = *robot_pos;
-    while grid[coord.y][coord.x] != Cell::Space {
-        if grid[coord.y][coord.x] == Cell::Wall {
-            // Can't move, return early
-            return;
+    let mut coords_to_move = vec![HashSet::from([*robot_pos])];
+
+    while !coords_to_move.last().unwrap().is_empty() {
+        // Use a hashset to prevent duplicates (e.g. both left and right adding left and right each)
+        let mut try_move = HashSet::default();
+        for coord in coords_to_move.last().unwrap() {
+            let target_coord = match instr {
+                '^' => Coord {
+                    x: coord.x,
+                    y: coord.y - 1,
+                },
+                '>' => Coord {
+                    x: coord.x + 1,
+                    y: coord.y,
+                },
+                'v' => Coord {
+                    x: coord.x,
+                    y: coord.y + 1,
+                },
+                '<' => Coord {
+                    x: coord.x - 1,
+                    y: coord.y,
+                },
+                _ => unreachable!("Got unexpected direction: {}", instr as u8),
+            };
+
+            match grid[target_coord.y][target_coord.x] {
+                // Can't move, return early
+                Cell::Wall => return,
+                Cell::Box => _ = try_move.insert(target_coord),
+                Cell::LeftBox => {
+                    try_move.insert(target_coord);
+                    if instr == '^' || instr == 'v' {
+                        try_move.insert(Coord {
+                            x: target_coord.x + 1,
+                            y: target_coord.y,
+                        });
+                    }
+                }
+                Cell::RightBox => {
+                    try_move.insert(target_coord);
+                    if instr == '^' || instr == 'v' {
+                        try_move.insert(Coord {
+                            x: target_coord.x - 1,
+                            y: target_coord.y,
+                        });
+                    }
+                }
+                Cell::Space => (),
+                Cell::Robot => unreachable!(), // robot never tries to push itself
+            }
         }
-
-        move_list.push(coord);
-
-        coord = match instr {
-            '^' => Coord {
-                x: coord.x,
-                y: coord.y - 1,
-            },
-            '>' => Coord {
-                x: coord.x + 1,
-                y: coord.y,
-            },
-            'v' => Coord {
-                x: coord.x,
-                y: coord.y + 1,
-            },
-            '<' => Coord {
-                x: coord.x - 1,
-                y: coord.y,
-            },
-            _ => unreachable!("Got unexpected direction: {}", instr as u8),
-        };
+        coords_to_move.push(try_move);
     }
-    assert!(grid[coord.y][coord.x] == Cell::Space);
 
-    grid[robot_pos.y][robot_pos.x] = Cell::Space;
-    if move_list.len() == 1 {
-        grid[coord.y][coord.x] = Cell::Robot;
-        *robot_pos = coord;
-    } else {
-        grid[coord.y][coord.x] = Cell::Box;
-        let new_robot_pos = move_list[1];
-        grid[new_robot_pos.y][new_robot_pos.x] = Cell::Robot;
-        *robot_pos = new_robot_pos;
+    for line in coords_to_move.iter().rev() {
+        for coord in line {
+            let new_coord = match instr {
+                '^' => Coord {
+                    x: coord.x,
+                    y: coord.y - 1,
+                },
+                '>' => Coord {
+                    x: coord.x + 1,
+                    y: coord.y,
+                },
+                'v' => Coord {
+                    x: coord.x,
+                    y: coord.y + 1,
+                },
+                '<' => Coord {
+                    x: coord.x - 1,
+                    y: coord.y,
+                },
+                _ => unreachable!("Got unexpected direction: {}", instr as u8),
+            };
+
+            assert!(matches!(
+                grid[coord.y][coord.x],
+                Cell::Robot | Cell::Box | Cell::LeftBox | Cell::RightBox
+            ));
+            grid[new_coord.y][new_coord.x] = grid[coord.y][coord.x];
+            grid[coord.y][coord.x] = Cell::Space;
+            if grid[new_coord.y][new_coord.x] == Cell::Robot {
+                *robot_pos = new_coord;
+            }
+        }
     }
 }
 
@@ -88,22 +156,30 @@ fn do_move(grid: &mut [Vec<Cell>], robot_pos: &mut Coord, instr: char) {
 fn print_grid(grid: &[Vec<Cell>]) {
     for row in grid {
         for c in row {
-            print!("{}", match c {
-                Cell::Wall => '#',
-                Cell::Robot => '@',
-                Cell::Space => '.',
-                Cell::Box => 'O',
-            });
+            print!(
+                "{}",
+                match c {
+                    Cell::Wall => '#',
+                    Cell::Robot => '@',
+                    Cell::Space => '.',
+                    Cell::Box => 'O',
+                    Cell::LeftBox => '[',
+                    Cell::RightBox => ']',
+                }
+            );
         }
         println!();
     }
 }
 
 fn find_robot(grid: &[Vec<Cell>]) -> Coord {
-    grid.iter().enumerate().find_map(|(y, row)| {
-        let x = row.iter().position(|c| *c == Cell::Robot)?;
-        Some(Coord { x, y })
-    }).unwrap()
+    grid.iter()
+        .enumerate()
+        .find_map(|(y, row)| {
+            let x = row.iter().position(|c| *c == Cell::Robot)?;
+            Some(Coord { x, y })
+        })
+        .unwrap()
 }
 
 fn grid_gps_sum(grid: &[Vec<Cell>]) -> usize {
@@ -112,8 +188,10 @@ fn grid_gps_sum(grid: &[Vec<Cell>]) -> usize {
         .map(|(y, row)| {
             row.iter()
                 .enumerate()
-                .filter(|(_, c)| **c == Cell::Box)
-                .map(|(x, _)| 100 * y + x)
+                .map(|(x, c)| match c {
+                    Cell::Box | Cell::LeftBox => 100 * y + x,
+                    _ => 0,
+                })
                 .sum::<usize>()
         })
         .sum()
@@ -125,12 +203,22 @@ fn main() -> io::Result<()> {
         unreachable!()
     };
     let mut grid = parse_grid(grid_str);
+    let mut expanded_grid = expand_grid(&grid);
     let mut robot_pos = find_robot(&grid);
 
     for instr in move_instrs.chars() {
         do_move(&mut grid, &mut robot_pos, instr);
     }
     println!("P1: Box GPS coordinate sum: {}", grid_gps_sum(&grid));
+
+    let mut robot_pos = find_robot(&expanded_grid);
+    for instr in move_instrs.chars() {
+        do_move(&mut expanded_grid, &mut robot_pos, instr);
+    }
+    println!(
+        "P2: Wide box GPS coordinate sum: {}",
+        grid_gps_sum(&expanded_grid)
+    );
 
     Ok(())
 }
@@ -157,5 +245,37 @@ mod tests {
             print_grid(&grid);
         }
         assert_eq!(grid_gps_sum(&grid), 2028);
+    }
+
+    #[test]
+    fn ex2() {
+        let input_grid = "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########";
+        let input_instr = "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+        let mut grid = expand_grid(&parse_grid(&input_grid));
+        let mut robot_pos = find_robot(&grid);
+        for instr in input_instr.chars() {
+            println!("{}", instr);
+            do_move(&mut grid, &mut robot_pos, instr);
+            print_grid(&grid);
+        }
+        assert_eq!(grid_gps_sum(&grid), 9021);
     }
 }
