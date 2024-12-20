9ae6132490e49c6ace8444f7e1a0337710a77016
Charles Pigott <charlespigott@googlemail.com>
2024-12-20 14:26:02 +0000

    2024 Day 17 part 2

--- 2024/src/bin/day17.rs
+++ 2024/src/bin/day17.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::fs;
 use std::io;
 
-fn run_program(init_a: i64, init_b: i64, init_c: i64, program: &[i64], output_limit: Option<usize>) -> Vec<i64> {
+fn run_program(init_a: i64, init_b: i64, init_c: i64, program: &[i64]) -> Vec<i64> {
     let mut a = init_a;
     let mut b = init_b;
     let mut c = init_c;
@@ -11,7 +11,7 @@ fn run_program(init_a: i64, init_b: i64, init_c: i64, program: &[i64], output_li
 
     let mut output = vec![];
 
-    while ip < program.len() && output_limit.is_none_or(|ol| output.len() < ol) {
+    while ip < program.len() {
         let op = program[ip];
 
         let literal = program[ip + 1];
@@ -70,6 +70,23 @@ fn parse_program(s: &str) -> Vec<i64> {
         .collect()
 }
 
+fn find_a(program: &[i64]) -> i64 {
+    let mut to_search: Vec<_> = vec![0];
+    while let Some(p) = to_search.pop() {
+        for a in 0..8 {
+            let new_a = p * 8 + a;
+            let out = run_program(new_a, 0, 0, program);
+            if out == program {
+                return new_a;
+            }
+            if program[program.len() - out.len()..] == out {
+                to_search.push(new_a);
+            }
+        }
+    }
+    unreachable!()
+}
+
 fn main() -> io::Result<()> {
     let input = fs::read_to_string(env::args().nth(1).expect("missing cli argument"))?;
     let Some((register_str, program_str)) = input.split_once("\n\n") else {
@@ -79,17 +96,17 @@ fn main() -> io::Result<()> {
     let (init_a, init_b, init_c) = parse_registers(register_str);
     let program = parse_program(program_str);
 
-    let program_output = run_program(init_a, init_b, init_c, &program, None);
+    let program_output = run_program(init_a, init_b, init_c, &program);
 
     println!("P1: Program output: {}", program_output.iter().join(","));
 
-    for trial_a in 0.. {
-        let program_output = run_program(trial_a, init_b, init_c, &program, Some(program.len()));
-        if program_output == program {
-            println!("P2: Register A value: {trial_a}");
-            break;
-        }
-    }
+    // My program is:
+    // while a != 0:
+    //     print((((a % 8) ^ 3) ^ (a // (2 ** ((a % 8) ^ 3)))) % 8)
+    //     print((a ^ (a >> ((a & 7) ^ 3))) % 8) # simplified
+    //     a //= 8
+    let reversed_a = find_a(&program);
+    println!("P2: Value of A necessary to output a quine: {reversed_a}");
     Ok(())
 }
 
@@ -99,19 +116,19 @@ mod tests {
 
     #[test]
     fn ex1a() {
-        let program_output = run_program(10, 0, 0, &[5, 0, 5, 1, 5, 4], None);
+        let program_output = run_program(10, 0, 0, &[5, 0, 5, 1, 5, 4]);
         assert_eq!(program_output, [0, 1, 2]);
     }
 
     #[test]
     fn ex1b() {
-        let program_output = run_program(2024, 0, 0, &[0, 1, 5, 4, 3, 0], None);
+        let program_output = run_program(2024, 0, 0, &[0, 1, 5, 4, 3, 0]);
         assert_eq!(program_output, [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]);
     }
 
     #[test]
     fn ex1c() {
-        let program_output = run_program(729, 0, 0, &[0, 1, 5, 4, 3, 0], None);
+        let program_output = run_program(729, 0, 0, &[0, 1, 5, 4, 3, 0]);
         assert_eq!(program_output, [4, 6, 3, 5, 6, 3, 5, 2, 1, 0]);
     }
 }
