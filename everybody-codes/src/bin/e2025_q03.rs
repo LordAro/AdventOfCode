ec924fd5bb75b54cab7c6101ed8853d787e73045
Charles Pigott <charlespigott@googlemail.com>
2025-11-06 06:40:22 +0000

    Some tidying

--- everybody-codes/src/bin/e2025_q03.rs
+++ everybody-codes/src/bin/e2025_q03.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs;
 use std::io;
 
@@ -5,40 +6,38 @@ fn main() -> io::Result<()> {
     let (p1_input_filename, p2_input_filename, p3_input_filename) =
         everybody_codes::get_input_files()?;
 
-    let mut p1_input: Vec<_> = fs::read_to_string(p1_input_filename)?
+    let p1_total: usize = fs::read_to_string(p1_input_filename)?
         .split(',')
         .map(|s| s.parse::<usize>().unwrap())
-        .collect();
-    p1_input.sort();
-    p1_input.dedup();
-    let p1_total: usize = p1_input.iter().sum();
+        .sorted()
+        .dedup()
+        .sum();
 
-    let mut p2_input: Vec<_> = fs::read_to_string(p2_input_filename)?
+    let p2_total: usize = fs::read_to_string(p2_input_filename)?
         .split(',')
         .map(|s| s.parse::<usize>().unwrap())
-        .collect();
-    p2_input.sort();
-    p2_input.dedup();
-    let p2_total: usize = p2_input.iter().take(20).sum();
+        .sorted()
+        .dedup()
+        .take(20)
+        .sum();
 
     let mut p3_input: Vec<_> = fs::read_to_string(p3_input_filename)?
         .split(',')
         .map(|s| s.parse::<usize>().unwrap())
+        .sorted()
         .collect();
-    p3_input.sort();
+
     let mut num_sets = 0;
     while !p3_input.is_empty() {
         let mut prev = 0;
         // don't actually need the result
-        let _: Vec<_> = p3_input
-            .extract_if(.., |x| {
-                if *x > prev {
-                    prev = *x;
-                    return true;
-                }
-                false
-            })
-            .collect();
+        p3_input.retain(|x| {
+            if *x <= prev {
+                return true;
+            }
+            prev = *x;
+            false
+        });
         num_sets += 1;
     }
 
