ada7c715dcbdc28f6170ed72293d7f621eabae77
Charles Pigott <charlespigott@googlemail.com>
2025-11-08 00:33:27 +0000

    Some tidying

--- everybody-codes/src/bin/e2025_q05.rs
+++ everybody-codes/src/bin/e2025_q05.rs
@@ -21,6 +21,7 @@ fn build_fishbone(nums: &[usize]) -> Vec<Node> {
                 continue 'outer;
             }
         }
+        // Got to the end of the spine, add the number here
         bone.push(Node {
             num: n,
             left: None,
@@ -28,6 +29,14 @@ fn build_fishbone(nums: &[usize]) -> Vec<Node> {
         });
     }
     bone
+}
+
+fn parse_fishbone(input: &str) -> (usize, Vec<Node>) {
+    let (id, bone_str) = input.split_once(':').unwrap();
+    let nums: Vec<_> = bone_str.split(',').map(|n| n.parse().unwrap()).collect();
+
+    let bone = build_fishbone(&nums);
+    (id.parse().unwrap(), bone)
 }
 
 fn num_digits(n: usize) -> u32 {
@@ -40,6 +49,7 @@ fn get_quality(bone: &[Node]) -> usize {
 }
 
 fn get_node_num(node: &Node) -> usize {
+    // Overly complex, but means I can use flatten to fold over all remaining numbers
     iter::once(node.left)
         .chain(iter::once(Some(node.num)))
         .chain(iter::once(node.right))
@@ -52,46 +62,22 @@ fn main() -> io::Result<()> {
         everybody_codes::get_input_files()?;
 
     let p1_input_str = fs::read_to_string(p1_input_filename)?;
-    let (_, p1_input) = p1_input_str.split_once(':').unwrap();
-    let p1_nums: Vec<_> = p1_input
-        .split(',')
-        .map(|n| n.parse::<usize>().unwrap())
-        .collect();
-
-    let p1_bone = build_fishbone(&p1_nums);
+    let p1_bone = parse_fishbone(&p1_input_str).1;
     let p1_quality = get_quality(&p1_bone);
 
     let p2_input = fs::read_to_string(p2_input_filename)?;
     let p2_bone_qualities: Vec<_> = p2_input
         .lines()
-        .map(|l| {
-            let nums: Vec<_> = l
-                .split_once(':')
-                .unwrap()
-                .1
-                .split(',')
-                .map(|n| n.parse::<usize>().unwrap())
-                .collect();
-            get_quality(&build_fishbone(&nums))
-        })
+        .map(|l| get_quality(&parse_fishbone(l).1))
         .collect();
-
     let p2_worst_bone = p2_bone_qualities.iter().min().unwrap();
     let p2_best_bone = p2_bone_qualities.iter().max().unwrap();
     let p2_bone_diff = p2_best_bone - p2_worst_bone;
 
     let p3_input = fs::read_to_string(p3_input_filename)?;
-    let mut p3_bones: Vec<_> = p3_input
-        .lines()
-        .map(|l| {
-            let (id, num_str) = l.split_once(':').unwrap();
-            let nums: Vec<_> = num_str
-                .split(',')
-                .map(|n| n.parse::<usize>().unwrap())
-                .collect();
-            (id.parse::<usize>().unwrap(), build_fishbone(&nums))
-        })
-        .collect();
+    let mut p3_bones: Vec<_> = p3_input.lines().map(parse_fishbone).collect();
+    // Great big sort function! Should probably be defined on a type, but...
+    // Note the reverse order
     p3_bones.sort_by(|(a_id, a_bone), (b_id, b_bone)| {
         get_quality(b_bone)
             .cmp(&get_quality(a_bone))
@@ -101,10 +87,8 @@ fn main() -> io::Result<()> {
                     .map(get_node_num)
                     .cmp(a_bone.iter().map(get_node_num))
             })
-            .then(std::cmp::Ordering::Equal)
             .then(b_id.cmp(a_id))
     });
-
     let p3_bone_checksum: usize = p3_bones
         .iter()
         .enumerate()
