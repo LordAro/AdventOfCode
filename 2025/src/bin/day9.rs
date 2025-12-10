c9a213ecf41c5af816b94d6c083a950b94cd291d
Charles Pigott <charlespigott@googlemail.com>
2025-12-10 18:38:27 +0000

    Complete day9p2

--- 2025/src/bin/day9.rs
+++ 2025/src/bin/day9.rs
@@ -12,14 +12,6 @@ struct Coord {
 
 fn main() -> io::Result<()> {
     let input: Vec<_> = fs::read_to_string(env::args().nth(1).expect("missing cli argument"))?
-//    let input: Vec<_> = "7,1
-//11,1
-//11,7
-//9,7
-//9,5
-//2,5
-//2,3
-//7,3"
         .lines()
         .map(|l| {
             let (x, y) = l.split_once(',').unwrap();
@@ -46,9 +38,6 @@ fn main() -> io::Result<()> {
         .chain(iter::once(last.as_slice()))
         .collect();
 
-    let bounding_min_x = input.iter().map(|c| c.x).min().unwrap();
-    let bounding_min_y = input.iter().map(|c| c.y).min().unwrap();
-
     let p2_max_rect = input
         .iter()
         .combinations(2)
@@ -61,14 +50,7 @@ fn main() -> io::Result<()> {
             a.x != b.x && a.y != b.y
         })
         .filter(|(a, b)| {
-            // remove any rectangles that have chunks taken out of them
-            !input.iter().any(|c| {
-                (a.x.min(b.x) + 1..a.x.max(b.x)).contains(&c.x)
-                    && (a.y.min(b.y) + 1..a.y.max(b.y)).contains(&c.y)
-            })
-        })
-        .filter(|(a, b)| {
-            // check if all 4 corners are within the shape
+            // remove any rectangles that have intersecting line segments
             let tl = Coord {
                 x: a.x.min(b.x),
                 y: a.y.min(b.y),
@@ -85,69 +67,50 @@ fn main() -> io::Result<()> {
                 x: a.x.max(b.x),
                 y: a.y.max(b.y),
             };
+            let rect_line_segs = [[tl, tr], [tr, br], [br, bl], [bl, tl]];
 
-            //println!("corner check");
-            // draw line from min_bounding edge (know we're definitely outside the shape) to the corner corner
-            [tl, tr, bl, br]
+            // true if no intersections
+            !line_segs
                 .iter()
-                .filter(|c| c != a && c != b) // skip our own corners
-                .all(|c| {
-                    //println!(" => corner: {c:?}");
-                    let mut in_shape = false;
-                    // start point is the minimum distance away from the bounding box
-                    let distance_offset =
-                        c.x.abs_diff(bounding_min_x)
-                            .min(c.y.abs_diff(bounding_min_y))
-                            + 1;
-                    let mut search_point = Coord {
-                        x: c.x - distance_offset,
-                        y: c.y - distance_offset,
-                    };
-                    //println!("  => search start: {search_point:?}");
+                .cartesian_product(rect_line_segs.iter())
+                .any(|(shape_ls, rect_ls)| {
+                    let shape_ls_min_x = shape_ls[0].x.min(shape_ls[1].x);
+                    let shape_ls_max_x = shape_ls[0].x.max(shape_ls[1].x);
+                    let rect_ls_min_x = rect_ls[0].x.min(rect_ls[1].x);
+                    let rect_ls_max_x = rect_ls[0].x.max(rect_ls[1].x);
+                    let shape_ls_min_y = shape_ls[0].y.min(shape_ls[1].y);
+                    let shape_ls_max_y = shape_ls[0].y.max(shape_ls[1].y);
+                    let rect_ls_min_y = rect_ls[0].y.min(rect_ls[1].y);
+                    let rect_ls_max_y = rect_ls[0].y.max(rect_ls[1].y);
 
-                    loop {
-                        // use diagonal to avoid having to deal with running along the perimeter
-                        search_point = Coord {
-                            x: search_point.x + 1,
-                            y: search_point.y + 1,
-                        };
-                        if in_shape && search_point == *c {
-                            // don't exit again
-                            break;
+                    if shape_ls_min_x == rect_ls_min_x
+                        && shape_ls_max_x == rect_ls_max_x
+                        && shape_ls_min_y == rect_ls_min_y
+                        && shape_ls_max_y == rect_ls_max_y
+                    {
+                        // identical don't intersect
+                        return false;
+                    }
+
+                    match (
+                        shape_ls_min_x == shape_ls_max_x,
+                        rect_ls_min_x == rect_ls_max_x,
+                    ) {
+                        // parallel lines can't intersect
+                        (true, true) | (false, false) => false,
+                        (true, false) => {
+                            (shape_ls_min_y..=shape_ls_max_y).contains(&rect_ls_min_y)
+                                // don't count end points for second check
+                                && (rect_ls_min_x + 1..rect_ls_max_x).contains(&shape_ls_min_x)
                         }
-                        // if the search point intersects with a line segment
-                        if line_segs.iter().any(|ls| {
-                            let tmp = if ls[0].x == ls[1].x {
-                                search_point.x == ls[0].x
-                                    && search_point.y >= ls[0].y.min(ls[1].y)
-                                    && search_point.y <= ls[0].y.max(ls[1].y)
-                            } else {
-                                search_point.y == ls[0].y
-                                    && search_point.x >= ls[0].x.min(ls[1].x)
-                                    && search_point.x <= ls[0].x.max(ls[1].x)
-                            };
-                            if tmp {
-                                //println!("  => search point {search_point:?} crossed line at {ls:?}");
-                            }
-                            tmp
-                        }) {
-                            in_shape = !in_shape;
-                        }
-                        if search_point == *c {
-                            break;
+                        (false, true) => {
+                            (shape_ls_min_x..=shape_ls_max_x).contains(&rect_ls_min_x)
+                                && (rect_ls_min_y + 1..rect_ls_max_y).contains(&shape_ls_min_y)
                         }
                     }
-                    if !in_shape {
-                        //println!(" => area: {a:?}-{b:?} found to be not be within the shape");
-                    }
-                    in_shape
                 })
         })
-        .map(|(a, b)| {
-            let tmp = (a.x.abs_diff(b.x) + 1) * (a.y.abs_diff(b.y) + 1);
-            println!("{a:?} -> {b:?} = {tmp}");
-            tmp
-        })
+        .map(|(a, b)| (a.x.abs_diff(b.x) + 1) * (a.y.abs_diff(b.y) + 1))
         .max()
         .unwrap();
 
